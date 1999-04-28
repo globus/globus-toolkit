5d7d15b219a97037fd09c24d1a2a57f33514f4b5
tedesco <>
1999-04-28 17:54:25 +0000

    Added globus_i_rsl_assist_get_ldap_param()

--- gram/rsl_assist/source/globus_i_rsl_assist.h
+++ gram/rsl_assist/source/globus_i_rsl_assist.h
@@ -92,6 +92,39 @@ int
 globus_i_rsl_assist_extract_attribute(globus_rsl_t * rsl,
 				   char * attribute,
 				   char ** value);
+/*
+ * Function:globus_i_rsl_assist_get_ldap_param()
+ *
+ * This function return the correct values to use when contacting/searching
+ * the MDS, as defined below:
+ *
+ * - Scan the environment variable GLOBUS_MDS_PORT, GLOBUS_MDS_HOST and
+ *   GLOBUS_MDS_BASE_DN and return there value if set.
+ *   Assumes arguments parsing has already overwritten env variable when
+ *   option -mdshost, -mdsport or -mdsbasedn used.
+ * - for each one not set, search for a grid-info.conf file and uses its
+ *   content to return a value. To determine the path to the file, use first
+ *   the environment variable GLOBUS_INSTALL_PATH if set; if not set, uses
+ *   GLOBUS_SYSCONFDIR, as defined at compile time;
+ * - If a valuse is still not defined after checking env variables
+ *   and grid-info.conf, uses the default -hard coded at compile time values.
+ *
+ * Parameters:
+ *     server  -  name of the MDS ldap server to contact
+ *     port    -  port to use when contacting the above server
+ *     base_dn -  DN to use as root for query/search on the above server.
+ *
+ * Note : server and base_dn are dynamically allocated strings which must be
+ *        freed by the user when not used any more
+ *
+ * Returns:
+ *     GLOBUS_SUCCESS or error code
+ */
+int
+globus_i_rsl_assist_get_ldap_param(char ** server,
+				   int *   port,
+				   char ** base_dn);
+				   
 #endif
 
 
