b04e180a837c4f9b71d4062106fd5ef0e7d7b793
tuecke <>
2000-04-21 21:56:59 +0000

    More or less complete api

--- gass/copy/source/globus_gass_copy.c
+++ gass/copy/source/globus_gass_copy.c
@@ -1,52 +1,219 @@
 
 /**
  * @file globus_gass_copy.c
+ *
+ * Short description
+ *
+ * Long description
  */
 
+
+/************************************************************
+ * Handle initialization and destruction
+ ************************************************************/
+
 /**
+ * Short description
  *
+ * Long descriptioin
+ *
+ * @param param
+ *        Description
+ * @param param
+ *        Description
+ *
+ * @return fuzzy description
+ *
+ * @retval GLOBUS_SUCCESS
+ *         Descriptions
+ * @retval GLOBUS_FAILRUE
+ *
+ * @see globus_gass_copy_destroy()
  */
 globus_result_t
 globus_gass_copy_init(
     globus_gass_copy_handle_t * handle)
+{
+}
 
 globus_result_t
 globus_gass_copy_destroy(
     globus_gass_copy_handle_t * handle)
+{
+}
+
+/************************************************************
+ * Transfer functions (synchronous)
+ ************************************************************/
 
 globus_result_t
-globus_gass_copy_url2url(
+globus_gass_copy_url_to_url(
     globus_gass_copy_handle_t * handle,
     char * source_url,
-    char * dest_url)
-
-globus_result_t
-globus_gass_copy_register_url2url(
-    globus_gass_copy_handle_t * handle,
-    char * source_url,
+    globus_gass_copy_attr_t * source_attr,
     char * dest_url,
-    callback_func,
-    callback_arg)
+    globus_gass_copy_attr_t * dest_attr)
+{
+}
 
 globus_result_t
-globus_gass_copy_url2fd(
+globus_gass_copy_url_to_handle(
     globus_gass_copy_handle_t * handle,
     char * source_url,
-    int dest_fd)
+    globus_gass_copy_attr_t * source_attr,
+    globus_io_handle_t * dest_handle)
+{
+}
 
 globus_result_t
-globus_gass_copy_fd2url(
+globus_gass_copy_handle_to_url(
     globus_gass_copy_handle_t * handle,
-    int source_fd,
-    char * dest_url)
+    globus_io_handle_t * source_handle,
+    char * dest_url,
+    globus_gass_copy_attr_t * dest_attr)
+{
+}
+
+/************************************************************
+ * Transfer functions (asynchronous)
+ ************************************************************/
 
 globus_result_t
-globus_gass_copy_fd2fd(
+globus_gass_copy_register_url_to_url(
     globus_gass_copy_handle_t * handle,
-    int source_fd,
-    int dest_fd)
+    char * source_url,
+    globus_gass_copy_attr_t * dest_attr,
+    char * dest_url,
+    globus_gass_copy_attr_t * source_attr,
+    globus_gass_copy_callback_t callback_func,
+    void * callback_arg)
+{
+}
+
+globus_result_t
+globus_gass_copy_register_url_to_handle(
+    globus_gass_copy_handle_t * handle,
+    char * source_url,
+    globus_gass_copy_attr_t * source_attr,
+    globus_io_handle_t * dest_handle,
+    globus_gass_copy_callback_t callback_func,
+    void * callback_arg)
+{
+}
+
+globus_result_t
+globus_gass_copy_register_handle_to_url(
+    globus_gass_copy_handle_t * handle,
+    globus_io_handle_t * source_handle,
+    char * dest_url,
+    globus_gass_copy_attr_t * dest_attr,
+    globus_gass_copy_callback_t callback_func,
+    void * callback_arg)
+{
+}
+
+/************************************************************
+ * Caching url state
+ ************************************************************/
+
+globus_result_t
+globus_gass_copy_cache_url_state(
+    globus_gass_copy_handle_t * handle,
+    char * url)
+{
+}
+
+globus_result_t
+globus_gass_copy_flush_url_state(
+    globus_gass_copy_handle_t * handle,
+    char * url)
+{
+}
+    
+/************************************************************
+ * User pointers on handles
+ ************************************************************/
+
+globus_result_t
+globus_gass_copy_set_user_pointer(
+    globus_gass_copy_handle_t * handle,
+    void * user_data)
+{
+}
+
+void *
+globus_gass_copy_get_user_pointer(
+    globus_gass_copy_handle_t * handle)
+{
+}
+
+    
+/************************************************************
+ * Attributes
+ ************************************************************/
+
+/**
+ * Set TCP buffer/window size
+ */
+globus_result_t
+globus_gass_copy_attr_set_tcpbuffer(
+    globus_gass_copy_attr_t * attr,
+    globus_gsiftp_control_tcpbuffer_t * tcpbuffer_info)
+{
+}
+
+globus_result_t
+globus_gass_copy_attr_set_parallelism(
+    globus_gass_copy_attr_t * attr,
+    globus_gsiftp_control_parallelism_t * parallelism_info)
+{
+}
+
+globus_result_t
+globus_gass_copy_attr_set_striping(
+    globus_gass_copy_attr_t * attr,
+    globus_gsiftp_control_striping_t * striping_info)
+{
+}
+
+globus_result_t
+globus_gass_copy_attr_set_authorization(
+    globus_gass_copy_attr_t * attr,
+    globus_io_authorization_t * authorization_info)
+{
+}
+    
+globus_result_t
+globus_gass_copy_attr_set_secure_channel(
+    globus_gass_copy_attr_t * attr,
+    globus_io_secure_channel_t * secure_channel_info)
+{
+}
 
 
+/************************************************************
+ * Attributes
+ ************************************************************/
 
-    globus_gass_copy_url2url(handle, gsiftp://pitcairn/foo, ...)
-    globus_gass_copy_url2url(handle, gsiftp://pitcairn/bar, ...)
+/*
+ * Also need get functions...
+ */
+
+globus_io_authorization_t a;
+globus_io_authorization_t b;
+
+a.mode = GLOBUS_IO_AUTHORIZATION_MODE_IDENTITY;
+strcpy(a.data.identity.subject, "foo");
+
+globus_gass_copy_attr_set_authorization(attr, &a); /* copy in */
+globus_gass_copy_attr_get_authorization(attr, &b); /* copy out */
+
+b.mode = ...
+
+globus_gass_copy_attr_set_authorization(attr2, &b); /* copy in */
+
+typedef struct globus_gass_copy_attr_s
+{
+    globus_io_authorization_t a;
+    ...
+} globus_gass_copy_attr_t;
--- gass/copy/source/globus_gass_copy.h
+++ gass/copy/source/globus_gass_copy.h
@@ -1,1 +1,111 @@
 
+typedef void (*globus_gass_copy_callback_t)(
+    void * callback_arg,
+    globus_gass_copy_handle_t * handle,
+    globus_object_t * result);
+
+
+
+typedef enum
+{
+    /** Don't change the TCP buffer/window size from the system default */
+    GLOBUS_GSIFTP_CONTROL_TCPBUFFER_DEFAULT,
+
+    /** Set the TCP buffer/window size to a fixed value */
+    GLOBUS_GSIFTP_CONTROL_TCPBUFFER_FIXED,
+
+    /** Automatically set the TCP buffer/window size */
+    GLOBUS_GSIFTP_CONTROL_TCPBUFFER_AUTOMATIC
+} globus_gsiftp_control_tcpbuffer_mode_t;
+    
+typedef struct globus_gsiftp_control_tcpbuffer_s
+{
+    globus_gsiftp_control_tcpbuffer_mode_t mode;
+    union
+    {
+	/*
+	 * No data required for:
+	 *     GLOBUS_GSIFTP_CONTROL_TCPBUFFER_DEFAULT
+	 */
+	struct /* GLOBUS_GSIFTP_CONTROL_TCPBUFFER_FIXED */
+	{
+	    unsigned long size;
+	} fixed;
+	struct /* GLOBUS_GSIFTP_CONTROL_TCPBUFFER_AUTOMATIC */
+	{
+	    unsigned long initial_size;
+	    unsigned long minimum_size;
+	    unsigned long maximum_size;
+	} automatic;
+    } data;
+} globus_gsiftp_control_tcpbuffer_t;
+
+
+
+typedef enum {
+    /** No parallelism */
+    GLOBUS_GSIFTP_CONTROL_PARALLELISM_NONE,
+
+    /** Partitioned parallelism */
+    GLOBUS_GSIFTP_CONTROL_PARALLELISM_FIXED,
+
+    /** Blocked parallelism with round-robin */
+    GLOBUS_GSIFTP_CONTROL_PARALLELISM_AUTOMATIC,
+} globus_gsiftp_control_parallelism_mode_t;
+    
+typedef struct globus_gsiftp_control_parallelism_s
+{
+    globus_gsiftp_control_parallelism_mode_t mode;
+    union
+    {
+	/*
+	 * No data required for:
+	 *     GLOBUS_GSIFTP_CONTROL_PARALLELISM_NONE
+	 */
+	struct /* GLOBUS_GSIFTP_CONTROL_PARALLELISM_FIXED */
+	{
+	    unsigned long size;
+	} fixed;
+	struct /* GLOBUS_GSIFTP_CONTROL_PARALLELIS_AUTOMATIC */
+	{
+	    unsigned long initial_size;
+	    unsigned long minimum_size;
+	    unsigned long maximum_size;
+	} automatic;
+    } data;
+} globus_gsiftp_control_parallelism_t;
+
+
+
+typedef enum
+{
+    /** No striping */
+    GLOBUS_GSIFTP_CONTROL_STRIPING_NONE,
+
+    /** Partitioned striping */
+    GLOBUS_GSIFTP_CONTROL_STRIPING_PARTITIONED,
+
+    /** Blocked striping with round-robin */
+    GLOBUS_GSIFTP_CONTROL_STRIPING_BLOCKED_ROUND_ROBIN,
+} globus_gsiftp_control_striping_mode_t;
+    
+typedef struct globus_gsiftp_control_striping_s
+{
+    globus_gsiftp_control_striping_mode_t mode;
+    union
+    {
+	/*
+	 * No data required for:
+	 *     GLOBUS_GSIFTP_CONTROL_STRIPING_NONE
+	 *     GLOBUS_GSIFTP_CONTROL_STRIPING_PARTITIONED
+	 */
+	struct /* GLOBUS_GSIFTP_CONTROL_STRIPING_BLOCKED_ROUND_ROBIN */
+	{
+	    unsigned long long block_size;
+	} blocked_round_robin;
+    } data;
+} globus_gsiftp_control_striping_t;
+
+
+
+
