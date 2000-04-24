954fae6d5db99a1c45765778f37a6895254079cd
smartin <>
2000-04-24 19:37:23 +0000

    added some documentation

--- gass/copy/source/globus_gass_copy.h
+++ gass/copy/source/globus_gass_copy.h
@@ -1,11 +1,70 @@
+ /**
+ * @file globus_gass_copy.h
+ *
+ * @brief Globus GASS copy API 
+ *
+ * <h2>Introduction</h2>
+ *
+ * The globus GASS copy library is motivated by the desire to provide a
+ * uniform interface to transfer files specified by different protocols.
+ *
+ * The goals in doing this are to:
+ *
+ * <ul>
+ * <li>Provide a robust way to describe and apply file transfer properties
+ * for a variety of protocols. These include the standard HTTP, FTP and 
+ * GSIFTP options.  Some of the new file transfer capabilities in GSIFTP are
+ * parallel, striping, authentication and TCP buffer sizing.</li>
+ *
+ * <li>Provide a service to support nonblocking file transfer and handle
+ * asynchronous file and network events.</li>
+ *
+ * <li>Provide a simple and portable way to implement file transfers.</li>
+ * </ul>
+ *
+ * Any program that uses Globus GASS copy functions must include
+ * "globus_gass_copy.h".
+ *
+ */
 
-typedef void (*globus_gass_copy_callback_t)(
-    void * callback_arg,
-    globus_gass_copy_handle_t * handle,
-    globus_object_t * result);
+#ifndef GLOBUS_INCLUDE_GLOBUS_GASS_COPY_H
+#define GLOBUS_INCLUDE_GLOBUS_GASS_COPY_H
+
+EXTERN_C_BEGIN
+
+/** Module descriptor
+ *
+ * Globus GASS copy uses standard Globus module activation and deactivation.
+ * Before any Globus GASS copy functions are called, the following function
+ * must be called:
+ *
+ * @code
+ *      globus_module_activate(GLOBUS_GASS_COPY_MODULE)
+ * @endcode
+ *
+ *
+ * This function returns GLOBUS_SUCCESS if Globus GASS copy was successfully
+ * initialized, and you are therefore allowed to subsequently call
+ * Globus GASS copy functions.  Otherwise, an error code is returned, and
+ * Globus GASS copy functions should not be subsequently called. This
+ * function may be called multiple times.
+ *
+ * To deactivate Globus GASS copy, the following function must be called:
+ *
+ * @code
+ *    globus_module_deactivate(GLOBUS_GASS_COPY_MODULE)
+ * @endcode
+ *
+ * This function should be called once for each time Globus GASS copy was
+ * activated.
+ *
+ */
+#define GLOBUS_GASS_COPY_MODULE (&globus_i_gass_copy_module)
+extern
+globus_module_descriptor_t        globus_i_gass_copy_module;
 
 
-
+/** Globus GASS copy GSIFTP control tcpbuffer Types */
 typedef enum
 {
     /** Don't change the TCP buffer/window size from the system default */
@@ -18,6 +77,7 @@ typedef enum
     GLOBUS_GSIFTP_CONTROL_TCPBUFFER_AUTOMATIC
 } globus_gsiftp_control_tcpbuffer_mode_t;
     
+/** Globus GASS copy GSIFTP control tcpbuffer attribute structure.  */
 typedef struct globus_gsiftp_control_tcpbuffer_s
 {
     globus_gsiftp_control_tcpbuffer_mode_t mode;
@@ -41,7 +101,7 @@ typedef struct globus_gsiftp_control_tcpbuffer_s
 } globus_gsiftp_control_tcpbuffer_t;
 
 
-
+/** Globus GASS copy GSIFTP control parallelism Types */
 typedef enum {
     /** No parallelism */
     GLOBUS_GSIFTP_CONTROL_PARALLELISM_NONE,
@@ -53,6 +113,7 @@ typedef enum {
     GLOBUS_GSIFTP_CONTROL_PARALLELISM_AUTOMATIC,
 } globus_gsiftp_control_parallelism_mode_t;
     
+/** Globus GASS copy GSIFTP control parallelism attribute structure.  */
 typedef struct globus_gsiftp_control_parallelism_s
 {
     globus_gsiftp_control_parallelism_mode_t mode;
@@ -77,6 +138,7 @@ typedef struct globus_gsiftp_control_parallelism_s
 
 
 
+/** Globus GASS copy GSIFTP control striping Types */
 typedef enum
 {
     /** No striping */
@@ -88,7 +150,8 @@ typedef enum
     /** Blocked striping with round-robin */
     GLOBUS_GSIFTP_CONTROL_STRIPING_BLOCKED_ROUND_ROBIN,
 } globus_gsiftp_control_striping_mode_t;
-    
+
+/** Globus GASS copy GSIFTP control striping attribute structure.  */
 typedef struct globus_gsiftp_control_striping_s
 {
     globus_gsiftp_control_striping_mode_t mode;
@@ -106,6 +169,31 @@ typedef struct globus_gsiftp_control_striping_s
     } data;
 } globus_gsiftp_control_striping_t;
 
+struct globus_gass_copy_handle_s
+{
+   int       fd;
+   void *    user_pointer;
+   /* ... add other fields */
+} globus_gass_copy_handle_t;
 
+/**
+ * Signature of a callback from globus_gass_copy_register_*() functions.
+ * (asynchronous transfer functions)
+ */
+typedef void (*globus_gass_copy_callback_t)(
+    void * callback_arg,
+    globus_gass_copy_handle_t * handle,
+    globus_object_t * result);
 
+/** initialization and destruction of GASS copy handle */
+globus_result_t
+globus_gass_copy_init(
+    globus_gass_copy_handle_t * handle);
 
+globus_result_t
+globus_gass_copy_destroy(
+    globus_gass_copy_handle_t * handle);
+
+EXTERN_C_END
+
+#endif /* GLOBUS_INCLUDE_GLOBUS_GASS_COPY_H */
