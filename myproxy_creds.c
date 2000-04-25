b7f137c7f8cfafb0b8e136a9d1a3f96d94208764
vwelch <vwelch>
2000-04-25 18:38:54 +0000

    All the code written and compiling, still not debugged.

--- myproxy_creds.c
+++ myproxy_creds.c
@@ -18,9 +18,24 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/param.h>
+#include <assert.h>
+#include <sys/stat.h>
 
 /* Files should only be readable by me */
 #define FILE_MODE		0600
+
+/* Where to store our files */
+#ifndef CREDS_STORAGE_DIRECTORY
+#define CREDS_STORAGE_DIRECTORY		"/usr/local/myproxy/store/"
+#endif /* CREDS_STORAGE_DIRECTORY */
+
+/**********************************************************************
+ *
+ * Internal variables
+ *
+ */
+
+char *storage_dir = CREDS_STORAGE_DIRECTORY;
 
 /**********************************************************************
  *
@@ -28,6 +43,118 @@
  *
  */
 
+/*
+ * mystrdup()
+ *
+ * Wrapper around strdup()
+ */
+static char *
+mystrdup(const char *string)
+{
+    char *dup = NULL;
+    
+    assert(string != NULL);
+    
+    dup = strdup(string);
+    
+    if (dup == NULL)
+    {
+	verror_put_errno(errno);
+	verror_put_string("strdup() failed");
+    }
+    
+    return dup;
+}
+
+	
+/*
+ * check_storage_directory()
+ *
+ * Check for existance and permissions on given storage directory.
+ *
+ * Returns 0 if ok, -1 on error.
+ */
+static int
+check_storage_directory(const char *path)
+{
+    struct stat statbuf;
+    int return_code = -1;
+    
+    
+    if (stat(path, &statbuf) == -1)
+    {
+	verror_put_errno(errno);
+	verror_put_string("could not stat directory %s", path);
+	goto error;
+    }
+    
+    if (!S_ISDIR(statbuf.st_mode))
+    {
+	verror_put_string("%s is not a directory", path);
+	goto error;
+    }
+    
+    /* Make sure it's owned by me */
+    if (statbuf.st_uid != getuid())
+    {
+	verror_put_string("bad ownership on %s", path);
+	goto error;
+    }
+    
+    /* Make sure it's not readable or writable by anyone else */
+    if ((statbuf.st_mode & S_IRWXG) ||
+	(statbuf.st_mode & S_IRWXO))
+    {
+	verror_put_string("bad permissions on %s", path);
+	goto error;
+    }
+    
+    /* Success */
+    return_code = 0;
+    
+  error:
+    return return_code;
+}
+
+
+/*
+ * sterilize_string
+ *
+ * Walk through a string and make sure that is it acceptable for using
+ * as part of a path.
+ */
+void
+sterilize_string(char *string)
+{
+    /* Characters to be removed */
+    char *bad_chars = "/";
+    /* Character to replace any of above characters */
+    char replacement_char = '-';
+    
+    assert(string != NULL);
+    
+    /* No '.' as first character */
+    if (*string == '.')
+    {
+	*string = replacement_char;
+    }
+    
+    /* Replace any bad characters with replacement_char */
+    while (*string != '\0')
+    {
+	if (strchr(bad_chars, *string) != NULL)
+	{
+	    *string = replacement_char;
+	}
+
+	string++;
+    }
+
+    return;
+}
+
+	
+    
 /*
  * get_storage_locations()
  *
@@ -44,7 +171,56 @@ get_storage_locations(const char *username,
 		      char *data_path,
 		      const int data_path_len)
 {
-    return -1;
+    int return_code = -1;
+    char *sterile_username = NULL;
+    const char *creds_suffix = ".creds";
+    const char *data_suffix = ".data";
+    
+    assert(username != NULL);
+    assert(creds_path != NULL);
+    assert(data_path != NULL);
+    assert(storage_dir != NULL);
+    
+    if (check_storage_directory(storage_dir) == -1)
+    {
+	goto error;
+    }
+    
+    sterile_username = mystrdup(username);
+
+    if (sterile_username == NULL)
+    {
+	goto error;
+    }
+    
+    sterilize_string(sterile_username);
+    
+    if (snprintf(creds_path, creds_path_len, "%s/%s%s",
+		 storage_dir, sterile_username, creds_suffix) == -1)
+    {
+	verror_put_string("Internal error: creds_path too small: %s line %s",
+			  __FILE__, __LINE__);
+	goto error;
+    }
+
+    if (snprintf(data_path, data_path_len, "%s/%s%s",
+		 storage_dir, sterile_username, data_suffix) == -1)
+    {
+	verror_put_string("Internal error: data_path too small: %s line %s",
+			  __FILE__, __LINE__);
+	goto error;
+    }
+
+    /* Success */
+    return_code = 0;
+
+  error:
+    if (sterile_username != NULL)
+    {
+	free(sterile_username);
+    }
+    
+    return return_code;
 }
 
 /*
@@ -60,6 +236,77 @@ copy_file(const char *source,
 	  const char *dest,
 	  const mode_t mode)
 {
+    int src_fd = -1;
+    int dst_fd = -1;
+    int src_flags = O_RDONLY;
+    int dst_flags = O_WRONLY | O_CREAT | O_TRUNC;
+    char buffer[2048];
+    int bytes_read;
+    int return_code = -1;
+    
+    assert(source != NULL);
+    assert(dest != NULL);
+    
+    src_fd = open(source, src_flags);
+    
+    if (src_fd == -1)
+    {
+	verror_put_errno(errno);
+	verror_put_string("opening %s for reading", source);
+	goto error;
+    }
+    
+    dst_fd = open(dest, dst_flags, mode);
+    
+    if (dst_fd == -1)
+    {
+	verror_put_errno(errno);
+	verror_put_string("opening %s for writing", dest);
+	goto error;
+    }
+    
+    do 
+    {
+	bytes_read = read(src_fd, buffer, sizeof(buffer));
+	
+	if (bytes_read == -1)
+	{
+	    verror_put_errno(errno);
+	    verror_put_string("reading %s", source);
+	    goto error;
+	}
+
+	if (bytes_read != 0)
+	{
+	    if (write(dst_fd, buffer, bytes_read) == -1)
+	    {
+		verror_put_errno(errno);
+		verror_put_string("writing %s", dest);
+		goto error;
+	    }
+	}
+    }
+    while (bytes_read > 0);
+    
+    /* Success */
+    return_code = 0;
+	
+  error:
+    if (src_fd != -1)
+    {
+	close(src_fd);
+    }
+    
+    if (dst_fd != -1)
+    {
+	close(dst_fd);
+
+	if (return_code == -1)
+	{
+	    unlink(dest);
+	}
+    }
+    
     return -1;
 }
 
@@ -139,7 +386,120 @@ static int
 read_data_file(struct myproxy_creds *creds,
 	       const char *datafile_path)
 {
-    return -1;
+    FILE *data_stream = NULL;
+    char *data_stream_mode = "r";
+    int done = 0;
+    int line_number = 0;
+    int return_code = -1;
+    
+    assert(creds != NULL);
+    assert(datafile_path != NULL);
+    
+    data_stream = fopen(datafile_path, data_stream_mode);
+    
+    if (data_stream == NULL)
+    {
+	verror_put_errno(errno);
+	verror_put_string("opening %s for reading", datafile_path);
+	goto error;
+    }
+
+    while (!done)
+    {
+	char buffer[512];
+	char *variable;
+	char *value;
+	
+	if (fgets(buffer, sizeof(buffer), data_stream) == NULL)
+	{
+	    int errno_save = errno;
+	    
+	    if (feof(data_stream))
+	    {
+		verror_put_string("unexpected EOF reading %s", datafile_path);
+		goto error;
+	    }
+	    else
+	    {
+		verror_put_errno(errno_save);
+		verror_put_string("reading %s", datafile_path);
+		goto error;
+	    }
+	    /* Not reached */
+	}
+
+	line_number++;
+	
+	value = strchr(buffer, '=');
+	
+	if (value != NULL)
+	{
+	    /* NUL-terminate variable name */
+	    *value = '\0';
+
+	    /* ...and advance value to point at value */
+	    value++;
+	}
+
+	if (strcmp(variable, "END_OPTIONS") == 0) 
+	{
+	    done = 1;
+	    break;
+	}
+	
+	/* Everything else requires values to be non-NULL */
+	if (value == NULL)
+	{
+	    verror_put_string("malformed line: %s line %d",
+			      datafile_path, line_number);
+	    goto error;
+	}
+	
+	if (strcmp(variable, "OWNER") == 0)
+	{
+	    creds->owner_name = mystrdup(value);
+	    
+	    if (creds->owner_name == NULL)
+	    {
+		goto error;
+	    }
+	    continue;
+	}
+
+	if (strcmp(variable, "PASSPHRASE") == 0)
+	{
+	    creds->pass_phrase = mystrdup(value);
+	    
+	    if (creds->pass_phrase == NULL)
+	    {
+		goto error;
+	    }
+	    continue;
+	}
+	
+	if (strcmp(variable, "LIFETIME") == 0)
+	{
+	    creds->lifetime = (int) strtol(value, NULL, 10);
+	    
+	    continue;
+	}
+	
+	/* Unrecognized varibale */
+	verror_put_string("unrecognized line: %s line %d",
+			  datafile_path, line_number);
+	goto error;
+    }
+
+    /* Success */
+    return_code = 0;
+    
+  error:
+    if (data_stream != NULL)
+    {
+	fclose(data_stream);
+    }
+    
+    return return_code;
 }
 
 
@@ -228,11 +588,10 @@ myproxy_creds_retrieve(const char *user_name,
 	goto error;
     }
 
-    creds->location = strdup(creds_path);
+    creds->location = mystrdup(creds_path);
     
     if (creds->location == NULL)
     {
-	verror_put_errno(errno);
 	goto error;
     }
     
