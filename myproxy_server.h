dec0fcd4997bde710ba48f83dd832ab930d17a97
vwelch <vwelch>
2000-05-01 20:53:26 +0000

    Added (untested) authorization checking code.

--- myproxy_server.h
+++ myproxy_server.h
@@ -27,8 +27,44 @@ typedef struct
 } myproxy_server_context_t;
 
 
-/* In myproxy_server_config.c */
-int mproxy_server_config_read(myproxy_server_context_t *context);
+/**********************************************************************
+ *
+ * Routines from myproxy_server_config.c
+ *
+ */
 
+/*
+ * myproxy_server_config_read()
+ *
+ * Read the configuration file as indicated in the context, parse
+ * it and store the results in the context.
+ *
+ * Returns 0 on success, -1 on error setting verror.
+ */
+int myproxy_server_config_read(myproxy_server_context_t *context);
+
+/*
+ * myproxy_server_check_client()
+ *
+ * Check to see if the given client is authorized to store credentials
+ * with the server based on the previously read context.
+ *
+ * Returns 1 if the client is authorized, 0 if unauthorized,
+ * -1 on error, setting verror.
+ */
+int myproxy_server_check_client(myproxy_server_context_t *context,
+				const char *client_name);
+/*
+ * myproxy_server_check_service()
+ *
+ * Check to see if the given service is authorized to retrieve
+ * credentials stored with the server based on the previously read
+ * context.
+ *
+ * Returns 1 if the client is authorized, 0 if unauthorized,
+ * -1 on error, setting verror.
+ */
+int myproxy_server_check_service(myproxy_server_context_t *context,
+				 const char *service_name);
 
 #endif /* !__MYPROXY_SERVER_H */
