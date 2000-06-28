5c63b7157ab13a94b087c9b30393ff06f18c46d5
vwelch <>
2000-06-28 00:20:57 +0000

    Fixes for my bone-headed coding as pointed out by Benjamin Temko.

--- myproxy/myproxy_server_config.c
+++ myproxy/myproxy_server_config.c
@@ -45,7 +45,8 @@ add_entry(char **entries,
     int current_length = 0;
     char **new_entries;
     char *my_entry;
-
+    int new_size;
+    
     assert(entry != NULL);
     
     my_entry = strdup(entry);
@@ -62,9 +63,11 @@ add_entry(char **entries,
 	    current_length++;
 	}
     }
-    
-    new_entries = realloc(entries,
-			  current_length + 2 /* New pointer and NULL */);
+
+    /* Add enough for new pointer and NULL */
+    new_size = sizeof(char *) * (current_length + 2);
+
+    new_entries = realloc(entries, new_size);
     
     if (new_entries == NULL)
     {
