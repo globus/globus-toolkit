d088065ca55df68409042c0c3dcd83acf9216ac7
bletzing <>
2000-12-13 18:50:25 +0000

    Fixed --ldflags bug.

--- core/source/tests/simple_dep.sh
+++ core/source/tests/simple_dep.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 ./generate_parameter_files.sh
-../scripts/globus_build_config.pl --flavor=sweet fum > simple_dep.rslt
+../scripts/globus_build_config.pl --flavor=sweet --ldflags="-lm -lXm" fum > simple_dep.rslt
 diff simple_dep.exp simple_dep.rslt
