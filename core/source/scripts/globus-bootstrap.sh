454672240d7669c1b497aa0c976b1e0a5fc1d604
meder <>
2001-06-12 21:16:42 +0000

    *** empty log message ***

--- core/source/scripts/globus-bootstrap.sh
+++ core/source/scripts/globus-bootstrap.sh
@@ -1,30 +1,30 @@
 
 if test ! -h globus_automake_targets ; then
-    echo "installing link to automake_targets"
+    echo "installing globus_automake_targets link"
     ln -s $GLOBUS_LOCATION/share/globus_aclocal/automake_targets \
     globus_automake_targets
 fi
 
 if test ! -h globus_automake_rules ; then
-    echo "installing link to automake_rules"
+    echo "installing globus_automake_rules link"
     ln -s $GLOBUS_LOCATION/share/globus_aclocal/automake_rules \
     globus_automake_rules
 fi
 
 if test ! -h globus_automake_doxygen_rules ; then
-    echo "installing link to automake_doxygen_rules"
+    echo "installing globus_automake_doxygen_rules link"
     ln -s $GLOBUS_LOCATION/share/globus_aclocal/automake_doxygen_rules \
     globus_automake_doxygen_rules
 fi
 
 if test ! -h globus_automake_top_rules ; then
-    echo "installing link to automake_top_rules"
+    echo "installing globus_automake_top_rules link"
     ln -s $GLOBUS_LOCATION/share/globus_aclocal/automake_top_rules \
     globus_automake_top_rules
 fi
 
 if test ! -h globus_automake_config ; then
-    echo "installing link to automake_config"
+    echo "installing globus_automake_config link"
     ln -s $GLOBUS_LOCATION/share/globus_aclocal/automake_config \
     globus_automake_config
 fi
