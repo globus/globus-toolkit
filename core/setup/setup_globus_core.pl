9728f25d3c47739db93ba6528be665fb88704c8c
meder <>
2001-06-20 20:38:18 +0000

    fix SH detection
    remove softlinking of sh and perl

--- core/setup/setup_globus_core.pl
+++ core/setup/setup_globus_core.pl
@@ -19,22 +19,6 @@ my $globusdir = $ENV{GLOBUS_LOCATION};
 my $setupdir = "$globusdir/setup/globus/";
 my $result = `$setupdir/findshelltools`;
 
-open(TOOLS, "$setupdir/globus-sh-tools.sh");
-
-my ($perl_location, $sh_location);
-
-while (<TOOLS>) {
-  m!PERL=(\S+)! && do { $perl_location = $1 };
-  m!SH=(\S+)! && do { $sh_location = $1 };
-}
-
-close TOOLS;
-
-mkinstalldirs("$globusdir/bin");
-
-$result = system("ln -s $perl_location $globusdir/bin/perl");
-$result = system("ln -s $sh_location $globusdir/bin/sh");
-
 for my $f ('globus-script-initializer', 'globus-sh-tools.sh')
 {
     $result = system("cp $f $globusdir/libexec");
@@ -44,17 +28,3 @@ for my $f ('globus-script-initializer', 'globus-sh-tools.sh')
 
 
 $metadata->finish();
-
-sub mkinstalldirs
-  {
-    my $dir = shift;
-    my @dirlist = split m!/!, $dir;
-    my $subdir= "";
-    for my $d (@dirlist) {
-      $subdir .= "/$d";
-      if (! -d $subdir) {
-	my $result = `mkdir $subdir`;
-      }
-    }
-  }
-
