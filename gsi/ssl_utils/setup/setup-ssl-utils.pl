f34d24b1ee496372cf7e5156607b39c88f236cfb
welch <>
2001-10-05 16:02:41 +0000

    -Check for existance of /etc/grid-security, don't do anything if  it already exists. -Create /etc/grid-security -Create /etc/grid-security and contents (Globus CA cert and policy)

--- gsi/ssl_utils/setup/setup-ssl-utils.pl
+++ gsi/ssl_utils/setup/setup-ssl-utils.pl
@@ -17,16 +17,53 @@ my $metadata = new Grid::GPT::Setup(package_name => "globus_ssl_utils_setup");
 
 my $globusdir = $ENV{GLOBUS_LOCATION};
 my $setupdir = "$globusdir/setup/globus/";
-my $result = `$setupdir/setup-ssl-utils-sh-scripts`;
 
-$result = system("cp $setupdir/grid-security.conf /etc/grid-security/grid-security.conf");
+my $target_dir = "/etc/grid-security";
+my $trusted_certs_dir = $target_dir . "/certificates";
 
-$result = system("chmod 0644 /etc/grid-security/grid-security.conf");
+my $myname = "setup-ssl-utils";
 
-$result = system("chmod 0755 $setupdir/grid-security-config");
+if ( -d $target_dir ) {
+  print "$myname: GSI configuration directory $target_dir already exists.\n";
+  print "$myname: Not doing anything.\n";
 
-$result = system("chmod 0755 $setupdir/grid-cert-request-config");
+} else {
 
-$result = system("$setupdir/grid-security-config");
+  my $result = `$setupdir/setup-ssl-utils-sh-scripts`;
+
+  $result = system("mkdir $target_dir");
+
+  if ($result != 0) {
+    # Make sure we have write permissions
+    die "Failed to create $target_dir: $!";
+  }
+
+  $result = system("chmod 755 $target_dir");
+
+  $result = system("cp $setupdir/grid-security.conf $target_dir/grid-security.conf");
+
+  $result = system("chmod 0644 $target_dir/grid-security.conf");
+
+  # XXX I believe this can die
+  $result = system("chmod 0755 $setupdir/grid-security-config");
+
+  # XXX I believe this can die
+  $result = system("chmod 0755 $setupdir/grid-cert-request-config");
+
+  $result = system("$setupdir/grid-security-config");
+
+  # Create trusted certs directory
+  $result = system("mkdir $trusted_certs_dir");
+
+  $result = system("chmod 755 $trusted_certs_dir");
+
+  $result = system("cp $setupdir/42864e48.0 $trusted_certs_dir");
+
+  $result = system("chmod 644 $trusted_certs_dir/42864e48.0");
+
+  $result = system("cp $setupdir/42864e48.signing_policy $trusted_certs_dir");
+
+  $result = system("chmod 644 $trusted_certs_dir/42864e48.signing_policy");
+}
 
 $metadata->finish();
