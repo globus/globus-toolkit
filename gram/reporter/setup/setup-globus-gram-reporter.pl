a1c0d59041b68460e4cf37bf0069a2baa453b05b
smartin <>
2001-10-16 04:54:56 +0000

    *** empty log message ***

--- gram/reporter/setup/setup-globus-gram-reporter.pl
+++ gram/reporter/setup/setup-globus-gram-reporter.pl
@@ -25,18 +25,25 @@ my $need_print = 1;
 my $hostname = `${setupdir}/globus-hostname`;
 $hostname =~ s/\s//g; #strip whitespace
 
-print "This entry must be added to the bottom of $globus_dir/etc/grid-info-resource-ldif.conf\n";
+print "\nIn the file ${globusdir}/etc/grid-info-slapd.conf\n";
+print "The following line must be added after the other include statements.\n";
+print ">>>>>>>>>\n";
+print "include  ${globusdir}/etc/grid-info-gram-reporter.schema\n";
+print "<<<<<<<<<\n\n\n";
+
+print "In the file $globus_dir/etc/grid-info-resource-ldif.conf\n";
+print "The following lines must be added at the bottom.\n";
 
 print "AFTER THIS LINE--------->\n";
 print "# generate gram info every 30 seconds\n";
-print "dn: Gram-service=jobmanager, Mds-Host-hn=${hostname}, Mds-Vo-name=local, o=grid\n";
+print "dn: Mds-Software-deployment=jobmanager, Mds-Host-hn=${hostname}, Mds-Vo-name=local, o=grid\n";
 print "objectclass: GlobusTop\n";
 print "objectclass: GlobusActiveObject\n";
 print "objectclass: GlobusActiveSearch\n";
 print "type: exec\n";
 print "path: $globusdir/libexec\n";
 print "base: globus-gram-reporter\n";
-print "args: -conf $globusdir/etc/globus-job-manager.conf -type fork -rdn jobmanager -dmdn=Mds-Host-hn=${hostname},Mds-Vo-name=local,o=grid\n";
+print "args: -conf $globusdir/etc/globus-job-manager.conf -type fork -rdn jobmanager -host-dn Mds-Host-hn=${hostname},Mds-Vo-name=local,o=grid\n";
 print "cachetime: 30\n";
 print "timelimit: 20\n";
 print "sizelimit: 20\n";
