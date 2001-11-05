14449e326083635c5069445fdb1b4faf50266e9b
smartin <>
2001-11-05 17:03:48 +0000

    removed unneccessary undefs as auto_header will find the AC_CHECK_HEADERS in configure.in

--- io/source/acconfig.h
+++ io/source/acconfig.h
@@ -1,13 +1,3 @@
 /* Application specific options */
 
-#undef HAVE_NETINET_TCP_H
-
-#undef HAVE_SYS_XTI_H
-
-#undef HAVE_SYS_SELECT_H
-
-#undef HAVE_STRING_H
-
-#undef HAVE_DCE_CMA_UX_H
-
 #undef GLOBUS_BUILD_WITH_NETLOGGER
