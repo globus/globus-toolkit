c6467a061860fa7d12bb1176f38ce47bd2ee81cd
jbasney <jbasney>
2001-11-15 21:15:59 +0000

    added cast on malloc() return value

--- gsi_proxy.c
+++ gsi_proxy.c
@@ -8,7 +8,7 @@ int proxy_pvd_init(const char *certdir, proxy_verify_desc *pvd)
    /* standalone GSI (1.1.3a - beta) or Globus v. 2.0 */
    proxy_verify_ctx_desc *pvxd;
 
-   pvxd = malloc(sizeof(*pvxd));
+   pvxd = (proxy_verify_ctx_desc *)malloc(sizeof(*pvxd));
    if (pvxd == NULL) 
       return -1;
    proxy_verify_ctx_init(pvxd);
