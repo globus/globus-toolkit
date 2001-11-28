7f6e60ed8c31e6c40f673ef3e9f9719a7fdeef3b
smartin <>
2001-11-28 15:44:32 +0000

    Per req# 3536
    Added braces {} around all variables.
    
    "In the file globus-user-env.sh the env variables
     should be surrounded by {} in order to avoid errors
     with some shell scripting languages (es. zsh)."

--- user_env/source/globus-user-env.csh
+++ user_env/source/globus-user-env.csh
@@ -21,36 +21,36 @@ if ( ! $?SASL_PATH ) then
 endif
 
 if ( $?GLOBUS_PATH ) then
-    setenv PATH `echo "$PATH" | sed -e "s%:$GLOBUS_PATH[^:]*%%g" -e "s%^$GLOBUS_PATH[^:]*:\{0,1\}%%"`
-    setenv MANPATH `echo "$MANPATH" | sed -e "s%:$GLOBUS_PATH[^:]*%%g" -e "s%^$GLOBUS_PATH[^:]*:\{0,1\}%%"`
-    setenv LD_LIBRARY_PATH `echo "$LD_LIBRARY_PATH" | sed -e "s%:$GLOBUS_PATH[^:]*%%g" -e "s%^$GLOBUS_PATH[^:]*:\{0,1\}%%"`
-    setenv SASL_PATH `echo "$SASL_PATH" | sed -e "s%:$GLOBUS_PATH[^:]*%%g" -e "s%^$GLOBUS_PATH[^:]*:\{0,1\}%%"`
+    setenv PATH `echo "${PATH}" | sed -e "s%:${GLOBUS_PATH}[^:]*%%g" -e "s%^${GLOBUS_PATH}[^:]*:\{0,1\}%%"`
+    setenv MANPATH `echo "${MANPATH}" | sed -e "s%:${GLOBUS_PATH}[^:]*%%g" -e "s%^${GLOBUS_PATH}[^:]*:\{0,1\}%%"`
+    setenv LD_LIBRARY_PATH `echo "${LD_LIBRARY_PATH}" | sed -e "s%:${GLOBUS_PATH}[^:]*%%g" -e "s%^${GLOBUS_PATH}[^:]*:\{0,1\}%%"`
+    setenv SASL_PATH `echo "${SASL_PATH}" | sed -e "s%:${GLOBUS_PATH}[^:]*%%g" -e "s%^${GLOBUS_PATH}[^:]*:\{0,1\}%%"`
 endif
 
-setenv PATH `echo "$PATH" | sed -e "s%:$GLOBUS_LOCATION[^:]*%%g" -e "s%^$GLOBUS_LOCATION[^:]*:\{0,1\}%%"`
-setenv MANPATH `echo "$MANPATH" | sed -e "s%:$GLOBUS_LOCATION[^:]*%%g" -e "s%^$GLOBUS_LOCATION[^:]*:\{0,1\}%%"`
-setenv LD_LIBRARY_PATH `echo "$LD_LIBRARY_PATH" | sed -e "s%:$GLOBUS_LOCATION[^:]*%%g" -e "s%^$GLOBUS_LOCATION[^:]*:\{0,1\}%%"`
-setenv SASL_PATH `echo "$SASL_PATH" | sed -e "s%:$GLOBUS_LOCATION[^:]*%%g" -e "s%^$GLOBUS_LOCATION[^:]*:\{0,1\}%%"`
+setenv PATH `echo "${PATH}" | sed -e "s%:${GLOBUS_LOCATION}[^:]*%%g" -e "s%^${GLOBUS_LOCATION}[^:]*:\{0,1\}%%"`
+setenv MANPATH `echo "${MANPATH}" | sed -e "s%:${GLOBUS_LOCATION}[^:]*%%g" -e "s%^${GLOBUS_LOCATION}[^:]*:\{0,1\}%%"`
+setenv LD_LIBRARY_PATH `echo "${LD_LIBRARY_PATH}" | sed -e "s%:${GLOBUS_LOCATION}[^:]*%%g" -e "s%^${GLOBUS_LOCATION}[^:]*:\{0,1\}%%"`
+setenv SASL_PATH `echo "${SASL_PATH}" | sed -e "s%:${GLOBUS_LOCATION}[^:]*%%g" -e "s%^${GLOBUS_LOCATION}[^:]*:\{0,1\}%%"`
 
-setenv GLOBUS_PATH "$GLOBUS_LOCATION"
-setenv PATH "$GLOBUS_LOCATION/bin:$GLOBUS_LOCATION/sbin:$PATH";
+setenv GLOBUS_PATH "${GLOBUS_LOCATION}"
+setenv PATH "${GLOBUS_LOCATION}/bin:${GLOBUS_LOCATION}/sbin:${PATH}";
 
 set DELIM
-if ( "X$MANPATH" != "X" ) then
+if ( "X${MANPATH}" != "X" ) then
     set DELIM=:
-    setenv MANPATH "$GLOBUS_LOCATION/man$DELIM$MANPATH"
+    setenv MANPATH "${GLOBUS_LOCATION}/man${DELIM}${MANPATH}"
 endif
 
 
 set DELIM=
-if ( "X$LD_LIBRARY_PATH" != "X" ) then
+if ( "X${LD_LIBRARY_PATH}" != "X" ) then
     set DELIM=:
 endif
-setenv LD_LIBRARY_PATH "$GLOBUS_LOCATION/lib$DELIM$LD_LIBRARY_PATH"
+setenv LD_LIBRARY_PATH "${GLOBUS_LOCATION}/lib${DELIM}${LD_LIBRARY_PATH}"
 
 set DELIM=
-if ( "X$SASL_PATH" != "X" ) then
+if ( "X${SASL_PATH}" != "X" ) then
     set DELIM=:
 endif
-setenv SASL_PATH "$GLOBUS_LOCATION/lib/sasl$DELIM$SASL_PATH"
+setenv SASL_PATH "${GLOBUS_LOCATION}/lib/sasl${DELIM}${SASL_PATH}"
 
--- user_env/source/globus-user-env.sh
+++ user_env/source/globus-user-env.sh
@@ -3,42 +3,42 @@
 # source this file to properly set up your environment for globus applications
 #
 
-if [ -z "$GLOBUS_LOCATION" ]; then
+if [ -z "${GLOBUS_LOCATION}" ]; then
     echo "ERROR: environment variable GLOBUS_LOCATION not defined"  1>&2
     return 1
 fi
 
-if [ -n "$GLOBUS_PATH" ]; then
-    PATH=`echo "$PATH" | sed -e "s%:$GLOBUS_PATH[^:]*%%g" -e "s%^$GLOBUS_PATH[^:]*:\{0,1\}%%"`
-    MANPATH=`echo "$MANPATH" | sed -e "s%:$GLOBUS_PATH[^:]*%%g" -e "s%^$GLOBUS_PATH[^:]*:\{0,1\}%%"`
-    LD_LIBRARY_PATH=`echo "$LD_LIBRARY_PATH" | sed -e "s%:$GLOBUS_PATH[^:]*%%g" -e "s%^$GLOBUS_PATH[^:]*:\{0,1\}%%"`
-    SASL_PATH=`echo "$SASL_PATH" | sed -e "s%:$GLOBUS_PATH[^:]*%%g" -e "s%^$GLOBUS_PATH[^:]*:\{0,1\}%%"`
+if [ -n "${GLOBUS_PATH}" ]; then
+    PATH=`echo "${PATH}" | sed -e "s%:${GLOBUS_PATH}[^:]*%%g" -e "s%^${GLOBUS_PATH}[^:]*:\{0,1\}%%"`
+    MANPATH=`echo "${MANPATH}" | sed -e "s%:${GLOBUS_PATH}[^:]*%%g" -e "s%^${GLOBUS_PATH}[^:]*:\{0,1\}%%"`
+    LD_LIBRARY_PATH=`echo "${LD_LIBRARY_PATH}" | sed -e "s%:${GLOBUS_PATH}[^:]*%%g" -e "s%^${GLOBUS_PATH}[^:]*:\{0,1\}%%"`
+    SASL_PATH=`echo "${SASL_PATH}" | sed -e "s%:${GLOBUS_PATH}[^:]*%%g" -e "s%^${GLOBUS_PATH}[^:]*:\{0,1\}%%"`
 fi
 
-PATH=`echo "$PATH" | sed -e "s%:$GLOBUS_LOCATION[^:]*%%g" -e "s%^$GLOBUS_LOCATION[^:]*:\{0,1\}%%"`
-MANPATH=`echo "$MANPATH" | sed -e "s%:$GLOBUS_LOCATION[^:]*%%g" -e "s%^$GLOBUS_LOCATION[^:]*:\{0,1\}%%"`
-LD_LIBRARY_PATH=`echo "$LD_LIBRARY_PATH" | sed -e "s%:$GLOBUS_LOCATION[^:]*%%g" -e "s%^$GLOBUS_LOCATION[^:]*:\{0,1\}%%"`
-SASL_PATH=`echo "$SASL_PATH" | sed -e "s%:$GLOBUS_LOCATION[^:]*%%g" -e "s%^$GLOBUS_LOCATION[^:]*:\{0,1\}%%"`
+PATH=`echo "${PATH}" | sed -e "s%:${GLOBUS_LOCATION}[^:]*%%g" -e "s%^${GLOBUS_LOCATION}[^:]*:\{0,1\}%%"`
+MANPATH=`echo "${MANPATH}" | sed -e "s%:${GLOBUS_LOCATION}[^:]*%%g" -e "s%^${GLOBUS_LOCATION}[^:]*:\{0,1\}%%"`
+LD_LIBRARY_PATH=`echo "${LD_LIBRARY_PATH}" | sed -e "s%:${GLOBUS_LOCATION}[^:]*%%g" -e "s%^${GLOBUS_LOCATION}[^:]*:\{0,1\}%%"`
+SASL_PATH=`echo "${SASL_PATH}" | sed -e "s%:${GLOBUS_LOCATION}[^:]*%%g" -e "s%^${GLOBUS_LOCATION}[^:]*:\{0,1\}%%"`
 
-GLOBUS_PATH=$GLOBUS_LOCATION
-PATH="$GLOBUS_LOCATION/bin:$GLOBUS_LOCATION/sbin:$PATH";
+GLOBUS_PATH=${GLOBUS_LOCATION}
+PATH="${GLOBUS_LOCATION}/bin:${GLOBUS_LOCATION}/sbin:${PATH}";
 
 DELIM=
-if [ -n "$MANPATH" ]; then
+if [ -n "${MANPATH}" ]; then
     DELIM=:
-    MANPATH="$GLOBUS_LOCATION/man$DELIM$MANPATH"
+    MANPATH="${GLOBUS_LOCATION}/man${DELIM}${MANPATH}"
 fi
 
 DELIM=
-if [ -n "$LD_LIBRARY_PATH" ]; then
+if [ -n "${LD_LIBRARY_PATH}" ]; then
     DELIM=:
 fi
-LD_LIBRARY_PATH="$GLOBUS_LOCATION/lib$DELIM$LD_LIBRARY_PATH"
+LD_LIBRARY_PATH="${GLOBUS_LOCATION}/lib${DELIM}${LD_LIBRARY_PATH}"
 
 DELIM=
-if [ -n "$SASL_PATH" ]; then
+if [ -n "${SASL_PATH}" ]; then
     DELIM=:
 fi
-SASL_PATH="$GLOBUS_LOCATION/lib/sasl$DELIM$SASL_PATH"
+SASL_PATH="${GLOBUS_LOCATION}/lib/sasl${DELIM}${SASL_PATH}"
 
 export GLOBUS_PATH PATH MANPATH LD_LIBRARY_PATH SASL_PATH
