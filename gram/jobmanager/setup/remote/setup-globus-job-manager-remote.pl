98bbdb50697634fbf54d41955fb70995172803d6
bester <>
2002-05-17 19:22:19 +0000

    Initial commit of remote GRAM scheduler interface. This scheduler type does an ssh to a remote host with the job manager scripts for a local scheduler.

---
+++ gram/jobmanager/setup/remote/setup-globus-job-manager-remote.pl
@@ -0,0 +1,83 @@
+my $gpath = $ENV{GPT_LOCATION};
+
+if (!defined($gpath))
+{
+    $gpath = $ENV{GLOBUS_LOCATION};
+}
+
+if (!defined($gpath))
+{
+    die "GPT_LOCATION or GLOBUS_LOCATION needs to be set before running this script";
+}
+
+@INC = (@INC, "$gpath/lib/perl");
+
+require Grid::GPT::Setup;
+use Getopt::Long;
+
+my $name		= 'jobmanager-remote';
+my $host;
+my $type;
+my $location;
+my $force		= 0;
+my $cmd;
+
+GetOptions('service-name|s=s' => \$name,
+	   'force|f' => \$force,
+	   'host|h=s'  => \$host,
+	   'type|t=s' => \$type,
+	   'location|l=s' => \$location,
+	   'help|h|?' => \$help);
+
+&usage if $help;
+
+my $metadata =
+    new Grid::GPT::Setup(package_name => "globus_remote_job_manager_setup");
+
+my $globusdir	= $ENV{GLOBUS_LOCATION};
+my $libexecdir	= "$globusdir/libexec";
+
+if($force != 0)
+{
+    $force = '-f';
+}
+else
+{
+    $force = '';
+}
+
+if($host eq "" || $type eq "" || $location eq "")
+{
+    &usage;
+}
+
+# Do script relocation
+print `./find-remote-tools --with-host=$host --with-location=$location --with-manager=$type`;
+if($? != 0)
+{
+    print STDERR "Error locating remote commands, aborting!\n";
+    exit 2;
+}
+
+# Create service
+$cmd = "$libexecdir/globus-add-job-manager-service -m remote -s \"$name\" $force";
+system("$cmd >/dev/null 2>/dev/null");
+
+if($? == 0)
+{
+    $metadata->finish();
+}
+else
+{
+    print STDERR "Error creating service entry $name. Aborting!\n";
+    exit 3;
+}
+
+sub usage
+{
+    print "Usage: $0 [options] -host=HOST -type=TYPE -location=LOCATION\n".
+          "Options:  [--service-name|-s service_name]\n".
+          "          [--force|-f]\n".
+	  "          [--help|-h]\n";
+    exit 1;
+}
