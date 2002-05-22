ebbdd188af8d432b6439a1fcef85165c8d29ea5f
bester <>
2002-05-22 22:15:07 +0000

    Added host type checking in setup-globus-job-manager-pbs and find-pbs-tools

--- gram/jobmanager/setup/pbs/setup-globus-job-manager-pbs.pl
+++ gram/jobmanager/setup/pbs/setup-globus-job-manager-pbs.pl
@@ -19,12 +19,15 @@ my $name		= 'jobmanager-pbs';
 my $manager_type	= 'pbs';
 my $force		= 0;
 my $cmd;
+my $host_type = "";
 
 GetOptions('service-name|s=s' => \$name,
+           'with-host-type=s' => \$host_type,
 	   'force|f' => \$force,
 	   'help|h|?' => \$help);
 
 &usage if $help;
+&usage if $host_type eq "";
 
 my $metadata =
     new Grid::GPT::Setup(package_name => "globus_pbs_job_manager_setup");
@@ -42,7 +45,7 @@ else
 }
 
 # Do script relocation
-print `./find-pbs-tools`;
+print `./find-pbs-tools --with-host-type=$host_type`;
 if($? != 0)
 {
     print STDERR "Error locating PBS commands, aborting!\n";
@@ -65,7 +68,7 @@ else
 
 sub usage
 {
-    print "Usage: $0 [options]\n".
+    print "Usage: $0 [options] --with-host-type=cluster|chiba_cluster|smp\n".
           "Options:  [--service-name|-s service_name]\n".
           "          [--force|-f]\n".
 	  "          [--help|-h]\n";
