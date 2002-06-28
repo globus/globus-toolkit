151a87f382939f37ff74a7201482f8a1a426e0b2
smartin <>
2002-06-28 21:11:51 +0000

    Updated setup script to set condor arch and condor os in service file Fixed parsing error, missing $condor_rm definition, and some other errors in the condor module.

--- gram/jobmanager/setup/condor/setup-globus-job-manager-condor.pl
+++ gram/jobmanager/setup/condor/setup-globus-job-manager-condor.pl
@@ -3,10 +3,15 @@ use Getopt::Long;
 
 my $name		= 'jobmanager-condor';
 my $manager_type	= 'condor';
+my $condor_os		= '';
+my $condor_arch		= '';
+my $c_opts		= '';
 my $cmd;
 
 GetOptions('service-name|s=s' => \$name,
-	   'help|h' => \$help);
+	   'help|h' => \$help,
+	   'condor-os=s' => \$condor_os,
+	   'condor-arch=s' => \$condor_arch);
 
 &usage if $help;
 
@@ -16,12 +21,25 @@ my $metadata =
 my $globusdir	= $ENV{GLOBUS_LOCATION};
 my $libexecdir	= "$globusdir/libexec";
 
-mkdir $ENV{GLOBUS_LOCATION} . "/lib/perl/Globus/GRAM/JobManager";
+mkdir $ENV{GLOBUS_LOCATION} . "/lib/perl/Globus/GRAM/JobManager", 0777;
 
-print `./find-condor-tools`;
+if($condor_os ne '')
+{
+    $c_opts = ' --with-condor-os=$condor_os';
+}
+if($condor_arch ne '')
+{
+    $c_opts .= ' --with-condor-arch=$condor_arch';
+}
+
+print `./find-condor-tools $c_opts --cache-file=/dev/null`;
+chmod 0755, 'globus-condor-print-config';
+
+my $condor_jm_config = `globus-condor-print-config`;
+chomp($condor_jm_config);
 
 $cmd = "$libexecdir/globus-job-manager-service-add -m condor -s \"$name\"";
-system("$cmd >/dev/null 2>/dev/null");
+system("$cmd -extra-config='$condor_jm_config' >/dev/null 2>/dev/null");
 
 if($? == 0)
 {
@@ -35,7 +53,9 @@ else
 sub usage
 {
     print "Usage: $0 [options]\n".
-          "Options:  [--service-name|-s service_name]\n".
-	  "          [--help|-h]\n";
+          "Options:  [-service-name|-s service_name]\n".
+	  "          [-condor-os=CONDOR OS]\n".
+	  "          [-condor-arch=CONDOR ARCH]\n".
+	  "          [-help]\n";
     exit 1;
 }
