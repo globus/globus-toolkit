3c60389bc117d86a3ab6abb21ce1f59b9bbce879
meder <>
2002-08-26 19:08:39 +0000

    make stuff run again

--- gsi/openssl_error/test/run-openssl-error-tests.pl
+++ gsi/openssl_error/test/run-openssl-error-tests.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use Test::Harness;
 require 5.005;
 use vars qw(@tests);
 
-@tests = qw( globus_openssl_error_test.pl
+@tests = qw( globus-openssl-error-test.pl
 	     );
 
 runtests(@tests);
