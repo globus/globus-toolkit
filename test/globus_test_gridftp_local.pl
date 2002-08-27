f4054fc0caac6397310c87290a987d2d31bde186
meder <>
2002-08-27 22:11:52 +0000

    add command_blocking and wait_command for dealing with blocking commands
    make ftp_local use these

--- test/globus_test_gridftp_local.pl
+++ test/globus_test_gridftp_local.pl
@@ -21,16 +21,46 @@ sub test_gridftp_local {
 
     $u->command(". env.sh");
 
-    $u->command("killall in.ftpd",0,1);
-    $u->command("in.ftpd -a -s -p 9998 > /dev/null &");
-    $u->command("in.ftpd -a -s -p 9999 > /dev/null &");
-    $output = $u->command("grid-cert-info -subject",1);
-    $u->command("globus-url-copy -s \"$output\" \\
-        gsiftp://localhost:9998/etc/termcap \\
+    my $subject = `grid-proxy-info -subject`;
+    chomp($subject);
+
+    $subject =~ s|(/CN=proxy)*||g;
+    
+    $ENV{GRIDMAP}="gridmap";
+    
+    if( 0 != system("grid-mapfile-add-entry -dn \"$subject\" -ln `whoami` -f $ENV{GRIDMAP} >/dev/null 2>&1") / 256)
+    {
+        print "Unable to create gridmap file\n";
+        exit 1;
+    }
+
+    my ($source_pid, $source_fd) = 
+        $u->command_blocking("in.ftpd -a -1 -s -p 9999");
+    my ($dest_pid, $dest_fd) = 
+        $u->command_blocking("in.ftpd -a -1 -s -p 9998");
+
+    $u->command("globus-url-copy -s \"$subject\" \\
+        gsiftp://localhost:9998/etc/group \\
         gsiftp://localhost:9999/tmp/gridftp.test");
-    $output = $u->command("diff /etc/termcap /tmp/gridftp.test");
-    $output eq "" ? $u->report("ok") : $u->report("not ok");
+
+    my ($server_rc, $server_output) = $u->wait_command($source_pid,
+                                                       $source_fd);
+    
+    if($server_rc == -1)
+    {
+        $output .= "$server_output\n";
+    }
+
+    ($server_rc, $server_output) = $u->wait_command($dest_pid,
+                                                    $dest_fd);
+
+    if($server_rc == -1)
+    {
+        $output .= "$server_output\n";
+    }
+
+    $output .= $u->command("diff /etc/group /tmp/gridftp.test");
+    $output eq "" ? $u->report("SUCCESS") : $u->report("FAILURE");
 
     $u->command("rm -f /tmp/gridftp.test");
-    $u->command("killall in.ftpd");
 }
