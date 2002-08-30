67f7c0b2e857ee7e1bc0d268bc579fda4df62624
meder <>
2002-08-30 02:53:03 +0000

    add dirt, GPT and flavor version info to banner and output of -V option

--- gridftp/gsi-wuftpd/source/src/ftpd.c
+++ gridftp/gsi-wuftpd/source/src/ftpd.c
@@ -274,6 +274,8 @@ int Send(FILE *sockfp, char *format,...);
 #ifdef USE_GLOBUS_DATA_CODE
 extern globus_ftp_control_handle_t               g_data_handle;
 extern char *                                    g_perf_log_file_name;
+extern int
+gsi_wuftp_get_version(void);
 #endif
 
 
@@ -287,7 +289,7 @@ extern char *ctime(const time_t *);
 extern char *crypt(const char *, const char *);
 #endif
 
-extern char version[];
+extern char *version;
 extern char *home;		/* pointer to home directory for glob */
 extern char cbuf[];
 extern off_t restart_point;
@@ -669,6 +671,9 @@ int i = 0;
     char *globus_authorization_config_file = NULL /* Use Default */;
 #endif /* GLOBUS_AUTHORIZATION */
 
+    /* generate version string; populates char * version global*/
+    gsi_wuftp_get_version();
+    
 #ifdef AUX
     setcompat(COMPAT_POSIX | COMPAT_BSDSETUGID);
 #endif
@@ -1464,9 +1469,10 @@ int i = 0;
 	    else if (!strcasecmp(ARG0, "brief"))
 		version_option = 1;
 	}
+	version_option = 0;
 	switch (version_option) {
 	default:
-	    reply(220, "%s FTP server (%s) ready.", hostname, version);
+	    reply(220, "%s %s ready.", hostname, version);
 	    break;
 	case 1:
 	    reply(220, "%s FTP server ready.", hostname);
