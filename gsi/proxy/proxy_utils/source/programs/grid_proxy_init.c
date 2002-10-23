1497bd7448e5269d5c41c0d51ccaeb72a6663da2
slang <>
2002-10-23 16:50:48 +0000

    disabled restriction/group stuff (correctly this time).

--- gsi/proxy/proxy_utils/source/programs/grid_proxy_init.c
+++ gsi/proxy/proxy_utils/source/programs/grid_proxy_init.c
@@ -159,15 +159,19 @@ main(
     BIO *                               pem_proxy_bio = NULL;
     time_t                              goodtill;
     time_t                              lifetime;
+/*
     char *                              restriction_buf = NULL;
     size_t                              restriction_buf_len = 0;
     char *                              restriction_filename = NULL;
     char *                              policy_language = NULL;
     int                                 policy_NID;
+*/
     int                                 (*pw_cb)() = NULL;
+/*
     char *                              trusted_subgroup = NULL;
     char *                              untrusted_subgroup = NULL;
     char *                              subgroup = NULL;
+*/
     int                                 return_value = 0;
     
     if(globus_module_activate(GLOBUS_GSI_PROXY_MODULE) != (int)GLOBUS_SUCCESS)
@@ -313,6 +317,7 @@ main(
         {
             pw_cb = globus_i_gsi_proxy_utils_pwstdin_callback;
         }
+/*
         else if(strcmp(argp, "-policy") == 0)
         {
             args_verify_next(arg_index, argp, 
@@ -351,6 +356,7 @@ main(
             untrusted_subgroup = argv[++arg_index];
             proxy_type = GLOBUS_RESTRICTED_PROXY;
         }
+*/
         else
         {
             args_error(arg_index, argp, "unrecognized option");
@@ -733,6 +739,7 @@ main(
     }
 
     /* add restrictions now */
+/** PROXY RESTRICTIONS/GROUPS DISABLED CURRENTLY
     if(restriction_filename)
     {
         int                             restriction_buf_size = 0;
@@ -751,7 +758,7 @@ main(
         {
             restriction_buf_size += 512;
             
-            /* First time through this is a essentially a malloc() */
+            * First time through this is a essentially a malloc() *
             restriction_buf = realloc(restriction_buf,
                                       restriction_buf_size);
 
@@ -767,13 +774,13 @@ main(
                 fread(&restriction_buf[restriction_buf_len], 1, 
                       512, restriction_fp);
 
-            /*
+            *
              * If we read 512 bytes then restriction_buf_len and
              * restriction_buf_size will be equal and there is
              * probably more to read. Even if there isn't more
              * to read, no harm is done, we just allocate 512
              * bytes we don't end up using.
-             */
+             *
         }
         while (restriction_buf_len == restriction_buf_size);
         
@@ -827,6 +834,7 @@ main(
             GLOBUS_I_GSI_PROXY_UTILS_PRINT_ERROR;
         }
     }
+** PROXY RESTRICTIONS/GROUPS currently DISABLED **/
 
     if (!quiet)
     {
