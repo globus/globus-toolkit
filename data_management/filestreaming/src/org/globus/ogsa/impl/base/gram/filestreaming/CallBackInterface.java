689fe97640e083a6cb9d992a18983fd5442d60f5
madduri <>
2002-12-19 22:05:00 +0000

    *** empty log message ***

---
+++ data_management/filestreaming/src/org/globus/ogsa/impl/base/gram/filestreaming/CallBackInterface.java
@@ -0,0 +1,7 @@
+package org.globus.ogsa.impl.base.gram.filestreaming;
+
+public interface CallBackInterface {
+
+    public void callBack();
+    public void register(CallBackInterface callBack);
+}
