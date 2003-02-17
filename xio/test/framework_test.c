7497cbb3dc69ce8c66671ab946a80fd174cc3f24
bresnaha <>
2003-02-17 20:46:45 +0000

    adding build files for test

--- xio/test/framework_test.c
+++ xio/test/framework_test.c
@@ -1,5 +1,16 @@
 #include "globus_xio.h"
 
+void
+test_res(
+    globus_result_t                         res,
+    int                                     line)
+{
+    if(res != GLOBUS_SUCCESS)
+    {
+        fprintf(stderr, "error at line %d.\n", line);
+        globus_assert(res == GLOBUS_SUCCESS);
+    }
+}
 
 int
 main(
@@ -10,6 +21,8 @@ main(
     globus_xio_stack_t                      stack;
     globus_xio_handle_t                     handle;
     globus_xio_driver_t                     driver;
+    globus_xio_target_t                     target;
+    globus_result_t                         res;
 
     rc = globus_module_activate(GLOBUS_XIO_MODULE);
     globus_assert(rc == 0);
@@ -17,7 +30,7 @@ main(
     res = globus_xio_stack_init(&stack, NULL);
     test_res(res, __LINE__);
 
-    res = globus_xio_stack_push_driver(stack, driver, NULL);
+    res = globus_xio_stack_push_driver(stack, driver);
     test_res(res, __LINE__);
 
     res = globus_xio_target_init(&target, NULL, "whatever", stack);
