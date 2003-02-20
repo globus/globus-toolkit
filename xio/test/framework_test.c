49827c3790b7a86c6a25e5fa4fee84fa1c0f284e
link <>
2003-02-20 23:51:30 +0000

    get rid of unload call

--- xio/test/framework_test.c
+++ xio/test/framework_test.c
@@ -301,8 +301,6 @@ main(
     }
     globus_mutex_unlock(&globus_l_mutex);
     
-    globus_xio_driver_unload("test", driver);
-    
     rc = globus_module_deactivate(GLOBUS_XIO_MODULE);
     globus_assert(rc == 0);
 
