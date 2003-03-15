e7e1d9ad959a594ca1ef07c2de63a4265940bd56
bresnaha <>
2003-03-15 07:23:12 +0000

    allowed to pass in a null context

--- xio/src/globus_xio_macro_magic.c
+++ xio/src/globus_xio_macro_magic.c
@@ -55,7 +55,11 @@ globus_xio_driver_pass_open_DEBUG(
               _driver->transform_open_func == NULL);
 
         /* hold a ref for this driver */
-        _context->ref++;
+        if(_out_context != NULL)
+        {
+            GlobusXIODebugSetOut(_out_context, _my_context);
+            _context->ref++;
+        }
 
         _op->entry[_prev_ndx].next_ndx = _op->ndx;
         _op->entry[_prev_ndx].type = GLOBUS_XIO_OPERATION_TYPE_OPEN;
@@ -112,7 +116,6 @@ globus_xio_driver_pass_open_DEBUG(
             globus_i_xio_handle_destroy(_handle);
         }
 
-        GlobusXIODebugSetOut(_out_context, _my_context);
         GlobusXIODebugSetOut(_out_res, _res);
     }
     GlobusXIODebugInternalExit();
