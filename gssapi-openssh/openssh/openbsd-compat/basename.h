1e4361b371e6d189d24986f6eed7cb4964351d29
cvs2git <>
2003-04-07 18:05:58 +0000

    This commit was manufactured by cvs2svn to create branch 'OPENSSH_3_1P1_GSSAPI_GPT-branch'.
    Cherrypick from OPENSSH_PORTABLE_DIST 2003-04-04 18:31:38 UTC jbasney 'Import of OpenSSH 3.6.1p1':
        gssapi-openssh/openssh/kexdhc.c
        gssapi-openssh/openssh/kexdhs.c
        gssapi-openssh/openssh/kexgexc.c
        gssapi-openssh/openssh/kexgexs.c
        gssapi-openssh/openssh/openbsd-compat/basename.c
        gssapi-openssh/openssh/openbsd-compat/basename.h
        gssapi-openssh/openssh/openbsd-compat/vis.c
        gssapi-openssh/openssh/openbsd-compat/vis.h
        gssapi-openssh/openssh/progressmeter.c
        gssapi-openssh/openssh/progressmeter.h
        gssapi-openssh/openssh/regress/keygen-change.sh
        gssapi-openssh/openssh/regress/proxy-connect.sh
    Cherrypick from master 2003-04-07 18:05:57 UTC jbasney 'moved kexgss_client() from kexgss.c to kexgssc.c and':
        gssapi-openssh/openssh/kexgssc.c
        gssapi-openssh/openssh/kexgsss.c

---
+++ gssapi-openssh/openssh/openbsd-compat/basename.h
@@ -0,0 +1,12 @@
+/* $Id$ */
+
+#ifndef _BASENAME_H 
+#define _BASENAME_H
+#include "config.h"
+
+#if !defined(HAVE_BASENAME)
+
+char *basename(const char *path);
+
+#endif /* !defined(HAVE_BASENAME) */
+#endif /* _BASENAME_H */
