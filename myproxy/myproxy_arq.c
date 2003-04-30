0ae79934e1d885daf83dfbdc937e3c03bb9275e4
jbasney <>
2003-04-30 17:39:51 +0000

    - added --credname, --expiring_in, and --remove

--- myproxy/myproxy_arq.c
+++ myproxy/myproxy_arq.c
@@ -10,40 +10,44 @@
 #define BINARY_NAME "myproxy-admin-query"
 #define SECONDS_PER_HOUR 3600
 
-static char usage[] = \
-"\n"\
-"Admin Repository Query Tool\n"\
-"\n"\
-" Syntax:  "  BINARY_NAME " [-usage|-help] [-version]\n"\
-"\n"\
-"    Options\n"\
-"    -h | --help                	Displays usage\n"\
-"    -u | --usage                             \n"\
-"						\n"\
-"    -l | --username <name>		Username of credentials (optional)\n"\
-"    -t | --time_left <hours>		Min. time left in hours (optional)\n"\
-"    -v | --verbose             	Display debugging messages\n"\
-"    -V | --version             	Displays version\n"\
-"    -s | --storage <directory> 	Specifies the credential storage directory\n"
-"                                            \n"\
+static char usage[] = 
+"\n"
+"Admin Repository Query Tool\n"
+"\n"
+" Syntax:  "  BINARY_NAME " [-usage|-help] [-version] ...\n"
+"\n"
+"    Options\n"
+"    -h | --help                     Displays usage\n"
+"    -u | --usage                                  \n"
+"                                                  \n"
+"    -s | --storage      <directory> Specifies the credential storage directory\n"
+"    -l | --username     <name>      Query by username\n"
+"    -k | --credname     <name>      Query by credential name\n"
+"    -e | --expiring_in  <hours>     Query for creds expiring in less than \n"
+"                                    specified <hours>\n"
+"    -t | --time_left    <hours>     Query for creds with lifetime greater \n"
+"                                    than specified <hours>\n"
+"    -r | --remove                   Remove credentials matching query\n"
+"    -v | --verbose                  Display debugging messages\n"
+"    -V | --version                  Displays version\n"
 "\n";
 
 struct option long_options[] =
 {
-    {"help",             no_argument, NULL, 'h'},
-    {"usage",            no_argument, NULL, 'u'},
-    {"username",   required_argument, NULL, 'l'},
-    {"time_left",  required_argument, NULL, 't'},
-    {"storage",	   required_argument, NULL, 's'},
-    {"verbose",          no_argument, NULL, 'v'},
-    {"version",          no_argument, NULL, 'V'},
+    {"help",              no_argument, NULL, 'h'},
+    {"usage",             no_argument, NULL, 'u'},
+    {"username",    required_argument, NULL, 'l'},
+    {"credname",    required_argument, NULL, 'k'},
+    {"expiring_in", required_argument, NULL, 'e'},
+    {"time_left",   required_argument, NULL, 't'},
+    {"storage",	    required_argument, NULL, 's'},
+    {"verbose",           no_argument, NULL, 'v'},
+    {"version",           no_argument, NULL, 'V'},
+    {"remove",            no_argument, NULL, 'r'},
     {0, 0, 0, 0}
 };
 
-static char short_options[] = "hul:t:s:vV";
-
-char *username;  /* User preferences */
-time_t time_left;
+static char short_options[] = "hul:k:e:t:s:vVr";
 
 static char version[] =
 BINARY_NAME "version " MYPROXY_VERSION " (" MYPROXY_VERSION_DATE ") "  "\n";
@@ -52,29 +56,23 @@ BINARY_NAME "version " MYPROXY_VERSION " (" MYPROXY_VERSION_DATE ") "  "\n";
 void init_arguments(int argc, char *argv[]);
 
 void print_cred_info(myproxy_creds_t *creds);
+void do_remove_creds(myproxy_creds_t *creds);
+
+struct myproxy_creds cred = {0};
+int remove_creds = 0;
 
 int
 main(int argc, char *argv[]) 
 {
-    struct myproxy_creds cred;
-
-    username = NULL;
-    time_left = 0;
-
    /* Initialize arguments*/
     init_arguments(argc, argv);
 
-    memset (&cred, 0, sizeof (cred));
-
-    if (username)
-	cred.username = strdup (username);
-
-    if (time_left)
-	cred.end_time = time_left+time(0);
-
     if (myproxy_admin_retrieve_all(&cred) < 0) {
         myproxy_log_verror();
-        fprintf (stderr, "Unable to read credentials !! %s\n", verror_get_string());
+        fprintf (stderr, "Failed to read credentials.\n%s\n",
+		 verror_get_string());
+    } else if (remove_creds) {
+	do_remove_creds (&cred);
     } else {
 	print_cred_info (&cred);
     }
@@ -90,30 +88,30 @@ init_arguments(int argc,
     int arg;
 
     while((arg = gnu_getopt_long(argc, argv, short_options, 
-                             long_options, NULL)) != EOF) 
-    {
-        switch(arg) 
-        {  
-        case 's': /* set the credential storage directory */
-        { char *s;
-          s=(char *) malloc(strlen(gnu_optarg) + 1);
-          strcpy(s,gnu_optarg);
-          myproxy_set_storage_dir(s);
-          break;
-         }
+                             long_options, NULL)) != EOF) {
+        switch(arg) {  
+	case 'h': 	/* print help and exit */
         case 'u': 	/* print help and exit */
             fprintf(stderr, usage);
             exit(1);
        	    break;
-	case 'h': 	/* print help and exit */
-            fprintf(stderr, usage);
-            exit(1);
-            break;
+        case 's': /* set the credential storage directory */
+	    myproxy_set_storage_dir(gnu_optarg);
+	    break;
         case 'l':	/* username */
-	    username = strdup (gnu_optarg);
+	    cred.username = strdup(gnu_optarg);
+	    break;
+        case 'k':	/* credname */
+	    cred.credname = strdup(gnu_optarg);
+	    break;
+	case 'e':	/* expiring in <hours> */
+	    cred.end_time = (SECONDS_PER_HOUR * atoi(gnu_optarg)) + time(0);
 	    break;
 	case 't':	/* time left */
-	    time_left = SECONDS_PER_HOUR * atoi(gnu_optarg);
+	    cred.start_time = (SECONDS_PER_HOUR * atoi(gnu_optarg)) + time(0);
+	    break;
+	case 'r':	/* remove */
+	    remove_creds = 1;
 	    break;
 	case 'v':	/* verbose */
 	    myproxy_debug_set_level(1);
@@ -184,3 +182,27 @@ print_cred_info(myproxy_creds_t *creds)
     }
 }
 
+void
+do_remove_creds(myproxy_creds_t *creds)
+{
+    if (!creds) return;
+
+    if (creds->owner_name == NULL && creds->next == NULL)
+    {
+	printf("No credentials present.\n");
+	return;
+    }
+
+    for (; creds; creds = creds->next) {
+	if (myproxy_creds_delete(creds) == 0) {
+	    printf("Credential for user %s (name: %s) removed.\n",
+		   creds->username,
+		   creds->credname ? creds->credname : "default");
+	} else {
+	    fprintf(stderr, "Failed to remove credential for user %s "
+		    "(name: %s).\n%s\n", creds->username,
+		    creds->credname ? creds->credname : "default",
+		    verror_get_string());
+	}
+    }
+}
