daee7d16ceea800825e25fb7025dc9a3c235a613
bresnaha <>
2003-05-15 20:23:42 +0000

    added telnet program

---
+++ xio/example/globus_xio_telnet.c
@@ -0,0 +1,131 @@
+#include "globus_xio.h"
+
+#define LINE_LEN 1024
+
+void
+test_res(
+    globus_result_t                         res)
+{
+    if(res == GLOBUS_SUCCESS)
+    {
+        return;
+    }
+
+    fprintf(stderr, "ERROR: %s\n", globus_object_printable_to_string(
+        globus_error_get(res)));
+
+    globus_assert(0);
+}
+
+void
+help()
+{
+    fprintf(stdout, "globus_xio_telnet [options] <host> <port>\n");
+    fprintf(stdout, "-----------------\n");
+    fprintf(stdout, "Opens up a tcp connection to host:port.  All data read\n");
+    fprintf(stdout, "from stdin is sent down the connection, and all data\n");
+    fprintf(stdout, "read from the connection is sent to stdout.\n");
+    fprintf(stdout, "\n");
+    fprintf(stdout, "options:\n");
+    fprintf(stdout, "-D <driver> : add this driver to the stack\n");
+}
+
+int
+main(
+    int                                     argc,
+    char **                                 argv)
+{
+    int                                     rc;
+    globus_xio_driver_t                     tcp_driver;
+    globus_xio_driver_t                     driver;
+    globus_xio_stack_t                      stack;
+    globus_xio_handle_t                     xio_handle;
+    globus_xio_target_t                     target;
+    char *                                  cs;
+    globus_result_t                         res;
+    char                                    line[LINE_LEN];
+    int                                     ctr;
+    globus_bool_t                           done = GLOBUS_FALSE;
+    globus_size_t                           nbytes;
+
+    globus_module_activate(GLOBUS_XIO_MODULE);
+
+    res = globus_xio_driver_load("tcp", &tcp_driver);
+    test_res(res);
+    globus_xio_stack_init(&stack, NULL);
+    globus_xio_stack_push_driver(stack, tcp_driver);
+
+    if(argc < 2)
+    {
+        help();
+        return 1;
+    }
+
+    for(ctr = 1; ctr < argc - 2; ctr++)
+    {
+        if(strcmp(argv[ctr], "-h") == 0)
+        {
+            help();
+            return 0;
+        }
+        else if(strcmp(argv[ctr], "-D") == 0)
+        {
+            res = globus_xio_driver_load(argv[ctr], &driver);
+            globus_xio_stack_push_driver(stack, driver);
+        }
+    }
+
+    cs = argv[1];
+
+    res = globus_xio_target_init(&target, NULL, cs, stack);
+    test_res(res);
+    res = globus_xio_open(&xio_handle, NULL, target);
+    test_res(res);
+
+    while(!done)
+    {
+        res = globus_xio_read(xio_handle, line,
+                sizeof(line), 1, &nbytes, NULL);
+        test_res(res);
+        for(ctr = 0; ctr < nbytes; ctr++)
+        {
+            if(isprint(line[ctr]) || line[ctr] == '\n')
+            {
+                fprintf(stdout, "%c", line[ctr]);
+            }
+        }
+        fflush(stdout);
+
+        if(fgets(line, LINE_LEN, stdin) == NULL)
+        {
+            done = GLOBUS_TRUE;
+        }
+        else
+        {
+            res = globus_xio_write(xio_handle, line, 
+                    sizeof(line), strlen(line), NULL, NULL);
+            test_res(res);
+            if(line[0] == 29)
+            {
+                fprintf(stdout, "xio telnet>");
+                fflush(stdout);
+                scanf("%s", line);
+                if(strcmp(line, "quit") == 0)
+                {
+                    done = GLOBUS_TRUE;
+                }
+            }
+        }
+    }
+
+    res = globus_xio_close(xio_handle, NULL);
+    test_res(res);
+
+    res = globus_xio_driver_unload(tcp_driver);
+    test_res(res);
+
+    rc = globus_module_activate(GLOBUS_XIO_MODULE);
+    globus_assert(rc == GLOBUS_SUCCESS);
+
+    return 0;
+}
