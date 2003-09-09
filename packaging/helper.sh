2f437b3e58355ad54d3bbc9db67f44dfa8509d2a
bacon <>
2003-09-09 15:59:52 +0000

    helper.sh now does GT3 base installer, rather than all gt2+gt3.

--- packaging/helper.sh
+++ packaging/helper.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-TAG2=globus_2_4_3_pre1
+TAG2=globus_2_4_3pre2
 TAG3=globus_3_0_branch
 
 if [ x$GLOBUS_LOCATION = x ]; then
 	echo "If you set a GLOBUS_LOCATION, I will install there."
+else
+	INSTALL=--install=/tmp/testing
 fi
 export GPT_LOCATION=`pwd`/gpt-2.2.9
 
-./make-packages.pl -t2=$TAG2 -t3=$TAG3 --uncool --version=3.0.2pre --paranoia $@
+./make-packages.pl -t2=$TAG2 -t3=$TAG3 --uncool --paranoia --bundles="globus-resource-management-server,gt3-all-src,mmjfs,mmjfs-static,ogsi-cbindings,gt3-extras,schedulers" $INSTALL $@
 
 if [ $? -ne 0 ]; then
 	echo Packaging failed.
@@ -26,15 +28,5 @@ rm packaging_list
 rm ../schedulers*.tar.gz
 
 if [ x$GLOBUS_LOCATION != x ]; then
-	cd ..
-	$GPT_LOCATION/sbin/gpt-build globus-reso*server* gcc32dbg
-	$GPT_LOCATION/sbin/gpt-build globus-data*server* gcc32dbg
-	# $GPT_LOCATION/sbin/gpt-build globus-info* gcc32dbgpthr
-	$GPT_LOCATION/sbin/gpt-build gt3-all-src* gcc32dbg
-	$GPT_LOCATION/sbin/gpt-build ogsi-cbindings* gcc32dbg
-	$GPT_LOCATION/sbin/gpt-build gt3-extras* gcc32dbg
-	$GPT_LOCATION/sbin/gpt-build mmjfs-3* gcc32dbg
-	$GPT_LOCATION/sbin/gpt-build -static mmjfs-s* gcc32dbg
-
 	$GPT_LOCATION/sbin/gpt-postinstall
 fi
