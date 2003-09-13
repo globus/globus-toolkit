ef2dc1daa7db79329b8a20d9c9d614b8b4de05a6
link <>
2003-09-13 04:01:55 +0000

    cant free fifo, it becomes list

--- common/source/library/globus_args.c
+++ common/source/library/globus_args.c
@@ -455,7 +455,6 @@ globus_args_scan(
 	rc = -1;   /* ensure negative return value for non-success */
 
     globus_mutex_unlock(&args_mutex);
-    globus_fifo_destroy(&fifo);
     return rc;
 }
 /* globus_args_scan() */
