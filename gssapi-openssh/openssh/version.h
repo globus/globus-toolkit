24f8da67833668bc378826e08e034f20984d9bdf
jbasney <>
2004-01-17 17:24:17 +0000

    include GSI, KRB5, and MECHGLUE in version string based on configure options for more debugging info

--- gssapi-openssh/openssh/version.h
+++ gssapi-openssh/openssh/version.h
@@ -1,4 +1,23 @@
 /* $OpenBSD: version.h,v 1.39 2003/09/16 21:02:40 markus Exp $ */
 
+#ifdef GSI
+#define GSI_VERSION	" GSI"
+#else
+#define GSI_VERSION	""
+#endif
+
+#ifdef KRB5
+#define KRB5_VERSION	" KRB5"
+#else
+#define KRB5_VERSION	""
+#endif
+
+#ifdef MECHGLUE
+#define MGLUE_VERSION	" MECHGLUE"
+#else
+#define MGLUE_VERSION	""
+#endif
+
 #define SSH_VERSION	"OpenSSH_3.7.1p2"	\
-			" NCSA_GSSAPI_20040117"
+			" NCSA_GSSAPI_20040117" \
+			GSI_VERSION KRB5_VERSION MGLUE_VERSION
