5d451df1a19319da80daddfb0ff60c38287bc5c2
gaffaney <>
2004-02-29 22:55:07 +0000

    'Renamed' WinProjects to win32, added winmake.am

---
+++ gsi/gssapi/source/win32/globus_gssapi_config.h
@@ -0,0 +1,12 @@
+/* library/globus_gssapi_config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if your processor stores words with the most significant
+   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
+#undef WORDS_BIGENDIAN
+
+/* Name of package */
+#define PACKAGE "gssapi"
+
+/* Version number of package */
+#define VERSION  "2.0"
+
