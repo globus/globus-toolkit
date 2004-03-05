fc2311d5436f1fcdb9c9615509ff8386d0f94491
jbasney <>
2004-03-05 19:22:40 +0000

    don't clear environment. GSI needs it (GLOBUS_LOCATION, X509_CERT_DIR, LD_LIBRARY_PATH, etc.)

--- gssapi-openssh/openssh/sshd.c
+++ gssapi-openssh/openssh/sshd.c
@@ -1119,8 +1119,10 @@ main(int ac, char **av)
 	chdir("/");
 
 #ifndef HAVE_CYGWIN
+#ifndef GSI /* GSI gets configuration from environment! */
 	/* Clear environment */
 	environ[0] = NULL;
+#endif
 #endif
 
 	/* ignore SIGPIPE */
