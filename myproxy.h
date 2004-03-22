801b3d9de95b6783cbca102e8ab576f3f2166408
jbasney <jbasney>
2004-03-22 19:36:45 +0000

    v0.6.0 22 Mar 2004

--- myproxy.h
+++ myproxy.h
@@ -12,9 +12,9 @@
 
 /* software version constants */
 #define MYPROXY_VERSION_MAJOR 0
-#define MYPROXY_VERSION_MINOR 5
-#define MYPROXY_VERSION_MICRO 9
-#define MYPROXY_VERSION_DATE "v0.5.9 28 Jan 2004"
+#define MYPROXY_VERSION_MINOR 6
+#define MYPROXY_VERSION_MICRO 0
+#define MYPROXY_VERSION_DATE "v0.6.0 22 Mar 2004"
 
 #include "myproxy_constants.h"
 #include "myproxy_authorization.h"
