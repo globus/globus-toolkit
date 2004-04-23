2ccf57550b9d7b173a5fd494cbc20832760384cf
jbasney <>
2004-04-23 02:16:56 +0000

    re-fix old bug, re-introduced on re-merge of Simon's code: gss_indicate_mechs() needs to be in PRIVSEP() because we need the list of mechanisms supported by the privileged process; the unprivileged process can't load gssapi mech libraries

--- gssapi-openssh/openssh/gss-serv.c
+++ gssapi-openssh/openssh/gss-serv.c
@@ -86,7 +86,8 @@ ssh_gssapi_supported_oids(gss_OID_set *oidset)
 	gss_OID_set supported;
 
 	gss_create_empty_oid_set(&min_status, oidset);
-	gss_indicate_mechs(&min_status, &supported);
+	/* Ask priviledged process what mechanisms it supports. */
+	PRIVSEP(gss_indicate_mechs(&min_status, &supported));
 
 	while (supported_mechs[i]->name != NULL) {
 		if (GSS_ERROR(gss_test_oid_set_member(&min_status,
