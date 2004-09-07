9f46eaac70c8eacd8390eba9ea840faa2b7c0089
mlink <>
2004-09-07 19:21:55 +0000

    stopped linking in icu4c (this won't get merged to trunk -- will only affect this branch)

--- common/source/library/globus_thread_common.c
+++ common/source/library/globus_thread_common.c
@@ -356,11 +356,6 @@ globus_l_thread_common_activate(void)
 {
     int rc;
 
-    /* 
-     * safely initialize ICU library w/ threads
-     */
-    uloc_getDefault(); 
-
     rc = globus_thread_key_create(&l_thread_stack_key,
 			          globus_l_thread_blocking_callback_destroy);
 
