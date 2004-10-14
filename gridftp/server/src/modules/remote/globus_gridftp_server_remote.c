b7991c92a1a7862c2df20ca301781a1984386c2a
mlink <>
2004-10-14 09:53:34 +0000

    more leaks in remote

--- gridftp/server/src/modules/remote/globus_gridftp_server_remote.c
+++ gridftp/server/src/modules/remote/globus_gridftp_server_remote.c
@@ -1328,7 +1328,7 @@ globus_l_gfs_remote_session_end(
         list = globus_list_rest(list))
     {
         node_info = (globus_l_gfs_remote_node_info_t *) 
-            globus_list_first(list);
+            globus_list_remove(&list, list);
                 
         result = globus_gfs_ipc_handle_release(node_info->ipc_handle);
         if(result != GLOBUS_SUCCESS)
