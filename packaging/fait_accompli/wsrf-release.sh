8cd0573d106011bc8a6f87387dca3d618628ac3b
bacon <>
2004-10-26 17:17:11 +0000

    Removing unused fait_accompli files Corrected GPT package name of gridftp_driver Took WS C Core out of RC generator Renamed gt2 test bundle to prews-test Included test bundles in RC

--- packaging/fait_accompli/wsrf-release.sh
+++ packaging/fait_accompli/wsrf-release.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-VERSION="3.9.3rc1"
+VERSION="3.9.3rc2"
 TAGOPTS="-t2=HEAD -t3=HEAD"
 INSTALLER="gt${VERSION}-wsrf-source-installer"
 GPT="fait_accompli/gpt-3.2autotools2004-src.tar.gz"
 
 mkdir $INSTALLER
 
-./make-packages.pl $TAGOPTS --bundles=gt2-threaded,gt2-unthreaded,globus-rls-server,gt4-java-ws-core,gt4-mds,gt4-delegation,gt4-rft,gt4-gram,gt4-cas,gt4-c-ws-core --version=$VERSION --installer=install-wsrf $@
+./make-packages.pl $TAGOPTS --bundles=gt2-threaded,gt2-unthreaded,globus-rls-server,gt4-java-ws-core,gt4-mds,gt4-delegation,gt4-rft,gt4-gram,gt4-cas --version=$VERSION --installer=install-wsrf $@
 
 if [ $? -ne 0 ]; then
    echo "ERROR"
@@ -23,21 +23,20 @@ cp $GPT $INSTALLER
 # make the dir where the GRAM scheduler bundles will go
 mkdir $INSTALLER/schedulers
 
-# build the PBS bundle
+# make the scheduler bundles
 rm -fr *-output
-./make-packages.pl -n --bundles=gt4-gram-pbs --version=$VERSION
+./make-packages.pl -n --bundles=gt4-gram-pbs,gt4-gram-condor,gt4-gram-lsf --version=$VERSION
 cp bundle-output/*.tar.gz  $INSTALLER/schedulers
 
-# build the Condor bundle
+# copy in test bundles
+mkdir $INSTALLER/tests
 rm -fr *-output
-./make-packages.pl -n --bundles=gt4-gram-condor --version=$VERSION
-cp bundle-output/*.tar.gz  $INSTALLER/schedulers
-
-# build the LSF bundle
-rm -fr *-output
-./make-packages.pl -n --bundles=gt4-gram-lsf --version=$VERSION
-cp bundle-output/*.tar.gz  $INSTALLER/schedulers
+./make-packages.pl -n --bundles=prews-test,gt4-java-ws-core-test,gt4-mds-test --version=$VERSION
+cp bundle-output/*.tar.gz $INSTALLER/tests
 
 if [ -d contrib ]; then
     cp -Rp contrib $INSTALLER
 fi
+
+cp fait_accompli/installer.INSTALL $INSTALLER/INSTALL
+cp fait_accompli/installer.README $INSTALLER/README
