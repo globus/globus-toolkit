9e384849bd2a8c431d7d4bb170ce10805f163b17
rynge <>
2005-01-03 17:41:17 +0000

    Possible fix for 2494 - need a FC3 box to fully test it

--- gsi/gaa_callbacks/gaa_simple/test/gaa_test_utils.h
+++ gsi/gaa_callbacks/gaa_simple/test/gaa_test_utils.h
@@ -5,6 +5,10 @@ extern gaa_status process_request(gaa_ptr gaa, gaa_sc_ptr sc, gaa_policy_ptr pol
 extern void process_print(gaa_ptr gaa, gaa_sc_ptr sc, gaa_policy_ptr *policy, char *outbuf, int outbsize);
 extern gaa_status process_inquire(gaa_ptr gaa, gaa_sc_ptr sc, gaa_policy_ptr *policy, char *outbuf, int outbsize);
 extern gaa_status process_clear(gaa_sc_ptr *sc);
-extern init_sc(gaa_ptr gaa, gaa_sc_ptr *sc, void *context);
+extern int init_sc(gaa_ptr gaa, gaa_sc_ptr *sc, void *context);
 extern gaa_status process_pull(gaa_ptr gaa, gaa_sc_ptr sc, char *out, int osize);
+
+extern gaa_status process_get_authz_id(gaa_ptr gaa,
+                                       char *outbuf,
+                                       int outbsize);
 
