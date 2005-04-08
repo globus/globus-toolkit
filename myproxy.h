9c7f77d718fe4aa1e21c2e69a6ce8136d7d46f15
jbasney <jbasney>
2005-04-08 02:52:00 +0000

    bump to v0.6.5, though we're not ready to release yet...

--- myproxy.h
+++ myproxy.h
@@ -13,8 +13,8 @@
 /* software version constants */
 #define MYPROXY_VERSION_MAJOR 0
 #define MYPROXY_VERSION_MINOR 6
-#define MYPROXY_VERSION_MICRO 4
-#define MYPROXY_VERSION_DATE "v0.6.4 4 Jan 2005"
+#define MYPROXY_VERSION_MICRO 5
+#define MYPROXY_VERSION_DATE "v0.6.5 X XXX 2005"
 
 #include "myproxy_constants.h"
 #include "myproxy_authorization.h"
