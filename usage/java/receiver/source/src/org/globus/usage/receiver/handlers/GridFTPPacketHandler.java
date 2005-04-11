8e3364c9e80fb88f1a1a5185d1da8e093b07ee27
jdicarlo <>
2005-04-11 07:51:35 +0000

    Fixed GridFTPPacketHandler to match change in class hierarchy of GFTPTextPacket

--- usage/java/receiver/source/src/org/globus/usage/receiver/handlers/GridFTPPacketHandler.java
+++ usage/java/receiver/source/src/org/globus/usage/receiver/handlers/GridFTPPacketHandler.java
@@ -25,28 +25,19 @@ public class GridFTPPacketHandler extends DefaultPacketHandler {
     }
 
     public UsageMonitorPacket instantiatePacket(CustomByteBuffer rawBytes) {
-	//we must inspect the byte buffer to decide whether this is
-	//GFTPMonitorPacket or GFTPTextPacket.
-	//It would have been better to assign different packet version codes
-	//to the two... but that requires changing the frozen code...
-	//for a temporary kluge, I search for the string HOSTNAME.
-	//But string search is an expensive call.
-	if (new String(rawBytes.array()).indexOf("HOSTNAME") != -1) {
-	    return new GFTPTextPacket();
-	} else {
-	    return new GFTPMonitorPacket();
-	}
+
+	return new GFTPTextPacket();
     }
    
     //uses DefaultPacketHandler's handlePacket().
 
     protected PreparedStatement makeSQLInsert(UsageMonitorPacket pack) throws SQLException{
-        if (!(pack instanceof GFTPMonitorPacket)) {
+        if (!(pack instanceof GFTPTextPacket)) {
             log.error("Something is seriously wrong: GridFTPPacketHandler got a packet which was not a GFTPMonitorPacket.");
             throw new SQLException("Can't happen.");
         }
 
-        GFTPMonitorPacket gmp = (GFTPMonitorPacket)pack;
+        GFTPTextPacket gmp = (GFTPTextPacket)pack;
         
         return gmp.toSQL(con, table);
     }
