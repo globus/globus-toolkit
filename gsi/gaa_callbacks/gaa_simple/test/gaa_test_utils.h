dea8daf1a42f999c0c8583f385bc19a8f8a19793
meder <>
2005-04-16 01:30:28 +0000

    license & copyright

--- gsi/gaa_callbacks/gaa_simple/test/gaa_test_utils.h
+++ gsi/gaa_callbacks/gaa_simple/test/gaa_test_utils.h
@@ -1,3 +1,14 @@
+/*
+ * Portions of this file Copyright 1999-2005 University of Chicago
+ * Portions of this file Copyright 1999-2005 The University of Southern California.
+ *
+ * This file or a portion of this file is licensed under the
+ * terms of the Globus Toolkit Public License, found at
+ * http://www.globus.org/toolkit/download/license.html.
+ * If you redistribute this file, with or without
+ * modifications, you must include this notice in the file.
+ */
+
 extern char *process_msg(gaa_ptr gaa, gaa_sc_ptr *sc, char *inbuf, char *outbuf, int outbsize, char **users, gaa_policy_ptr *policy);
 extern gaa_status process_assert(gaa_ptr gaa, gaa_sc_ptr sc, char **users, char *outbuf, int outbsize);
 extern gaa_status process_getpolicy(gaa_ptr gaa, gaa_policy_ptr *policy, char *outbuf, int outbsize);
