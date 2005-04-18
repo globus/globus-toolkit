882b9b061e2db1c1d22d2f8ee0dce63f7e92a6fc
mlink <>
2005-04-18 23:05:42 +0000

    copyrights

--- gridftp/client/test/globus_ftp_client_user_auth_test.c
+++ gridftp/client/test/globus_ftp_client_user_auth_test.c
@@ -1,4 +1,15 @@
 /*
+ * Portions of this file Copyright 1999-2005 University of Chicago
+ * Portions of this file Copyright 1999-2005 The University of Southern California.
+ *
+ * This file or a portion of this file is licensed under the
+ * terms of the Globus Toolkit Public License, found at
+ * http://www.globus.org/toolkit/download/license.html.
+ * If you redistribute this file, with or without
+ * modifications, you must include this notice in the file.
+ */
+
+/*
  * User-specified authorized subject.
  *
  * Let the user set the auth subject for the ftp client library. Make sure
