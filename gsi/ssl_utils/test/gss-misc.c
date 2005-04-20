54b00e8da288bd52ba6c0aa522929a34d91d9c7c
meder <>
2005-04-20 18:38:40 +0000

    Stuff thats in the source dist but not installed

--- gsi/ssl_utils/test/gss-misc.c
+++ gsi/ssl_utils/test/gss-misc.c
@@ -1,4 +1,15 @@
 /*
+ * Portions of this file Copyright 1999-2005 University of Chicago
+ * Portions of this file Copyright 1999-2005 The University of Southern California.
+ *
+ * This file or a portion of this file is licensed under the
+ * terms of the Globus Toolkit Public License, found at
+ * http://www.globus.org/toolkit/download/license.html.
+ * If you redistribute this file, with or without
+ * modifications, you must include this notice in the file.
+ */
+
+/*
  * Copyright 1994 by OpenVision Technologies, Inc.
  * 
  * Permission to use, copy, modify, distribute, and sell this software
