60f73092466d05531d9e8fac59b2b779dcb2339a
jdicarlo <>
2005-04-20 20:28:13 +0000

    Made C Core handler catch packets with either version number 1 or 4.

--- usage/java/receiver/source/src/org/globus/usage/receiver/handlers/CCorePacketHandler.java
+++ usage/java/receiver/source/src/org/globus/usage/receiver/handlers/CCorePacketHandler.java
@@ -31,7 +31,10 @@ public class CCorePacketHandler extends DefaultPacketHandler {
     }
 
     public boolean doCodesMatch(short componentCode, short versionCode) {
-        return (componentCode == 4 && versionCode == 1);
+	/*1 is the correct version code, but there are many installations
+	out there that erroneously send packets with version code 4, and
+	we want to catch both:*/
+        return (componentCode == 4 && (versionCode == 1 || versionCode == 4));
     }
 
     public UsageMonitorPacket instantiatePacket(CustomByteBuffer rawBytes) {
