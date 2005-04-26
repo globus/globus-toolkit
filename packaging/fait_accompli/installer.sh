f62df3afb2364b895eaa61bca8953adff70acf8b
bacon <>
2005-04-26 19:01:18 +0000

    Binary installer fixups. Breaking patch/ step to make Mats unhappy.

--- packaging/fait_accompli/installer.sh
+++ packaging/fait_accompli/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=4.0.0rc1
+VERSION=4.0.0rc4
 INSTALLER=gt$VERSION-all-source-installer
 AUTOTOOLS=source-trees/autotools/autotools/autoconf-2.59/config
 GPT=gpt-3.2autotools2004-src.tar.gz
@@ -8,19 +8,19 @@ GPT=gpt-3.2autotools2004-src.tar.gz
 TARFILES="gsi_openssh-3.5-src.tar.gz gsi_openssh_setup-3.5-src.tar.gz myproxy-1.17.tar.gz"
 
 echo Making configure/make installer
-./make-packages.pl --trees=autotools,gt2,gt4 --skippackage --skipbundle $@
+./make-packages.pl --trees=autotools --skippackage --skipbundle $@
+#
+#if [ -d patches ]; then
+   #echo
+   #echo "Patching..."
+   #for PATCH in `ls patches 2>/dev/null`; do
+       #echo "Applying $PATCH"
+       #cat patches/$PATCH | patch -p0
+   #done
+   #echo
+#fi
 
-if [ -d patches ]; then
-   echo
-   echo "Patching..."
-   for PATCH in `ls patches 2>/dev/null`; do
-       echo "Applying $PATCH"
-       cat patches/$PATCH | patch -p0
-   done
-   echo
-fi
-
-./make-packages.pl --bundles=globus-resource-management-server,globus-resource-management-client,globus-resource-management-sdk,globus-data-management-server,globus-data-management-client,globus-data-management-sdk,globus-information-services-server,globus-information-services-client,globus-information-services-sdk,globus-rls-server,gt4-java-ws-core,gt4-java-admin,gt4-mds,gt4-delegation,gt4-rft,gt4-gram,gt4-gram-pbs,gt4-gram-condor,gt4-gram-lsf,gt4-cas,gt4-c-ws-core,prews-test,globus-internationalization,gt4-java-ws-core-test,gt4-c-ws-core-test,gt4-mds-test,gt4-gram-test,gt4-cas-delegation-test,gt4-rft-test,gt4-webmds,gt4-webmds-test,globus-gsi,gt4-replicator --list-packages --deps --deporder -n $@ | tee farfleblatt
+./make-packages.pl --bundles=globus-resource-management-server,globus-resource-management-client,globus-resource-management-sdk,globus-data-management-server,globus-data-management-client,globus-data-management-sdk,globus-information-services-server,globus-information-services-client,globus-information-services-sdk,globus-rls-server,gt4-java-ws-core,gt4-java-admin,gt4-mds,gt4-delegation,gt4-rft,gt4-gram,gt4-gram-pbs,gt4-gram-condor,gt4-gram-lsf,gt4-cas,gt4-c-ws-core,prews-test,globus-internationalization,gt4-java-ws-core-test,gt4-c-ws-core-test,gt4-mds-test,gt4-gram-test,gt4-cas-delegation-test,gt4-rft-test,gt4-webmds,gt4-webmds-test,globus-gsi,gt4-replicator --list-packages --deps --deporder $@ | tee farfleblatt
 
 
 if [ $? -ne 0 ]; then
