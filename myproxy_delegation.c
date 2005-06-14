0c5aa628639be33678b93615913f23aa33e194c6
jbasney <jbasney>
2005-06-14 19:26:04 +0000

    don't fprintf(stderr) in here. use verror().

--- myproxy_delegation.c
+++ myproxy_delegation.c
@@ -61,7 +61,6 @@ int myproxy_get_delegation(
     /* Set up client socket attributes */
     if (socket_attrs->gsi_socket == NULL) {
 	if (myproxy_init_client(socket_attrs) < 0) {
-	    fprintf(stderr, "Error: %s\n", verror_get_string());
 	    return(1);
 	}
     }
@@ -72,22 +71,17 @@ int myproxy_get_delegation(
 
      /* Authenticate client to server */
     if (myproxy_authenticate_init(socket_attrs, NULL) < 0) {
-        fprintf(stderr, "Error: %s: %s\n", 
-		socket_attrs->pshost, verror_get_string());
         return(1);
     }
 
     /* Serialize client request object */
     requestlen = myproxy_serialize_request_ex(client_request, &request_buffer);
     if (requestlen < 0) {
-        fprintf(stderr, "Error in myproxy_serialize_request_ex():\n");
         return(1);
     }
 
     /* Send request to the myproxy-server */
     if (myproxy_send(socket_attrs, request_buffer, requestlen) < 0) {
-        fprintf(stderr, "Error in myproxy_send_request(): %s\n", 
-		verror_get_string());
         return(1);
     }
     free(request_buffer);
@@ -96,21 +90,17 @@ int myproxy_get_delegation(
     /* Continue unless the response is not OK */
     if (myproxy_recv_response_ex(socket_attrs, server_response,
 				 client_request) != 0) {
-	fprintf(stderr, "%s\n", verror_get_string());
 	return(1);
     }
     
     /* Accept delegated credentials from server */
     if (myproxy_accept_delegation(socket_attrs, delegfile, sizeof(delegfile),
 				  NULL) < 0) {
-        fprintf(stderr, "Error in myproxy_accept_delegation(): %s\n", 
-		verror_get_string());
 	return(1);
     }      
 
 #if 0 /* response was lost in myproxy_accept_delegation() */
     if (myproxy_recv_response(socket_attrs, server_response) < 0) {
-       fprintf(stderr, "%s\n", verror_get_string());
        return(1);
     }
 #endif
@@ -118,7 +108,7 @@ int myproxy_get_delegation(
     /* move delegfile to outputfile if specified */
     if (outfile != NULL) {
         if (copy_file(delegfile, outfile, 0600) < 0) {
-	    fprintf(stderr, "Error creating file: %s\n", outfile);
+	    verror_put_string("Error creating file: %s\n", outfile);
 	    return(1);
 	}
 	ssl_proxy_file_destroy(delegfile);
