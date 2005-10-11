d54bf9b1f06b04dc1b2e8f9e26eb37466bdb5362
jbasney <>
2005-10-11 15:49:21 +0000

    use struct fd_set instead of fd_set to make splint happy

--- myproxy/gsi_socket.c
+++ myproxy/gsi_socket.c
@@ -180,7 +180,7 @@ read_token(const int sock,
 	unsigned char header[5];
 	char *bufferp;
 	int data_len, buffer_len;
-	fd_set rfds;
+	struct fd_set rfds;
 	struct timeval tv = { 0 };
 
 	if (read_all(sock, (char *)header, sizeof(header)) < 0) {
