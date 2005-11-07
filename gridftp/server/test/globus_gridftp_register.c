867b533a9b9346c8a6b8b8e07e8d737b8f9b1407
bresnaha <>
2005-11-07 02:47:38 +0000

    *** empty log message ***

--- gridftp/server/test/globus_gridftp_register.c
+++ gridftp/server/test/globus_gridftp_register.c
@@ -40,12 +40,13 @@ main(
     int                                     argc,
     char **                                 argv)
 {
+    int                                     arg_i = 0;
     int                                     c_count;
     globus_xio_driver_t                     tcp_driver;
     globus_xio_driver_t                     gsi_driver;
     globus_xio_stack_t                      stack;
     globus_xio_handle_t                     xio_handle;
-    char *                                  c_port;
+    char *                                  cs;
     char *                                  repo;
     char *                                  registry_cs;
     globus_result_t                         res;
@@ -55,7 +56,7 @@ main(
     char *                                  local_contact;
     char *                                  tmp_ptr;
 
-    if(argc < 3)
+    if(argc < 4)
     {
         help();
         return 1;
@@ -68,31 +69,31 @@ main(
     test_res(res);
     res = globus_xio_stack_push_driver(stack, tcp_driver);
     test_res(res);
-/*
-    res = globus_xio_driver_load("gsi", &gsi_driver);
-    test_res(res);
-  */  
-/*
-    res = globus_xio_stack_push_driver(stack, gsi_driver);
-    test_res(res);
-*/
-    registry_cs = argv[1];
-    c_port = argv[2];
 
-    if(argc < 5)
+    arg_i = 1;
+    if(strcmp(argv[arg_i], "-s") == 0)
+    {
+        arg_i++;
+        res = globus_xio_driver_load("gsi", &gsi_driver);
+        test_res(res);
+        res = globus_xio_stack_push_driver(stack, gsi_driver);
+        test_res(res);
+    }
+    registry_cs = argv[arg_i];
+    arg_i++;
+    cs = argv[arg_i];
+    arg_i++;
+    c_count = atoi(argv[arg_i]);
+    arg_i++;
+
+    if(arg_i == argc)
     {
         repo = "";
     }
-    else if(argc < 4)
-    {
-        help();
-        exit(1);
-    }
     else
     {
-        repo = argv[4];
+        repo = argv[arg_i];
     }
-    c_count = atoi(argv[3]);
     res = globus_xio_handle_create(&xio_handle, stack);
     test_res(res);
 
@@ -109,13 +110,14 @@ main(
     tmp_ptr = strchr(local_contact, ':');
     assert(tmp_ptr != NULL);
     *tmp_ptr = '\0';
+    printf("%s\n", local_contact); /* write out the actual IP we will use */
 
     memset(msg, '\0', 256);
     len = strlen(repo);
     *msg = (char)c_count;
     memcpy(&msg[1], repo, len);
     msg[len+1] = '\0';
-    sprintf(&msg[len+2], "%s:%s", local_contact, c_port);
+    sprintf(&msg[len+2], "%s:%s", local_contact, cs);
     printf("registering\n  repo=[%s]\n  server contact=[%s]\n  max=[%d]\n",
         repo, &msg[len+2], c_count);
     res = globus_xio_write(xio_handle, msg, 256, 256, &nbytes, NULL);
