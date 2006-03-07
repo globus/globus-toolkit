06aee44fdfbe71b22a5c6a70b111e1dd87fce395
gawor <>
2006-03-07 05:07:17 +0000

    pull db info from file

--- usage/java/reports/source/src/org/globus/usage/report/Database.java
+++ usage/java/reports/source/src/org/globus/usage/report/Database.java
@@ -1,13 +1,36 @@
 package org.globus.usage.report;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Database {
 
     private String driverClass;
     private String url;
 
     public Database() throws Exception {
-        this.driverClass = "org.postgresql.Driver";
-        this.url = "jdbc:postgresql://pgsql.mcs.anl.gov:5432/usagestats?user=allcock&password=bigio";
+        this("etc/globus_usage_reports/db.properties");
+    }
+
+    public Database(String props) throws Exception {
+
+        InputStream in = 
+            getClass().getClassLoader().getResourceAsStream(props);
+        if (in == null) {
+            throw new Exception("Unable to load resource");
+        }
+        
+        Properties properties = new Properties();
+        try {
+            properties.load(in);
+        } finally {
+            try {
+                in.close();
+            } catch (Exception e) {}
+        }
+
+        this.driverClass = properties.getProperty("db.driver");
+        this.url = properties.getProperty("db.url");
 
         Class.forName(this.driverClass);
     }
