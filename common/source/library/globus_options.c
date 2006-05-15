0a9c7902e270c29a4edff0911c5d1e0924fb25be
bresnaha <>
2006-05-15 20:03:12 +0000

    changed how globus_options works and pushed the changes through to clients that use it.

--- common/source/library/globus_options.c
+++ common/source/library/globus_options.c
@@ -18,23 +18,27 @@
 
 #define HELP_LINE_LENGTH 70
 
+typedef struct globus_l_options_table_s
+{
+    void *                              user_arg;
+    globus_options_entry_t *            table;
+    int                                 table_size;
+} globus_l_options_table_t;
+
 typedef struct globus_l_options_handle_s
 {
-    globus_options_unknown_callback_t    unknown_func;
-    void *                              user_arg;
-    globus_options_entry_t *             table;
-    int                                 table_size;
+    globus_options_unknown_callback_t   unknown_func;
+    globus_list_t *                     table_list;
+    void *                              unknown_arg;
 } globus_l_options_handle_t;
 
 globus_result_t
 globus_options_init(
-    globus_options_handle_t *            out_handle,
-    globus_options_unknown_callback_t    unknown_func,
-    void *                              user_arg,
-    globus_options_entry_t *             table)
+    globus_options_handle_t *           out_handle,
+    globus_options_unknown_callback_t   unknown_func,
+    void *                              unknown_arg)
 {
-    int                                 i;
-    globus_l_options_handle_t *          handle;
+    globus_l_options_handle_t *         handle;
 
     handle = (globus_l_options_handle_t *)
         globus_calloc(1, sizeof(globus_l_options_handle_t));
@@ -42,15 +46,35 @@ globus_options_init(
     {
     }
     handle->unknown_func = unknown_func;
-    handle->user_arg = user_arg;
-    handle->table = table;
-
-    for(i = 0; handle->table[i].func != NULL; i++)
-    {
-    }
-    handle->table_size = i;
+    handle->unknown_arg = unknown_arg;
+    handle->table_list = NULL;
 
     *out_handle = handle;
+
+    return GLOBUS_SUCCESS;
+}
+
+globus_result_t
+globus_options_add_table(
+    globus_options_handle_t             handle,
+    globus_options_entry_t *            in_table,
+    void *                              user_arg)
+{
+    int                                 i;
+    globus_l_options_table_t *          table;
+
+    table = (globus_l_options_table_t *)
+        globus_calloc(1, sizeof(globus_l_options_table_t));
+
+    table->user_arg = user_arg;
+    table->table = in_table;
+
+    for(i = 0; table->table[i].func != NULL; i++)
+    {
+    }
+    table->table_size = i;
+
+    globus_list_insert(&handle->table_list, table);
 
     return GLOBUS_SUCCESS;
 }
@@ -64,62 +88,71 @@ globus_options_destroy(
     return GLOBUS_SUCCESS;
 }
 
-static
 void
 globus_options_help(
     globus_options_handle_t             handle)
 {
+    globus_l_options_table_t *          table;
     char *                              ptr;
     int                                 i;
     int                                 length;
     int                                 ndx;
+    globus_list_t *                     list;
     char                                buf[HELP_LINE_LENGTH+8];
 
-    for(i = 0; i < handle->table_size; i++)
+    for(list = handle->table_list;
+        !globus_list_empty(list);
+        list = globus_list_rest(list))
     {
-        fprintf(stdout, "-%s | --%s ",
-            handle->table[i].short_opt, handle->table[i].opt_name);
-        if(handle->table[i].parms_desc != NULL)
+        table = (globus_l_options_table_t *) globus_list_first(list);
+
+        for(i = 0; i < table->table_size; i++)
         {
-            fprintf(stdout, "%s", handle->table[i].parms_desc);
-        }
-        fprintf(stdout, "\n");
-        if(handle->table[i].description != NULL)
-        {
-            ndx = 0;
-            do 
+            fprintf(stdout, "-%s | --%s ",
+                table->table[i].short_opt, table->table[i].opt_name);
+            if(table->table[i].parms_desc != NULL)
             {
-                length = strlen(&handle->table[i].description[ndx]);
-                if(length > HELP_LINE_LENGTH)
+                fprintf(stdout, "%s", table->table[i].parms_desc);
+            }
+            fprintf(stdout, "\n");
+            if(table->table[i].description != NULL)
+            {
+                ndx = 0;
+                do 
                 {
-                    ptr = &handle->table[i].description[ndx] + HELP_LINE_LENGTH;
-                    while(ptr > &handle->table[i].description[ndx] &&
-                        *ptr != ' ')
+                    length = strlen(&table->table[i].description[ndx]);
+                    if(length > HELP_LINE_LENGTH)
                     {
-                        ptr--;
+                        ptr = &table->table[i].description[ndx] 
+                            + HELP_LINE_LENGTH;
+                        while(ptr > &table->table[i].description[ndx] &&
+                            *ptr != ' ')
+                        {
+                            ptr--;
+                        }
+                        if(ptr != &table->table[i].description[ndx])
+                        {
+                            length = ptr - &table->table[i].description[ndx];
+                        }
+                        else
+                        {
+                            length = HELP_LINE_LENGTH;
+                        }
                     }
-                    if(ptr != &handle->table[i].description[ndx])
-                    {
-                        length = ptr - &handle->table[i].description[ndx];
-                    }
-                    else
-                    {
-                        length = HELP_LINE_LENGTH;
-                    }
-                }
-                snprintf(buf,
-                    length+1,"%s",&handle->table[i].description[ndx]);
-                buf[length+1] = '\0';
-                fprintf(stderr, "    %s\n", buf);
-                ndx += length;
-            } while(length > 0);
+                    snprintf(buf,
+                        length+1,"%s",&table->table[i].description[ndx]);
+                    buf[length+1] = '\0';
+                    fprintf(stderr, "    %s\n", buf);
+                    ndx += length;
+                } while(length > 0);
+            }
         }
     }
 }
 
 globus_result_t
 globus_options_command_line_process(
-    globus_options_handle_t              handle,
+    globus_options_handle_t             handle,
     int                                 argc,
     char **                             argv)
 {
@@ -127,10 +160,12 @@ globus_options_command_line_process(
     globus_result_t                     res;
     int                                 i;
     int                                 j;
-    char *                              arg_parm;
+    char **                             arg_parm;
     int                                 used;
     char *                              current_arg;
     globus_bool_t                       found;
+    globus_l_options_table_t *          table;
+    globus_list_t *                     list;
     GlobusFuncName(globus_options_command_line_process);
 
     for(i = 1; i < argc; i++)
@@ -138,46 +173,75 @@ globus_options_command_line_process(
         current_arg = argv[i];
         found = GLOBUS_FALSE;
 
-        for(j = 0; j < handle->table_size && !found; j++)
+        for(list = handle->table_list;
+            !globus_list_empty(list);
+            list = globus_list_rest(list))
         {
-            found = GLOBUS_FALSE;
-            if(handle->table[j].short_opt != NULL &&
-                current_arg[0] == '-' &&
-                strcmp(&current_arg[1], handle->table[j].short_opt) == 0)
+            table = (globus_l_options_table_t *)globus_list_first(list);
+            for(j = 0; j < table->table_size && !found; j++)
             {
-                found = GLOBUS_TRUE;
-            }
-            else if(handle->table[j].opt_name != NULL &&
-                ((current_arg[0] == '-' && 
-                    strcmp(&current_arg[1], handle->table[j].opt_name) == 0) ||
-                 (current_arg[0] == '-' && current_arg[1] == '-' &&
-                    strcmp(&current_arg[2], handle->table[j].opt_name) == 0)))
-            {
-                found = GLOBUS_TRUE;
-            }
-
-            if(found)
-            {
-                func_argc = argc - i;
-                if(handle->table[j].arg_count == 0)
+                found = GLOBUS_FALSE;
+                if(table->table[j].short_opt != NULL &&
+                    current_arg[0] == '-' &&
+                    strcmp(&current_arg[1], table->table[j].short_opt) == 0)
                 {
-                    arg_parm = NULL;
+                    found = GLOBUS_TRUE;
                 }
-                else
+                else if(table->table[j].opt_name != NULL &&
+                    ((current_arg[0] == '-' && 
+                        strcmp(&current_arg[1], 
+                            table->table[j].opt_name) == 0) ||
+                     (current_arg[0] == '-' && current_arg[1] == '-' &&
+                    strcmp(&current_arg[2], table->table[j].opt_name) == 0)))
                 {
-                    arg_parm = argv[i+1];
+                    found = GLOBUS_TRUE;
                 }
 
-                if(func_argc > handle->table[j].arg_count)
+                if(found)
                 {
-                    res = handle->table[j].func(
-                        handle->table[j].opt_name,
-                        arg_parm,
-                        handle->user_arg,
-                        &used);
-                    if(used > 1 || used < 0)
+                    func_argc = argc - i;
+                    if(table->table[j].arg_count == 0)
                     {
-                        res = globus_error_put(globus_error_construct_error(
+                        arg_parm = NULL;
+                    }
+                    else
+                    {
+                        arg_parm = &argv[i+1];
+                    }
+
+                    if(func_argc > table->table[j].arg_count)
+                    {
+                        /* intialized the in/out to the max */
+                        used = table->table[j].arg_count;
+
+                        res = table->table[j].func(
+                            handle,
+                            table->table[j].opt_name,
+                            arg_parm,
+                            table->user_arg,
+                            &used);
+                        if(used > 1 || used < 0)
+                        {
+                            res=globus_error_put(globus_error_construct_error(
+                                NULL,
+                                NULL,
+                                GLOBUS_OPTIONS_NOT_ENOUGH_ARGS,
+                                __FILE__,
+                                _globus_func_name,
+                                __LINE__,
+                                "Not enough parameters for: %s",
+                                current_arg));
+                            return res;
+                        }
+                        if(res != GLOBUS_SUCCESS)
+                        {
+                            return res;
+                        }
+                        i += used;
+                    }
+                    else
+                    {
+                        return globus_error_put(globus_error_construct_error(
                             NULL,
                             NULL,
                             GLOBUS_OPTIONS_NOT_ENOUGH_ARGS,
@@ -186,49 +250,17 @@ globus_options_command_line_process(
                             __LINE__,
                             "Not enough parameters for: %s",
                             current_arg));
-                        return res;
                     }
-                    if(res != GLOBUS_SUCCESS)
-                    {
-                        return res;
-                    }
-                    i += used;
-                }
-                else
-                {
-                    return globus_error_put(globus_error_construct_error(
-                        NULL,
-                        NULL,
-                        GLOBUS_OPTIONS_NOT_ENOUGH_ARGS,
-                        __FILE__,
-                        _globus_func_name,
-                        __LINE__,
-                        "Not enough parameters for: %s",
-                        current_arg));
                 }
             }
         }
         if(!found)
         {
-            if(strcmp(argv[i],"--help") == 0 ||
-                strcmp(argv[i],"-help") == 0 ||
-                strcmp(argv[i],"-h") == 0   ||
-                strcmp(argv[i],"-?") == 0)
+            /* call unknow handler if not found and it starts with a -,
+                this is so trailing args dont call unknown handler */
+            if(handle->unknown_func != NULL && *argv[i] == '-')
             {
-                globus_options_help(handle);
-                return globus_error_put(globus_error_construct_error(
-                    NULL,
-                    NULL,
-                    GLOBUS_OPTIONS_HELP,
-                    __FILE__,
-                    _globus_func_name,
-                    __LINE__,
-                    "Program usage"));
-            }
-            /* invalid handler */
-            else if(handle->unknown_func != NULL)
-            {
-                res = handle->unknown_func(argv[i], handle->user_arg);
+                res = handle->unknown_func(argv[i], handle->unknown_arg);
                 if(res != GLOBUS_SUCCESS)
                 {
                     return res;
@@ -244,30 +276,42 @@ globus_result_t
 globus_options_env_process(
     globus_options_handle_t             handle)
 {
+    globus_list_t *                     list;
+    globus_l_options_table_t *          table;
     globus_result_t                     res;
     int                                 i;
     char *                              tmp_str;
     int                                 used;
 
-    for(i = 0; i < handle->table_size; i++)
+    for(list = handle->table_list;
+        !globus_list_empty(list);
+        list = globus_list_rest(list))
     {
-        if(handle->table[i].env != NULL)
+        table = (globus_l_options_table_t *) globus_list_first(list);
+
+        for(i = 0; i < table->table_size; i++)
         {
-            tmp_str = globus_libc_getenv(handle->table[i].env);
-            if(tmp_str != NULL)
+            if(table->table[i].env != NULL)
             {
-                if(*tmp_str == '\0')
+                tmp_str = globus_libc_getenv(table->table[i].env);
+                /* TODO: parse into mulitple args */
+                if(tmp_str != NULL)
                 {
-                    tmp_str = NULL;
-                }
-                res = handle->table[i].func(
-                    handle->table[i].opt_name,
-                    tmp_str,
-                    handle->user_arg,
-                    &used);
-                if(res != GLOBUS_SUCCESS)
-                {
-                    return res;
+                    if(*tmp_str == '\0')
+                    {
+                        tmp_str = NULL;
+                    }
+                    used = 1;
+                    res = table->table[i].func(
+                        handle,
+                        table->table[i].opt_name,
+                        &tmp_str,
+                        table->user_arg,
+                        &used);
+                    if(res != GLOBUS_SUCCESS)
+                    {
+                        return res;
+                    }
                 }
             }
         }
@@ -279,9 +323,11 @@ globus_options_env_process(
 
 globus_result_t
 globus_options_file_process(
-    globus_options_handle_t              handle,
+    globus_options_handle_t             handle,
     char *                              filename)
 {
+    globus_list_t *                     list;
+    globus_l_options_table_t *          table;
     char *                              opt_arg;
     int                                 used;
     globus_result_t                     res;
@@ -378,18 +424,24 @@ globus_options_file_process(
             goto error_parse;
         }
 
-        for(i = 0; i < handle->table_size; i++)
+        for(list = handle->table_list;
+            !globus_list_empty(list);
+            list = globus_list_rest(list))
         {
-            if(strcmp(file_option, handle->table[i].opt_name) == 0)
+            for(i = 0; i < table->table_size; i++)
             {
-                res = handle->table[i].func(
-                    handle->table[i].opt_name,
-                    opt_arg,
-                    handle->user_arg,
-                    &used);
-                if(res != GLOBUS_SUCCESS)
+                if(strcmp(file_option, table->table[i].opt_name) == 0)
                 {
-                    return res;
+                    res = table->table[i].func(
+                        handle,
+                        table->table[i].opt_name,
+                        &opt_arg,
+                        table->user_arg,
+                        &used);
+                    if(res != GLOBUS_SUCCESS)
+                    {
+                        return res;
+                    }
                 }
             }
         }
--- common/source/library/globus_options.h
+++ common/source/library/globus_options.h
@@ -28,11 +28,14 @@ enum
     GLOBUS_OPTIONS_UNKNOWN
 };
 
+typedef struct globus_l_options_handle_s * globus_options_handle_t;
+
 typedef
 globus_result_t
 (*globus_options_callback_t)(
+    globus_options_handle_t             opts_handle,
     char *                              cmd,
-    char *                              parm,
+    char **                             parm,
     void *                              arg,
     int *                               out_parms_used);
 
@@ -53,14 +56,19 @@ typedef struct globus_options_entry_s
     globus_options_callback_t           func;
 } globus_options_entry_t;
 
-typedef struct globus_l_options_handle_s * globus_options_handle_t;
+#define  GLOBUS_OPTIONS_END {NULL, NULL, NULL, NULL, NULL, 0, NULL}
 
 globus_result_t
 globus_options_init(
     globus_options_handle_t *           out_handle,
     globus_options_unknown_callback_t   unknown_func,
-    void *                              user_arg,
-    globus_options_entry_t *            table);
+    void *                              unknown_arg);
+
+globus_result_t
+globus_options_add_table(
+    globus_options_handle_t             handle,
+    globus_options_entry_t *            table,
+    void *                              user_arg);
 
 globus_result_t
 globus_options_destroy(
@@ -80,5 +88,9 @@ globus_result_t
 globus_options_file_process(
     globus_options_handle_t             handle,
     char *                              filename);
+
+void
+globus_options_help(
+    globus_options_handle_t             handle);
 
 #endif
