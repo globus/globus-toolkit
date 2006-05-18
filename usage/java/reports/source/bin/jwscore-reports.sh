22e6b599d0d86f5190314df94b811d0a2f7c9741
gawor <>
2006-05-18 21:41:46 +0000

    simplified things

--- usage/java/reports/source/bin/jwscore-reports.sh
+++ usage/java/reports/source/bin/jwscore-reports.sh
@@ -1,11 +1,44 @@
 #! /bin/sh
 
+runReport() {
+
+  REPORT_NAME=$1
+  REPORT_OUTPUT=$2
+  ANT_TARGET=$3
+  PLOT_FILES=$4  
+
+  shift
+  shift
+  shift
+  shift
+
+  OUTPUT=$PWD/$REPORT_OUTPUT
+
+  $GLOBUS_LOCATION/bin/$REPORT_NAME "$@" > $OUTPUT
+  if [ $? != 0 ]; then
+    echo "Error: Failed to generate the report"
+    exit 2
+  fi
+
+  ant -f $GLOBUS_LOCATION/etc/globus_usage_reports/reports.xml $ANT_TARGET -Din.xml=$OUTPUT
+  if [ $? != 0 ]; then
+    echo "Error: Failed to generate gnuplot files"
+    exit 3
+  fi
+
+  gnuplot $PLOT_FILES
+  if [ $? != 0 ]; then
+    echo "Error: Failed to generate report graphs"
+    exit 3
+  fi
+}
+
+### MAIN ###
+
 if [ ! -d "$GLOBUS_LOCATION" ] ; then
   echo "Error: GLOBUS_LOCATION invalid or not set: $GLOBUS_LOCATION" 1>&2
   exit 1
 fi
-
-. $GLOBUS_LOCATION/share/globus_usage_reports/common.sh
 
 REPORT_TYPE=$1
 shift
