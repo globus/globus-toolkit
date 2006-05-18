6ee92ecfbcc48f5716a3ea00c1cc82457111aec7
gawor <>
2006-05-18 21:29:53 +0000

    diff approach

---
+++ usage/java/reports/source/bin/jwscore-reports.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+if [ ! -d "$GLOBUS_LOCATION" ] ; then
+  echo "Error: GLOBUS_LOCATION invalid or not set: $GLOBUS_LOCATION" 1>&2
+  exit 1
+fi
+
+. $GLOBUS_LOCATION/share/globus_usage_reports/common.sh
+
+REPORT_TYPE=$1
+shift
+
+case $REPORT_TYPE in
+uptime)
+        runReport "jwscore-container-up-report" "uptime.xml" "gnuplot-uptime" "uptime.gnuplot" "$@"
+        ;;
+uptime2)
+        runReport "jwscore-container-up-2-report" "uptime.xml" "gnuplot-uptime" "uptime.gnuplot" "$@"
+        ;;
+longuptime)
+        runReport "jwscore-container-long-up-report" "longuptime.xml" "gnuplot-long-uptime" "longuptime.gnuplot" "$@"
+        ;;
+longuptime2)
+        runReport "jwscore-container-long-up-2-report" "longuptime.xml" "gnuplot-long-uptime" "longuptime.gnuplot" "$@"
+        ;;
+event)
+        runReport "jwscore-container-event-report" "event.xml" "gnuplot-event" "event.gnuplot" "$@"
+        ;;
+*)
+    echo "Unknown report type: $REPORT_TYPE";
+    exit 1
+    ;;
+esac
