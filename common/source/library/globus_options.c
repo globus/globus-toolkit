64d6279b38b2307c34b664bc9871eb0979ceb921
bester <>
2006-05-19 20:52:17 +0000

    Added globus_module_activate_array() for multiple activations with unrolling in case of a failure Globus options:     Compacted -help output a bit, alphabetized options     Changed unknown handler to be called with array of all arguments after last option

--- common/source/library/globus_options.c
+++ common/source/library/globus_options.c
@@ -16,7 +16,8 @@
 
 #include "globus_common.h"
 
-#define HELP_LINE_LENGTH 70
+#define HELP_LINE_LENGTH 78
+#define HELP_LINE_COLUMN 29
 
 typedef struct globus_l_options_table_s
 {
@@ -88,6 +89,45 @@ globus_options_destroy(
     return GLOBUS_SUCCESS;
 }
 
+static
+int
+globus_l_alphabetize_list(
+    void *                              low_datum,
+    void *                              high_datum,
+    void *                              arg)
+{
+    globus_options_entry_t *            e1 = low_datum;
+    globus_options_entry_t *            e2 = high_datum;
+
+    if (e1->short_opt && !e2->short_opt)
+    {
+        return GLOBUS_TRUE;
+    }
+    else if ((!e1->short_opt) && e2->short_opt)
+    {
+        return GLOBUS_FALSE;
+    }
+    else if ((!e1->short_opt) && (!e2->short_opt))
+    {
+        if (globus_libc_strncasecmp(e1->short_opt, e2->short_opt, strlen(e1->short_opt)) <= 0)
+        {
+            return GLOBUS_TRUE;
+        }
+        else
+        {
+            return GLOBUS_FALSE;
+        }
+    }
+    else if (globus_libc_strncasecmp(e1->short_opt, e2->short_opt, strlen(e1->short_opt)) <= 0)
+    {
+        return GLOBUS_TRUE;
+    }
+    else
+    {
+        return GLOBUS_FALSE;
+    }
+}
+
 void
 globus_options_help(
     globus_options_handle_t             handle)
@@ -98,7 +138,10 @@ globus_options_help(
     int                                 length;
     int                                 ndx;
     globus_list_t *                     list;
+    int                                 skip;
     char                                buf[HELP_LINE_LENGTH+8];
+    globus_list_t *                     alphabetized_options = NULL;
+    globus_options_entry_t *            entry;
 
     for(list = handle->table_list;
         !globus_list_empty(list);
@@ -108,46 +151,79 @@ globus_options_help(
 
         for(i = 0; i < table->table_size; i++)
         {
-            fprintf(stdout, "-%s | --%s ",
-                table->table[i].short_opt, table->table[i].opt_name);
-            if(table->table[i].parms_desc != NULL)
-            {
-                fprintf(stdout, "%s", table->table[i].parms_desc);
-            }
-            fprintf(stdout, "\n");
-            if(table->table[i].description != NULL)
-            {
-                ndx = 0;
-                do 
-                {
-                    length = strlen(&table->table[i].description[ndx]);
-                    if(length > HELP_LINE_LENGTH)
-                    {
-                        ptr = &table->table[i].description[ndx] 
-                            + HELP_LINE_LENGTH;
-                        while(ptr > &table->table[i].description[ndx] &&
-                            *ptr != ' ')
-                        {
-                            ptr--;
-                        }
-                        if(ptr != &table->table[i].description[ndx])
-                        {
-                            length = ptr - &table->table[i].description[ndx];
-                        }
-                        else
-                        {
-                            length = HELP_LINE_LENGTH;
-                        }
-                    }
-                    snprintf(buf,
-                        length+1,"%s",&table->table[i].description[ndx]);
-                    buf[length+1] = '\0';
-                    fprintf(stderr, "    %s\n", buf);
-                    ndx += length;
-                } while(length > 0);
-            }
+            globus_list_insert(&alphabetized_options, &table->table[i]);
         }
     }
+
+    alphabetized_options = globus_list_sort_destructive(
+            alphabetized_options, globus_l_alphabetize_list, NULL);
+
+    for(list = alphabetized_options;
+        !globus_list_empty(list);
+        list = globus_list_rest(list))
+    {
+        entry = globus_list_first(list);
+
+        /* Print out -short | --opt-name PARMS_DESC
+         * with any of -short, --opt-name, parms-desc being optional, and if
+         * not present, avoid adding inter-item punctuation.
+         */
+        fprintf(stdout, " %s%s%s%s%s%s%s%n",
+            entry->short_opt ? "-" : "",
+            entry->short_opt ? entry->short_opt : "",
+            entry->short_opt && entry->opt_name ? " | " : "",
+            entry->opt_name ? "--" : "",
+            entry->opt_name ? entry->opt_name : "",
+            entry->parms_desc ? " " : "",
+            entry->parms_desc ? entry->parms_desc : "",
+            &skip);
+        if (skip >= HELP_LINE_COLUMN - 1)
+        {
+            fprintf(stdout, "\n");
+            skip = 0;
+        }
+        if(entry->description != NULL)
+        {
+            ndx = 0;
+            do 
+            {
+                while (entry->description[ndx] == ' ')
+                {
+                    ndx++;
+                }
+                length = strlen(&entry->description[ndx]);
+                if(length > (HELP_LINE_LENGTH-HELP_LINE_COLUMN))
+                {
+                    ptr = &entry->description[ndx] 
+                        + HELP_LINE_LENGTH - HELP_LINE_COLUMN;
+                    while(ptr > &entry->description[ndx] &&
+                        *ptr != ' ')
+                    {
+                        ptr--;
+                    }
+                    if(ptr != &entry->description[ndx])
+                    {
+                        length = ptr - &entry->description[ndx];
+                    }
+                    else
+                    {
+                        length = HELP_LINE_LENGTH - HELP_LINE_COLUMN;
+                    }
+                }
+                if (length > 0)
+                {
+                    memset(buf, ' ', HELP_LINE_COLUMN - skip);
+                    snprintf(buf + HELP_LINE_COLUMN - skip,
+                        length+1,"%s",&entry->description[ndx]);
+                    buf[HELP_LINE_COLUMN-skip+length+1] = '\0';
+                    fprintf(stdout, "%s\n", buf);
+                    ndx += length;
+                    skip = 0;
+                }
+            } while(length > 0);
+        }
+    }
+    globus_list_free(alphabetized_options);
 }
 
 globus_result_t
@@ -220,7 +296,7 @@ globus_options_command_line_process(
                             arg_parm,
                             table->user_arg,
                             &used);
-                        if(used > 1 || used < 0)
+                        if(used < 0)
                         {
                             res=globus_error_put(globus_error_construct_error(
                                 NULL,
@@ -256,15 +332,23 @@ globus_options_command_line_process(
         }
         if(!found)
         {
-            /* call unknow handler if not found and it starts with a -,
-                this is so trailing args dont call unknown handler */
-            if(handle->unknown_func != NULL && *argv[i] == '-')
+            /* call unknown handler with an array of all remaining args */
+            if(handle->unknown_func != NULL)
             {
-                res = handle->unknown_func(argv[i], handle->unknown_arg);
-                if(res != GLOBUS_SUCCESS)
-                {
-                    return res;
-                }
+                res = handle->unknown_func(handle, handle->unknown_arg, argc-i, &argv[i]);
+                return res;
+            }
+            else
+            {
+                return globus_error_put(globus_error_construct_error(
+                    NULL,
+                    NULL,
+                    GLOBUS_OPTIONS_INVALID_PARAMETER,
+                    __FILE__,
+                    _globus_func_name,
+                    __LINE__,
+                    "Invalid parameter: %s",
+                    current_arg));
             }
         }
     }
