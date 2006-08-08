487f9ed890a3684b90a5e0348c51f25ba80e6cb3
gawor <>
2006-08-08 17:09:03 +0000

    updated reboot script, added script to check log file for errors

--- usage/java/receiver/source/bin/reboot-if-down.sh
+++ usage/java/receiver/source/bin/reboot-if-down.sh
@@ -6,17 +6,18 @@ if [ -z "$GLOBUS_LOCATION" ] ; then
 fi
 
 #Use ps to check whether receiver is running
-count=`ps -ef | grep receiver -c`
+count=`ps -ef | grep receiver | grep -c -v "grep receiver"`
 
 #if it isn't, start the receiver:
-if [ $count -le 1 ]
+if [ $count -lt 1 ]
 then
-    mv $GLOBUS_LOCATION/receiver.log $GLOBUS_LOCATION/receiver.log.`date +%Y%m%d-%H%M%S`
+    date=`date +%Y%m%d-%H%M%S`
+    mv $GLOBUS_LOCATION/receiver.log "$GLOBUS_LOCATION/receiver.log.$date"
     nohup $GLOBUS_LOCATION/bin/globus-usage-receiver &> $GLOBUS_LOCATION/receiver.log &
     #notify that this has occured
     time=`date`
     echo "An automatic check at $time found that the usage-stats was not running, and restarted it." 
     if [ -n "$1" ] ; then 
-      echo "Receiver was not running, and was restarted." | mail -s "Usage-Stats receiver restarted" $1
+      echo "${date}: Receiver was not running, and was restarted." | mail -s "Usage-Stats receiver restarted" $1
     fi
 fi
