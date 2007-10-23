2ac28225dea34685e132b877e3dafafc96fa61e8
bacon <>
2007-10-23 21:35:53 +0000

    committing dodgy mlink patch

--- packaging/fait_accompli/installer.sh
+++ packaging/fait_accompli/installer.sh
@@ -5,9 +5,8 @@ INSTALLER=gt$VERSION-all-source-installer
 AUTOTOOLS=source-trees/autotools/autoconf-2.59/config
 GPT=gpt*.tar.gz
 
-BUNDLES=globus-resource-management-server,globus-resource-management-client,globus-resource-management-sdk,globus-data-management-server,globus-data-management-client,globus-data-management-sdk,globus-rls-server,gt4-java-ws-core,gt4-java-admin,gt4-mds,gt4-delegation,gt4-rft,gt4-gram,gt4-gram-pbs,gt4-gram-condor,gt4-gram-lsf,gt4-cas,gt4-c-ws-core,prews-test,globus-internationalization,gt4-java-ws-core-test,gt4-c-ws-core-test,gt4-mds-test,gt4-gram-test,gt4-cas-delegation-test,gt4-rft-test,gt4-webmds,gt4-webmds-test,globus-gsi,gt4-replicator,gt4-wsrls,gsi_openssh_bundle,gridshib_bundle
-PACKAGES=globus_rendezvous,globus_xio_udt_ref_driver,globus_xio_skeleton_driver,globus_rls_client_java,myproxy,gridway
-
+BUNDLES=globus-resource-management-server,globus-resource-management-client,globus-resource-management-sdk,globus-data-management-server,globus-data-management-client,globus-data-management-sdk,globus-xio-extra-drivers,globus-rls-server,gt4-java-ws-core,gt4-java-admin,gt4-mds,gt4-delegation,gt4-rft,gt4-gram,gt4-gram-pbs,gt4-gram-condor,gt4-gram-lsf,gt4-cas,gt4-c-ws-core,prews-test,globus-internationalization,gt4-java-ws-core-test,gt4-c-ws-core-test,gt4-mds-test,gt4-gram-test,gt4-cas-delegation-test,gt4-rft-test,gt4-webmds,gt4-webmds-test,globus-gsi,gt4-replicator,gt4-wsrls,gsi_openssh_bundle,gridshib_bundle
+PACKAGES=globus_rendezvous,globus_rls_client_java,myproxy,gridway
 
 echo Making configure/make installer
 echo Step: Checking out and building autotools.
@@ -22,6 +21,17 @@ echo Step: Checking out source code.
 if [ $? -ne 0 ]; then
 	echo There was trouble checking out sources
 	exit 8
+fi
+
+if [ "X$BRANCH" != "X" ]; then
+    echo Step: Updating source with branch $BRANCH.
+    mkdir tmp-branch
+    cd tmp-branch
+    cvs -Q co -r $BRANCH all
+    cd ..
+    cp -R tmp-branch/* source-trees/
+    rm -rf tmp-branch
+    INSTALLER=gt$BRANCH-all-source-installer
 fi
 
 if [ -d patches ]; then
