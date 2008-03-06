589a64640bbd3b7bad1e6805b8e74ebaff5c2b96
cvs2git <>
2008-03-06 19:03:35 +0000

    This commit was manufactured by cvs2svn to create branch 'openssl_aes128_ctr'.
    Cherrypick from master 2008-03-06 19:03:34 UTC bester '.':
        gsi/callback/source/dirt.sh
        gsi/callback/source/library/globus_gsi_callback.c
        gsi/callback/source/pkgdata/pkg_data_src.gpt.in
        gsi/cert_utils/source/dirt.sh
        gsi/cert_utils/source/library/globus_gsi_cert_utils.h
        gsi/cert_utils/source/library/globus_gsi_cert_utils_constants.h
        gsi/cert_utils/source/pkgdata/pkg_data_src.gpt.in
        gsi/gssapi/test/Makefile.am
        gsi/gssapi/test/dirt.sh
        gsi/gssapi/test/gssapi-acquire-test.pl
        gsi/gssapi/test/gssapi-anonymous-test.pl
        gsi/gssapi/test/gssapi-context-test.pl
        gsi/gssapi/test/gssapi-delegation-compat-test.pl
        gsi/gssapi/test/gssapi-delegation-test.pl
        gsi/gssapi/test/gssapi-delegation-test.stdout
        gsi/gssapi/test/gssapi-expimp-test.pl
        gsi/gssapi/test/gssapi-group-test.pl
        gsi/gssapi/test/gssapi-import-context-test.pl
        gsi/gssapi/test/gssapi-inquire-sec-ctx-by-oid-test.pl
        gsi/gssapi/test/gssapi-limited-delegation-test.pl
        gsi/gssapi/test/gssapi-limited-delegation-test.stdout
        gsi/gssapi/test/gssapi-thread-test.pl
        gsi/gssapi/test/gssapi_delegation_test.c
        gsi/gssapi/test/pkgdata/pkg_data_src.gpt.in
        gsi/hostname_lookup/dirt.sh
        gsi/hostname_lookup/globus_hostname_lookup.c
        gsi/proxy/proxy_core/source/dirt.sh
        gsi/proxy/proxy_core/source/library/globus_gsi_proxy.c
        gsi/proxy/proxy_core/source/library/globus_gsi_proxy_handle.c
        gsi/proxy/proxy_core/source/pkgdata/pkg_data_src.gpt.in
        gsi/proxy/proxy_utils/source/dirt.sh
        gsi/proxy/proxy_utils/source/pkgdata/pkg_data_src.gpt.in
        gsi/proxy/proxy_utils/source/programs/grid_proxy_init.c
        gsi/proxy/proxy_utils/test/.cvsignore
        gsi/proxy/proxy_utils/test/Makefile.am
        gsi/proxy/proxy_utils/test/TESTS.pl
        gsi/proxy/proxy_utils/test/bootstrap
        gsi/proxy/proxy_utils/test/configure.in
        gsi/proxy/proxy_utils/test/dirt.sh
        gsi/proxy/proxy_utils/test/grid-proxy-utils-test.pl
        gsi/proxy/proxy_utils/test/pkgdata/.cvsignore
        gsi/proxy/proxy_utils/test/pkgdata/pkg_data_src.gpt.in
        packaging/etc/bundles
        packaging/etc/package-list
        xio/drivers/token_bucket/source/globus_xio_token_bucket_driver.c
        xio/drivers/token_bucket/source/globus_xio_token_bucket_driver.h
        xio/src/builtins/file/globus_xio_file_driver.c
        xio/src/builtins/file/globus_xio_file_driver.h
        xio/src/dirt.sh
        xio/src/globus_i_xio.h
        xio/src/globus_xio.h
        xio/src/globus_xio_attr.c
        xio/src/globus_xio_driver.c
        xio/src/globus_xio_driver.h
        xio/src/globus_xio_handle.c
        xio/src/globus_xio_types.h

---
+++ gsi/cert_utils/source/library/globus_gsi_cert_utils.h
@@ -0,0 +1,185 @@
+/*
+ * Copyright 1999-2006 University of Chicago
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GLOBUS_DONT_DOCUMENT_INTERNAL
+/**
+ * @file globus_gsi_cert_utils.h
+ * Globus GSI Cert Utils Library
+ * @author Sam Lang
+ * @author Sam Meder
+ *
+ * $RCSfile$
+ * $Revision$
+ * $Date$
+ */
+#endif
+
+#ifndef GLOBUS_GSI_CERT_UTILS_H
+#define GLOBUS_GSI_CERT_UTILS_H
+
+#ifndef EXTERN_C_BEGIN
+#    ifdef __cplusplus
+#        define EXTERN_C_BEGIN extern "C" {
+#        define EXTERN_C_END }
+#    else
+#        define EXTERN_C_BEGIN
+#        define EXTERN_C_END
+#    endif
+#endif
+
+EXTERN_C_BEGIN
+
+#include "globus_common.h"
+
+/**
+ * @mainpage Globus GSI Certificate Handling Utilities
+ *
+ * The Globus GSI Certificate Handling Utilities library. This library contains
+ * helper functions for dealing with certificates.
+ *
+ * - @ref globus_gsi_cert_utils_activation
+ * - @ref globus_gsi_cert_utils
+ * - @ref globus_gsi_cert_utils_constants
+ */
+
+/**
+ * @defgroup globus_gsi_cert_utils_activation Activation
+ *
+ * Globus GSI Cert Utils uses standard Globus module activation and
+ * deactivation.  Before any Globus GSI Cert Utils functions are called, the
+ * following function must be called:
+ *
+ * @code
+ *      globus_module_activate(GLOBUS_GSI_CERT_UTILS_MODULE)
+ * @endcode
+ *
+ *
+ * This function returns GLOBUS_SUCCESS if Globus GSI Credential was
+ * successfully initialized, and you are therefore allowed to
+ * subsequently call Globus GSI Cert Utils functions.  Otherwise, an error
+ * code is returned, and Globus GSI Cert Utils functions should not be
+ * subsequently called. This function may be called multiple times.
+ *
+ * To deactivate Globus GSI Cert Utils, the following function must be called:
+ *
+ * @code
+ *    globus_module_deactivate(GLOBUS_GSI_CERT_UTILS_MODULE)
+ * @endcode
+ *
+ * This function should be called once for each time Globus GSI Cert Utils
+ * was activated. 
+ *
+ */
+
+/**
+ * Module descriptor
+ * @ingroup globus_gsi_cert_utils_activation
+ * @hideinitializer
+ */
+#define GLOBUS_GSI_CERT_UTILS_MODULE    (&globus_i_gsi_cert_utils_module)
+
+extern 
+globus_module_descriptor_t              globus_i_gsi_cert_utils_module;
+
+#define _CUSL(s) globus_common_i18n_get_string(GLOBUS_GSI_CERT_UTILS_MODULE,\
+		s)
+/**
+ * @defgroup globus_gsi_cert_utils Cert Utils Functions
+ *
+ * A generic set of utility functions for manipulating 
+ * OpenSSL objects, such as X509 certificates.
+ */
+
+#ifndef DOXYGEN
+
+#include "openssl/x509.h"
+#include "openssl/asn1.h"
+#include "globus_error_openssl.h"
+#include "globus_gsi_cert_utils_constants.h"
+
+#define GLOBUS_GSI_CERT_UTILS_IS_PROXY(cert_type) \
+        ((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_PROXY_MASK) != 0)
+
+#define GLOBUS_GSI_CERT_UTILS_IS_RFC_PROXY(cert_type) \
+        (((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_PROXY_MASK) != 0) && \
+         ((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_RFC) != 0))
+
+#define GLOBUS_GSI_CERT_UTILS_IS_GSI_3_PROXY(cert_type) \
+        (((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_PROXY_MASK) != 0) && \
+         ((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_GSI_3) != 0))
+
+#define GLOBUS_GSI_CERT_UTILS_IS_GSI_2_PROXY(cert_type) \
+        (((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_PROXY_MASK) != 0) && \
+         ((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_GSI_2) != 0))
+
+#define GLOBUS_GSI_CERT_UTILS_IS_INDEPENDENT_PROXY(cert_type) \
+        ((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_INDEPENDENT_PROXY) != 0)
+
+#define GLOBUS_GSI_CERT_UTILS_IS_RESTRICTED_PROXY(cert_type) \
+        ((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_RESTRICTED_PROXY) != 0)
+
+#define GLOBUS_GSI_CERT_UTILS_IS_LIMITED_PROXY(cert_type) \
+        ((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_LIMITED_PROXY) != 0)
+
+#define GLOBUS_GSI_CERT_UTILS_IS_IMPERSONATION_PROXY(cert_type) \
+        ((cert_type & GLOBUS_GSI_CERT_UTILS_TYPE_IMPERSONATION_PROXY) != 0)
+
+globus_result_t
+globus_gsi_cert_utils_make_time(
+    ASN1_UTCTIME *                      ctm,
+    time_t *                            newtime);
+
+globus_result_t
+globus_gsi_cert_utils_get_base_name(
+    X509_NAME *                         subject,
+    STACK_OF(X509) *                    cert_chain);
+
+globus_result_t
+globus_gsi_cert_utils_get_cert_type(
+    X509 *                              cert,
+    globus_gsi_cert_utils_cert_type_t * type);
+
+globus_result_t
+globus_gsi_cert_utils_get_x509_name(
+    char *                              subject_string,
+    int                                 length,
+    X509_NAME *                         x509_name);
+
+int
+globus_i_gsi_cert_utils_dn_cmp(
+    const char *                        dn1,
+    const char *                        dn2);
+
+
+/* For backwards compatibility */
+
+#define globus_gsi_cert_utils_create_string \
+    globus_common_create_string
+
+#define globus_gsi_cert_utils_create_nstring \
+    globus_common_create_nstring
+
+#define globus_gsi_cert_utils_v_create_string \
+    globus_common_v_create_string
+
+#define globus_gsi_cert_utils_v_create_nstring \
+    globus_common_v_create_nstring
+
+#endif /* DOXYGEN */
+
+EXTERN_C_END
+
+#endif /* GLOBUS_GSI_CERT_UTILS_H */
---
+++ gsi/gssapi/test/gssapi-delegation-compat-test.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/env perl
+
+use strict;
+use POSIX;
+use Test;
+use Globus::Testing::Utilities;
+
+my $test_prog = 'gssapi-delegation-compat-test';
+
+my $diff = 'diff';
+my @tests;
+my @todo;
+
+Globus::Testing::Utilities::testcred_setup
+    || die "Unable to set up test credentials\n";
+
+sub basic_func
+{
+    my ($errors,$rc) = ("",0);
+   
+    $rc = system("./$test_prog 1>$test_prog.log.stdout 2>$test_prog.log.stderr") / 256;
+
+    if($rc != 0)
+    {
+        $errors .= "Test exited with $rc. ";
+    }
+
+    if($rc & 128)
+    {
+        $errors .= "\n# Core file generated.";
+    }
+   
+    open(EXPECTED, "<$test_prog.stdout") || die "Couldn't open $test_prog.stdout: $!\n";    
+    open(LOGGED, "<$test_prog.log.stdout") || die "Couldn't open $test_prog.log.stdout: $!\n";
+    $rc = 0;
+    while ( my $line = <EXPECTED> )
+    {
+	my $logged = <LOGGED>;
+	$rc++ unless ( $logged =~ /$line/ );
+    }
+				    
+    # $rc = system("$diff $test_prog.log.stdout $test_prog.stdout") / 256;
+   
+    if($rc != 0)
+    {
+        $errors .= "Test produced unexpected output, see $test_prog.log.stdout";
+    }
+
+    $rc = system("$diff $test_prog.log.stderr $test_prog.stderr") / 256;
+   
+    if($rc != 0)
+    {
+        $errors .= "Test produced unexpected output, see $test_prog.log.stderr";
+    }
+   
+    if($errors eq "")
+    {
+        ok('success', 'success');
+      
+        if( -e "$test_prog.log.stdout" )
+        {
+	    unlink("$test_prog.log.stdout");
+        }
+      
+        if( -e "$test_prog.log.stderr" )
+        {
+	    unlink("$test_prog.log.stderr");
+        } 
+    }
+    else
+    {
+        ok($errors, 'success');
+    }
+
+}
+
+sub sig_handler
+{
+    if( -e "$test_prog.log.stdout" )
+    {
+        unlink("$test_prog.log.stdout");
+    }
+
+    if( -e "$test_prog.log.stderr" )
+    {
+        unlink("$test_prog.log.stderr");
+    }
+}
+
+$SIG{'INT'}  = 'sig_handler';
+$SIG{'QUIT'} = 'sig_handler';
+$SIG{'KILL'} = 'sig_handler';
+
+
+push(@tests, "basic_func();");
+
+# Now that the tests are defined, set up the Test to deal with them.
+plan tests => scalar(@tests), todo => \@todo;
+
+# And run them all.
+foreach (@tests)
+{
+    eval "&$_";
+}
