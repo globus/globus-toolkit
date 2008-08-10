96ce0d1d2212c5a4bf3c62099802b202da35ebd6
bresnaha <>
2008-08-10 18:08:53 +0000

    changed some internal static names

--- xio/drivers/rate/source/globus_xio_rate_driver.c
+++ xio/drivers/rate/source/globus_xio_rate_driver.c
@@ -30,7 +30,7 @@ typedef enum
     GLOBUS_XIO_RATE_DEBUG_WARNING = 2,
     GLOBUS_XIO_RATE_DEBUG_TRACE = 4,
     GLOBUS_XIO_RATE_DEBUG_INFO = 8,
-} globus_xio_tb_debug_levels_t;
+} globus_xio_rate_debug_levels_t;
 
 
 #define GlobusXIOTBDebugPrintf(level, message)                              \
@@ -71,13 +71,13 @@ GlobusXIODefineModule(rate) =
 };
 
 typedef void
-(*l_xio_tb_finished_func_t)(
+(*l_xio_rate_finished_func_t)(
     globus_xio_operation_t              op,
     globus_result_t                     result,
     globus_size_t                       nread);
 
 typedef globus_result_t
-(*l_xio_tb_pass_func_t)(
+(*l_xio_rate_pass_func_t)(
     globus_xio_operation_t              op,
     globus_xio_iovec_t *                iovec,
     int                                 iovec_count,
@@ -85,20 +85,20 @@ typedef globus_result_t
     globus_xio_driver_data_callback_t   cb,
     void *                              user_arg);
 
-typedef struct l_xio_tb_attr_s
+typedef struct l_xio_rate_attr_s
 {
     globus_off_t                        rate;
     int                                 us_period;
     globus_size_t                       burst_size;
-} l_xio_tb_attr_t;
+} l_xio_rate_attr_t;
 
-typedef struct l_xio_tb_rw_attr_s
+typedef struct l_xio_rate_rw_attr_s
 {
-    l_xio_tb_attr_t                     read_attr;
-    l_xio_tb_attr_t                     write_attr;
-} l_xio_tb_attr_rw_t;
+    l_xio_rate_attr_t                     read_attr;
+    l_xio_rate_attr_t                     write_attr;
+} l_xio_rate_attr_rw_t;
 
-static l_xio_tb_attr_rw_t               l_xio_tb_default_attr;
+static l_xio_rate_attr_rw_t               l_xio_rate_default_attr;
 
 typedef struct l_xio_rate_op_handle_s
 {
@@ -107,8 +107,8 @@ typedef struct l_xio_rate_op_handle_s
     globus_off_t                        per_tic;
     globus_callback_handle_t            cb_handle;
     globus_reltime_t                    us_period;
-    l_xio_tb_finished_func_t            finished_func;
-    l_xio_tb_pass_func_t                pass_func;
+    l_xio_rate_finished_func_t            finished_func;
+    l_xio_rate_pass_func_t                pass_func;
     globus_bool_t                       outstanding;
     globus_size_t                       max_allowed;
     int                                 ref;
@@ -135,10 +135,10 @@ typedef struct l_xio_rate_handle_s
 
 static
 void
-l_xio_tb_net_ops(
+l_xio_rate_net_ops(
     l_xio_rate_op_handle_t *    op_handle);
 
-static globus_mutex_t                   xio_l_tb_hash_mutex;
+static globus_mutex_t                   xio_l_rate_hash_mutex;
 
 static
 void
@@ -226,13 +226,13 @@ globus_l_xio_rate_op_cb(
  */
 static
 void
-l_xio_tb_net_ops(
+l_xio_rate_net_ops(
     l_xio_rate_op_handle_t *    op_handle)
 {
     globus_size_t                       len;
     globus_result_t                     res;
     l_xio_rate_data_t *         data;
-    GlobusXIOName(l_xio_tb_net_ops);
+    GlobusXIOName(l_xio_rate_net_ops);
 
     GlobusXIOTBDebugEnter();
     if(op_handle->outstanding)
@@ -276,11 +276,11 @@ l_xio_tb_net_ops(
 
 static
 void
-l_xio_tb_ticker_cb(
+l_xio_rate_ticker_cb(
     void *                              user_arg)
 {
     l_xio_rate_op_handle_t *    op_handle;
-    GlobusXIOName(l_xio_tb_ticker_cb);
+    GlobusXIOName(l_xio_rate_ticker_cb);
 
     GlobusXIOTBDebugEnter();
     op_handle = (l_xio_rate_op_handle_t *) user_arg;
@@ -293,7 +293,7 @@ l_xio_tb_ticker_cb(
         {
             op_handle->allowed = op_handle->max_allowed;
         }
-        l_xio_tb_net_ops(op_handle);
+        l_xio_rate_net_ops(op_handle);
     }
     globus_mutex_unlock(&op_handle->mutex);
     GlobusXIOTBDebugExit();
@@ -306,7 +306,7 @@ l_xio_tb_ticker_cb(
  */
 static
 l_xio_rate_op_handle_t *
-xio_l_tb_start_ticker(
+xio_l_rate_start_ticker(
     l_xio_rate_op_handle_t *            handle)
 {
     globus_bool_t                       start = GLOBUS_FALSE;
@@ -333,7 +333,7 @@ xio_l_tb_start_ticker(
             &handle->cb_handle,
             &handle->us_period,
             &handle->us_period,
-            l_xio_tb_ticker_cb,
+            l_xio_rate_ticker_cb,
             handle);
     }
     return handle;
@@ -360,25 +360,25 @@ globus_l_xio_rate_open_cb(
     }
     else
     {
-        globus_mutex_lock(&xio_l_tb_hash_mutex);
+        globus_mutex_lock(&xio_l_rate_hash_mutex);
         {
-            handle->write_handle = xio_l_tb_start_ticker(
+            handle->write_handle = xio_l_rate_start_ticker(
                 handle->write_handle);
-            handle->read_handle = xio_l_tb_start_ticker(
+            handle->read_handle = xio_l_rate_start_ticker(
                 handle->read_handle);
         }
-        globus_mutex_unlock(&xio_l_tb_hash_mutex);
+        globus_mutex_unlock(&xio_l_rate_hash_mutex);
     }
     GlobusXIOTBDebugExit();
 }
 
 static
 l_xio_rate_op_handle_t *
-xio_l_tb_attr_to_handle(
+xio_l_rate_attr_to_handle(
     l_xio_rate_handle_t *               daddy,
-    l_xio_tb_attr_t *                   attr,
-    l_xio_tb_finished_func_t            finished_func,
-    l_xio_tb_pass_func_t                pass_func)
+    l_xio_rate_attr_t *                   attr,
+    l_xio_rate_finished_func_t            finished_func,
+    l_xio_rate_pass_func_t                pass_func)
 {
     l_xio_rate_op_handle_t *            handle;
 
@@ -421,29 +421,29 @@ globus_l_xio_rate_open(
 {
     globus_result_t                     res;
     l_xio_rate_handle_t *       handle;
-    l_xio_tb_attr_rw_t *                attr;
+    l_xio_rate_attr_rw_t *                attr;
     GlobusXIOName(globus_l_xio_rate_open);
 
     GlobusXIOTBDebugEnter();
     if(driver_attr == NULL)
     {
-        attr = &l_xio_tb_default_attr;
+        attr = &l_xio_rate_default_attr;
     }
     else
     {
-        attr = (l_xio_tb_attr_rw_t *) driver_attr;
+        attr = (l_xio_rate_attr_rw_t *) driver_attr;
     }
 
     handle = (l_xio_rate_handle_t *) 
         globus_calloc(1, sizeof(l_xio_rate_handle_t));
 
-    handle->read_handle = xio_l_tb_attr_to_handle(
+    handle->read_handle = xio_l_rate_attr_to_handle(
         handle,
         &attr->read_attr,
         globus_xio_driver_finished_read,
         globus_xio_driver_pass_read);
 
-    handle->write_handle = xio_l_tb_attr_to_handle(
+    handle->write_handle = xio_l_rate_attr_to_handle(
         handle,
         &attr->write_attr,
         globus_xio_driver_finished_write,
@@ -466,7 +466,7 @@ error:
 
 static
 globus_bool_t
-xio_l_tb_ref_dec(
+xio_l_rate_ref_dec(
     l_xio_rate_handle_t *               handle,
     l_xio_rate_op_handle_t *            op_handle,
     globus_callback_func_t              cb)
@@ -493,11 +493,11 @@ xio_l_tb_ref_dec(
 
 static
 void
-l_xio_tb_write_unreg(
+l_xio_rate_write_unreg(
     void *                              user_arg)
 {
     l_xio_rate_handle_t *       handle;
-    GlobusXIOName(l_xio_tb_read_unreg);
+    GlobusXIOName(l_xio_rate_read_unreg);
 
     GlobusXIOTBDebugEnter();
     handle = (l_xio_rate_handle_t *) user_arg;
@@ -512,25 +512,25 @@ l_xio_tb_write_unreg(
 
 static
 void
-l_xio_tb_read_unreg(
+l_xio_rate_read_unreg(
     void *                              user_arg)
 {
     globus_bool_t                       b = GLOBUS_FALSE;
     l_xio_rate_handle_t *       handle;
-    GlobusXIOName(l_xio_tb_read_unreg);
+    GlobusXIOName(l_xio_rate_read_unreg);
 
     GlobusXIOTBDebugEnter();
     handle = (l_xio_rate_handle_t *) user_arg;
 
-    globus_mutex_lock(&xio_l_tb_hash_mutex);
+    globus_mutex_lock(&xio_l_rate_hash_mutex);
     {
         if(handle->write_handle != NULL)
         {
-            b = xio_l_tb_ref_dec(handle,
-                handle->write_handle, l_xio_tb_write_unreg);
+            b = xio_l_rate_ref_dec(handle,
+                handle->write_handle, l_xio_rate_write_unreg);
         }
     }
-    globus_mutex_unlock(&xio_l_tb_hash_mutex);
+    globus_mutex_unlock(&xio_l_rate_hash_mutex);
 
     l_xio_rate_destroy_op_handle(handle->read_handle);
     if(!b)
@@ -557,23 +557,23 @@ globus_l_xio_rate_close_cb(
     handle = (l_xio_rate_handle_t *) user_arg;
     handle->close_result = result;
 
-    globus_mutex_lock(&xio_l_tb_hash_mutex);
+    globus_mutex_lock(&xio_l_rate_hash_mutex);
     {
         if(handle->read_handle != NULL)
         {
-            b = xio_l_tb_ref_dec(handle,
-                handle->read_handle, l_xio_tb_read_unreg);
+            b = xio_l_rate_ref_dec(handle,
+                handle->read_handle, l_xio_rate_read_unreg);
         }
         if(!b)
         {
             if(handle->write_handle != NULL)
             {
-                b = xio_l_tb_ref_dec(handle,
-                    handle->write_handle, l_xio_tb_write_unreg);
+                b = xio_l_rate_ref_dec(handle,
+                    handle->write_handle, l_xio_rate_write_unreg);
             }
         }
     }
-    globus_mutex_unlock(&xio_l_tb_hash_mutex);
+    globus_mutex_unlock(&xio_l_rate_hash_mutex);
 
     if(!b)
     {
@@ -663,7 +663,7 @@ globus_l_xio_rate_read(
 
         globus_mutex_lock(&data->op_handle->mutex);
         {
-            l_xio_tb_net_ops(data->op_handle);
+            l_xio_rate_net_ops(data->op_handle);
         }
         globus_mutex_unlock(&data->op_handle->mutex);
     }
@@ -723,7 +723,7 @@ globus_l_xio_rate_write(
 
         globus_mutex_lock(&data->op_handle->mutex);
         {
-            l_xio_tb_net_ops(data->op_handle);
+            l_xio_rate_net_ops(data->op_handle);
         }
         globus_mutex_unlock(&data->op_handle->mutex);
     }
@@ -746,15 +746,15 @@ globus_l_xio_rate_cntl(
 
 static
 globus_result_t
-globus_l_xio_tb_attr_copy(
+globus_l_xio_rate_attr_copy(
     void **                             dst,
     void *                              src)
 {
-    l_xio_tb_attr_rw_t *                src_attr;
-    l_xio_tb_attr_rw_t *                dst_attr;
+    l_xio_rate_attr_rw_t *                src_attr;
+    l_xio_rate_attr_rw_t *                dst_attr;
 
-    src_attr = (l_xio_tb_attr_rw_t *) src;
-    dst_attr = (l_xio_tb_attr_rw_t *) globus_calloc(1, sizeof(l_xio_tb_attr_rw_t));
+    src_attr = (l_xio_rate_attr_rw_t *) src;
+    dst_attr = (l_xio_rate_attr_rw_t *) globus_calloc(1, sizeof(l_xio_rate_attr_rw_t));
 
     dst_attr->read_attr.rate = src_attr->read_attr.rate;
     dst_attr->read_attr.burst_size = src_attr->read_attr.burst_size;
@@ -770,12 +770,12 @@ globus_l_xio_tb_attr_copy(
 
 static
 globus_result_t
-globus_l_xio_tb_attr_init(
+globus_l_xio_rate_attr_init(
     void **                             out_driver_attr)
 {
-    l_xio_tb_attr_t *                   attr;
+    l_xio_rate_attr_t *                   attr;
 
-    globus_l_xio_tb_attr_copy((void **)&attr, (void *)&l_xio_tb_default_attr);
+    globus_l_xio_rate_attr_copy((void **)&attr, (void *)&l_xio_rate_default_attr);
 
     *out_driver_attr = attr;
 
@@ -784,19 +784,19 @@ globus_l_xio_tb_attr_init(
 
 static
 globus_result_t
-globus_l_xio_tb_attr_destroy(
+globus_l_xio_rate_attr_destroy(
     void *                              driver_attr)
 {
-    l_xio_tb_attr_rw_t *                attr;
+    l_xio_rate_attr_rw_t *                attr;
 
-    attr = (l_xio_tb_attr_rw_t *) driver_attr;
+    attr = (l_xio_rate_attr_rw_t *) driver_attr;
     globus_free(attr);
 
     return GLOBUS_SUCCESS;
 }
 
 
-static globus_xio_string_cntl_table_t  tb_l_string_opts_table[] =
+static globus_xio_string_cntl_table_t  rate_l_string_opts_table[] =
 {
     {"rate", GLOBUS_XIO_RATE_SET_RATE, globus_xio_string_cntl_formated_off},
     {"read_rate", GLOBUS_XIO_RATE_SET_READ_RATE, globus_xio_string_cntl_formated_off},
@@ -813,15 +813,15 @@ static globus_xio_string_cntl_table_t  tb_l_string_opts_table[] =
 
 static
 globus_result_t
-globus_l_xio_tb_attr_cntl(
+globus_l_xio_rate_attr_cntl(
     void *                              driver_attr,
     int                                 cmd,
     va_list                             ap)
 {
-    l_xio_tb_attr_rw_t *                attr;
-    GlobusXIOName(globus_l_xio_tb_attr_cntl);
+    l_xio_rate_attr_rw_t *                attr;
+    GlobusXIOName(globus_l_xio_rate_attr_cntl);
 
-    attr = (l_xio_tb_attr_rw_t *) driver_attr;
+    attr = (l_xio_rate_attr_rw_t *) driver_attr;
 
     switch(cmd)
     {
@@ -896,13 +896,13 @@ globus_l_xio_rate_init(
 
     globus_xio_driver_set_attr(
         driver,
-        globus_l_xio_tb_attr_init,
-        globus_l_xio_tb_attr_copy,
-        globus_l_xio_tb_attr_cntl,
-        globus_l_xio_tb_attr_destroy);
+        globus_l_xio_rate_attr_init,
+        globus_l_xio_rate_attr_copy,
+        globus_l_xio_rate_attr_cntl,
+        globus_l_xio_rate_attr_destroy);
 
 
-    globus_xio_driver_string_cntl_set_table(driver, tb_l_string_opts_table);
+    globus_xio_driver_string_cntl_set_table(driver, rate_l_string_opts_table);
 
 
     *out_driver = driver;
@@ -935,15 +935,15 @@ globus_l_xio_rate_activate(void)
     {
         GlobusXIORegisterDriver(rate);
     }
-    globus_mutex_init(&xio_l_tb_hash_mutex, NULL);
+    globus_mutex_init(&xio_l_rate_hash_mutex, NULL);
     
-    l_xio_tb_default_attr.read_attr.rate = DEFAULT_RATE;
-    l_xio_tb_default_attr.read_attr.us_period = DEFAULT_PERIOD_US;
-    l_xio_tb_default_attr.read_attr.burst_size = -1;
+    l_xio_rate_default_attr.read_attr.rate = DEFAULT_RATE;
+    l_xio_rate_default_attr.read_attr.us_period = DEFAULT_PERIOD_US;
+    l_xio_rate_default_attr.read_attr.burst_size = -1;
 
-    l_xio_tb_default_attr.write_attr.rate = DEFAULT_RATE;
-    l_xio_tb_default_attr.write_attr.us_period = DEFAULT_PERIOD_US;
-    l_xio_tb_default_attr.write_attr.burst_size = -1;
+    l_xio_rate_default_attr.write_attr.rate = DEFAULT_RATE;
+    l_xio_rate_default_attr.write_attr.us_period = DEFAULT_PERIOD_US;
+    l_xio_rate_default_attr.write_attr.burst_size = -1;
 
     return rc;
 }
@@ -952,7 +952,7 @@ static
 int
 globus_l_xio_rate_deactivate(void)
 {
-    globus_mutex_destroy(&xio_l_tb_hash_mutex);
+    globus_mutex_destroy(&xio_l_rate_hash_mutex);
 
     GlobusXIOUnRegisterDriver(rate);
     return globus_module_deactivate(GLOBUS_XIO_MODULE);
