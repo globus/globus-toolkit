e4dc44a747a87e60414f7e6a35a9fccce5434b63
bester <>
2009-09-15 14:37:28 +0000

    Null-terminate extra arguments to scripts

--- gram/jobmanager/source/globus_gram_job_manager_script.c
+++ gram/jobmanager/source/globus_gram_job_manager_script.c
@@ -507,7 +507,8 @@ globus_gram_job_manager_script_submit(
                     script_cmd,
                     globus_l_gram_job_manager_default_done,
                     NULL,
-                    "emitcondorprocesses", 'd', 1);
+                    "emitcondorprocesses", 'd', 1,
+                    NULL);
     }
     else if (request->config->seg_module &&
         strcmp(request->config->seg_module, "fork") == 0)
@@ -517,7 +518,8 @@ globus_gram_job_manager_script_submit(
                     script_cmd,
                     globus_l_gram_job_manager_default_done,
                     NULL,
-                    "useforkstarter", 'd', 1);
+                    "useforkstarter", 'd', 1,
+                    NULL);
     }
     else
     {
