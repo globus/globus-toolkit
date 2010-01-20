df5ce7156818e24807c8201a70cc30f4d2cab53f
deschon <>
2010-01-20 19:35:56 +0000

    Updated options parsing and online help

--- gass/sync/client/source/globus_i_url_sync_args.c
+++ gass/sync/client/source/globus_i_url_sync_args.c
@@ -22,9 +22,11 @@
  * $Revision$
  */
 
+#include "globus_common.h"
 #include "globus_i_url_sync_args.h"
 #include "globus_url.h"
 #include "globus_libc.h"
+#include "version.h"  /* provides local_version */
 #include <getopt.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -37,13 +39,49 @@ globus_bool_t           globus_i_url_sync_args_debug;
 static globus_url_t     globus_l_url_sync_args_source;
 static globus_url_t     globus_l_url_sync_args_destination;
 
+static globus_args_option_descriptor_t args_options[2];
+static char *verbose_args[] = {"-v", "-verbose", GLOBUS_NULL};
+static char *debug_args[] = {"-d", "-debug", GLOBUS_NULL};
+enum {
+	arg_verbose = 1,
+	arg_debug,
+	arg_num = arg_debug,
+};
+static globus_args_option_descriptor_t verbose_def =
+	{arg_verbose, verbose_args, 0, GLOBUS_NULL, GLOBUS_NULL};
+static globus_args_option_descriptor_t debug_def =
+{arg_debug, debug_args, 0, GLOBUS_NULL, GLOBUS_NULL};
+
+static char * usage_str= 
+"\nglobus_url_sync [-help | -usage] [-version] [-d | -v] <sourceURL> <destURL>\n\n";
+static char * help_str= 
+"\nglobus_url_sync [options] <sourceURL> <destURL>\n\n"
+"OPTIONS\n"
+"  -help | -usage\n"
+"\tPrint help\n"
+"  -version\n"
+"\tPrint the version of this program\n"
+"  -d | -debug | -v | -verbose\n"
+"\tPrint additional detail.\n\n"
+"URL scheme(s) supported:\n"
+"  gsiftp\n"
+"    For example:\n"
+"\tFile name, absolute path:\t\"gsiftp://myhost.edu//tmp/file1\"\n"
+"\t\t\t\t\t\"gsiftp://myhost.edu/~/file1\"\n"
+"\tFile name, relative path:\t\"gsiftp://myhost.edu/file1\"\n"
+"\tDirectory, absolute path:\t\"gsiftp://myhost.edu//tmp/dir1/\"\n"
+"  file\n"
+"    To be supported in the future.\n"
+"\n"
+;
+
 /*
  * globus_l_url_sync_usage -- prints usage for this program
  */
 static void
 globus_l_url_sync_usage(char *prog)
 {
-    globus_libc_fprintf(stderr, "Usage: %s: [OPTIONS] SOURCE DESTINATION\n", prog);
+    globus_libc_fprintf(stderr, usage_str, prog);
 }
 /* globus_l_url_sync_usage */
 
@@ -53,7 +91,7 @@ globus_l_url_sync_usage(char *prog)
 static void
 globus_l_url_sync_badarg(char *badarg)
 {
-    globus_libc_fprintf(stderr, "Bad argument: %s\n", badarg);
+    globus_libc_fprintf(stderr, "Error, bad argument: %s\n", badarg);
 }
 /* globus_l_url_sync_badarg */
 
@@ -62,68 +100,98 @@ globus_l_url_sync_badarg(char *badarg)
  */
 globus_result_t
 globus_i_url_sync_parse_args(
-        int             argc,
-        char            *argv[])
+     int	argc,
+     char	 *argv[])
 {
-    int                 c, result;
+    int		result;
+    char	*program;
+    globus_args_option_instance_t *instance = NULL;
+    globus_list_t	*options_found = NULL,
+			*list = NULL;
 
     /* Defaults */
     globus_i_url_sync_args_verbose  = GLOBUS_FALSE;
     globus_i_url_sync_args_debug    = GLOBUS_FALSE;
 
-    /* Parse args */
-    while ((c = getopt(argc, argv, "dhv")) != -1)
-    {
-         switch (c)
-         {
-             case 'd':
-                 globus_i_url_sync_args_debug = GLOBUS_TRUE;
-                 break;
-             case 'v':
-                 globus_i_url_sync_args_verbose = GLOBUS_TRUE;
-                 break;
-             case '?':
-             case 'h':
-             default:
-                 goto usage;
-         }
+    /* determine the program name */
+	program = strrchr(argv[0],'/');
+    if (!program) {
+        program = argv[0];
+    } else {
+		program++;
     }
-
+	
+    args_options[0] = verbose_def;
+    args_options[1] = debug_def;
+    if (globus_args_scan(
+			&argc,
+			&argv,
+			arg_num,
+			args_options,
+			program,
+			&local_version,
+			usage_str,
+			help_str,
+			&options_found,
+			NULL) < 0) {
+        /* error on argument line */
+        return GLOBUS_FAILURE;
+    }
+    for (list = options_found;
+		 !globus_list_empty(list);
+		 list = globus_list_rest(list)) 
+	{
+		instance = globus_list_first(list);
+		
+        switch(instance->id_number) {
+			case arg_verbose:
+				globus_i_url_sync_args_verbose = GLOBUS_TRUE;
+				break;
+			case arg_debug:
+				globus_i_url_sync_args_debug = GLOBUS_TRUE;
+				break;
+			default:
+				/* should not get here */
+				break;
+		}
+	}
+	
+	globus_args_option_instance_list_free(&options_found);
+	
+	/* argc should be equal to 3; argv[1] is source and argv[2] is destination */
+	
     /* Get source, destination */
-    if (optind != argc-2)
-    {
+    if (argc != 3) {
         goto usage;
     }
-
-    result = globus_url_parse(argv[optind], &globus_l_url_sync_args_source);
-    if (result != GLOBUS_URL_SUCCESS)
-    {
-        globus_l_url_sync_badarg(argv[optind]);
+	
+    result = globus_url_parse(argv[1], &globus_l_url_sync_args_source);
+    if (result != GLOBUS_URL_SUCCESS) {
+        globus_l_url_sync_badarg(argv[1]);
         globus_url_destroy(&globus_l_url_sync_args_source);
         goto usage;
     }
     globus_i_url_sync_args_source = &globus_l_url_sync_args_source;
-
+	
     if (globus_i_url_sync_args_verbose)
-        globus_libc_fprintf(stderr, "Source: %s\n", argv[optind]);
-
-    result = globus_url_parse(argv[optind+1], &globus_l_url_sync_args_destination);
-    if (result != GLOBUS_URL_SUCCESS)
-    {
-        globus_l_url_sync_badarg(argv[optind+1]);
+        globus_libc_fprintf(stderr, "Source: %s\n", globus_i_url_sync_args_source);
+	
+    result = globus_url_parse(argv[2], &globus_l_url_sync_args_destination);
+    if (result != GLOBUS_URL_SUCCESS) {
+        globus_l_url_sync_badarg(argv[2]);
         globus_url_destroy(&globus_l_url_sync_args_source);
         globus_url_destroy(&globus_l_url_sync_args_destination);
         goto usage;
     }
     globus_i_url_sync_args_destination = &globus_l_url_sync_args_destination;
-
+	
     if (globus_i_url_sync_args_verbose)
-        globus_libc_fprintf(stderr, "Destination: %s\n", argv[optind+1]);
-
+        globus_libc_fprintf(stderr, "Destination: %s\n", globus_i_url_sync_args_destination);
+	
     return GLOBUS_SUCCESS;
-
+	
 usage:
-    globus_l_url_sync_usage(argv[0]);
+	globus_l_url_sync_usage(program);
     return GLOBUS_FAILURE;
 }
 /* globus_i_url_sync_parse_args */
