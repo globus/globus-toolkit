fc9a8f8f08355f66ac104f9d593aba05905074cf
mlink <>
2010-02-12 00:15:51 +0000

    add cygwin support files

---
+++ packaging/fait_accompli/cygwin.script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp -a source-trees/xio/drivers/gsi source-trees/xio/src/builtins/gsi
+cp -a source-trees/xio/drivers/popen/source source-trees/xio/src/builtins/popen
+cp -a source-trees/xio/drivers/pipe/source source-trees/xio/src/builtins/pipe
+
