de4478a687dfce7742c44a5a42df8aa86ca90dbd
mlink <>
2011-03-03 13:12:51 +0000

    update cygwin patches

--- packaging/fait_accompli/cygwin.script.sh
+++ packaging/fait_accompli/cygwin.script.sh
@@ -2,4 +2,4 @@
 cp -a source-trees/xio/drivers/gsi source-trees/xio/src/builtins/gsi
 cp -a source-trees/xio/drivers/popen/source source-trees/xio/src/builtins/popen
 cp -a source-trees/xio/drivers/pipe/source source-trees/xio/src/builtins/pipe
-
+mkdir source-trees/database/c/sqlite/patches
