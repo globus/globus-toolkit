d28af797448f1424172e35d0340e34c3dead7d1a
blau <>
2011-04-25 19:14:13 +0000

    add tags

--- packaging/checkout-specs.pl
+++ packaging/checkout-specs.pl
@@ -1,7 +1,15 @@
 #!/bin/env perl
 use Cwd;
+use Getopt::Long;
+my $sourcelistfile, $tag;
 
+GetOptions( 'f|file=s' => \$sourcelistfile,
+	    't|tag=s' => \$tag);
+  if ($sourcelistfile ne ''){
+    open(PKG, "$sourcelistfile");
+  }else{
   open(PKG, "etc/package-list-spec");
+  }
   mkdir "./source-trees";
   chdir "./source-trees";
   my $topsrcdir=cwd();
@@ -11,7 +19,17 @@ use Cwd;
         my ($pkg, $subdir, $custom, $pnb, $pkgtag) = split(' ', $_);
 	print cwd()."\n";
 	print $subdir."\n";
-	system("cvs -d blau\@cvs.globus.org:/home/globdev/CVS/globus-packages co $subdir ");
+	if ($tag ne ''){
+	  system("cvs -d blau\@cvs.globus.org:/home/globdev/CVS/globus-packages co -r $tag $subdir");
+	}else { #no overriding tag is given, default to HEAD unless there's a
+		#tag in the sourcelistfile
+	  if ($pkgtag eq ''){
+	    system("cvs -d blau\@cvs.globus.org:/home/globdev/CVS/globus-packages co $subdir ");}
+	  else {
+	    system("cvs -d blau\@cvs.globus.org:/home/globdev/CVS/globus-packages co -r $pkgtag $subdir");
+	  }
+	}
+	
     }
 
 
