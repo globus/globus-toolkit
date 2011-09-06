64c7e5bed131680688ffe087b0ebc745f022b75a
jbasney <>
2011-09-06 20:13:15 +0000

    merged OPENSSH_5_9P1_GSSAPI_20110906 to GPT-branch

--- gssapi-openssh/openssh/version.h
+++ gssapi-openssh/openssh/version.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: version.h,v 1.61 2011/02/04 00:44:43 djm Exp $ */
+/* $OpenBSD: version.h,v 1.62 2011/08/02 23:13:01 djm Exp $ */
 
 #ifdef GSI
 #define GSI_VERSION	" GSI"
@@ -18,11 +18,11 @@
 #define MGLUE_VERSION	""
 #endif
 
-#define GPT_VERSION	" GSI_GSSAPI_GPT_5.3"
+#define GPT_VERSION	" GSI_GSSAPI_GPT_5.4"
 
-#define SSH_VERSION	"OpenSSH_5.8"
+#define SSH_VERSION	"OpenSSH_5.9"
 
-#define SSH_PORTABLE	"p2"
+#define SSH_PORTABLE	"p1"
 #define SSH_HPN         "-hpn13v11"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN \
             GPT_VERSION GSI_VERSION KRB5_VERSION MGLUE_VERSION
