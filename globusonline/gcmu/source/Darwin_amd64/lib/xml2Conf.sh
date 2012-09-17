376892f81a08c8bf033dbbcd76d91938589a6741
bester <>
2012-09-17 15:24:39 +0000

    remove binaries

--- globusonline/gcmu/source/Darwin_amd64/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/lukasz/tmp/gcmu/scratch/gt/lib"
-XML2_LIBS="-lxml2    -liconv -lm  "
-XML2_INCLUDEDIR="-I/Users/lukasz/tmp/gcmu/scratch/gt/include/libxml2"
-MODULE_VERSION="xml2-1.8"
-
--- globusonline/gcmu/source/Linux_i386/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/tmp/scratch/gt/lib"
-XML2_LIBS="-lxml2    -lm  "
-XML2_INCLUDEDIR="-I/tmp/scratch/gt/include/libxml2"
-MODULE_VERSION="xml2-1.8"
-
