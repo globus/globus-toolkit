5739d26e69bc5dd4eb84559cd73ba7c611cc1c0a
Joseph Bester <bester@uchicamando.local>
2013-03-28 05:24:59 -0400

    Make it easier to generate native RPM packages
    - Add a setup.py file and MANIFEST.in so that its easier to create
      a source tarball and direct the various pieces of the package to the correct
      directories.
    - Create an example apache 2.4 configuration with the new allow/deny syntax
    - Fix non-absolute sqlite url path
    - Add workaround for older versions of libraries in RHEL5 based systems,
      though there remain issues in RHEL5 and RHEL6.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+from distutils.core import setup
+import os
+
+setup(name = 'myproxy_oauth',
+    version = '0.0',
+    description = 'MyProxy OAuth Delegation Service',
+    author = 'Globus Toolkit',
+    author_email = 'support@globus.org',
+    packages = [
+            'myproxy',
+            'myproxyoauth',
+            'oauth2'],
+    package_data = {
+        'myproxyoauth': [ 'templates/*.html', 'static/*.png' ]
+    },
+    scripts = [ 'wsgi.py' ],
+    data_files = [ ('conf.d', [ 'conf/myproxy-oauth' ])],
+)
