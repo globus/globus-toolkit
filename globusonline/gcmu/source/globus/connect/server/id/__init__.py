52abd01234acc4eeecf288fdc9763b2cadcb0fef
bester <>
2013-10-09 17:14:55 +0000

    Missing dir in rename

---
+++ globusonline/gcmu/source/globus/connect/server/id/__init__.py
@@ -0,0 +1,292 @@
+#!/usr/bin/python
+# Copyright 2012-2013 University of Chicago
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Configure a MyProxy server for use with Globus Online
+
+"""
+
+import copy
+import logging
+import os
+import pkgutil
+import string
+import shutil
+import sys
+
+__path__ = pkgutil.extend_path(__path__, __name__)
+
+import globus.connect.security as security
+import globus.connect.server as gcmu
+
+from globusonline.transfer.api_client import TransferAPIClient
+from globusonline.transfer.api_client import TransferAPIError
+from urlparse import urlparse
+
+from subprocess import Popen, PIPE
+
+class ID(gcmu.GCMU):
+    """
+    Configure a MyProxy service based on a GCMU configuration. 
+    """
+    def __init__(self, **kwargs):
+        super(ID, self).__init__(**kwargs)
+        self.myproxy_cred_repo_config = None
+        self.myproxy_ca_config = None
+        self.myproxy_pam_config = None
+        self.myproxy_mapapp_config = None
+        self.service = "myproxy-server"
+
+    def is_local(self):
+        return self.is_local_myproxy()
+
+    def setup(self, **kwargs):
+        self.logger.debug("ENTER: ID.setup()")
+
+        if not self.is_local_myproxy():
+            print "Using MyProxy server on " \
+                + str(self.conf.get_myproxy_server())
+            self.logger.debug("No MyProxy configured for this host")
+            return
+
+        self.configure_credential(**kwargs)
+        self.configure_myproxy_ca(**kwargs)
+        self.configure_trust_roots(**kwargs)
+        self.configure_myproxy_pam()
+        self.configure_myproxy_mapapp()
+        self.configure_myproxy_cred_repo()
+        self.configure_myproxy_port()
+        
+        self.write_myproxy_conf()
+        self.write_myproxy_init_conf()
+        self.enable()
+        self.restart()
+        cadir = self.conf.get_myproxy_ca_directory()
+        cert_path = os.path.join(cadir, "cacert.pem")
+
+        print "Configured MyProxy server on " \
+            + self.conf.get_myproxy_server() + ":7512"
+        print "CA DN: " + security.get_certificate_subject(cert_path)
+        myproxy_dn = self.get_myproxy_dn_from_server()
+        if myproxy_dn is not None:
+            print "Service DN: " + myproxy_dn
+
+        self.logger.debug("EXIT: ID.setup()")
+        
+    def cleanup(self, **kwargs):
+        self.logger.debug("ENTER: IO:cleanup()")
+        server = self.conf.get_myproxy_server()
+        if server is None:
+            self.logger.debug("No MyProxy server defined")
+            return
+
+        self.disable()
+        self.stop()
+
+        myproxy_dir = self.conf.get_etc_myproxy_d()
+
+        if os.path.exists(myproxy_dir):
+            for name in os.listdir(myproxy_dir):
+                if name.startswith("globus-connect-server"):
+                    os.remove(os.path.join(myproxy_dir, name))
+        self.cleanup_trust_roots()
+        self.logger.debug("EXIT: IO:cleanup()")
+
+
+    def configure_myproxy_ca(self, force=False):
+        if not self.conf.get_myproxy_ca():
+            self.logger.debug("Not using MyProxy CA, nothing to configure")
+            return
+
+        cadir = self.conf.get_myproxy_ca_directory()
+        if force:
+            if cadir is not None and os.path.exists(cadir):
+                shutil.rmtree(cadir, ignore_errors=True)
+
+        if cadir is not None and not os.path.exists(cadir):
+            ca_subject = self.conf.get_myproxy_ca_subject_dn()
+            if ca_subject is None:
+                ca_subject = security.get_certificate_subject(
+                        self.conf.get_security_certificate_file(),
+                        nameopt='RFC2253')
+            try:
+                args = [ 
+                    'grid-ca-create',
+                    '-nobuild',
+                    '-verbose',
+                    '-dir', self.conf.get_myproxy_ca_directory(),
+                    '-subject', ca_subject,
+                    '-noint']
+                if force:
+                    args.append('-force')
+                ca_create = Popen(args, stdout = PIPE, stderr = PIPE)
+                (out, err) = ca_create.communicate()
+                out = "".join(s for s in out if s in string.printable)
+                err = "".join(s for s in err if s in string.printable)
+                self.logger.debug("ca create output: " + out)
+                self.logger.debug("ca create stderr: " + err)
+            finally:
+                pass
+
+            if ca_create.returncode != 0:
+                raise Exception("Error creating CA: " + \
+                    str(ca_create.returncode) + out + err)
+
+        trustdir = self.conf.get_security_trusted_certificate_directory()
+        if trustdir is not None:
+            if not os.path.exists(trustdir):
+                os.makedirs(trustdir, 0755)
+
+            cert_path = os.path.join(cadir, "cacert.pem")
+            signing_policy_path = os.path.join(cadir, "signing-policy")
+
+            cahash = security.get_certificate_hash(cert_path)
+
+            installed_cert_path = os.path.join(trustdir, cahash + ".0")
+            installed_signing_policy = os.path.join(
+                trustdir, cahash + ".signing_policy")
+
+            shutil.copyfile(signing_policy_path, installed_signing_policy)
+            os.chmod(installed_signing_policy, 0644)
+            shutil.copyfile(cert_path, installed_cert_path)
+            os.chmod(installed_cert_path, 0644)
+
+        self.myproxy_ca_config = """
+                certificate_issuer_cert "%(cadir)s/cacert.pem"
+                certificate_issuer_key "%(cadir)s/private/cakey.pem"
+                certificate_issuer_key_passphrase "%(passphrase)s"
+                certificate_serialfile "%(cadir)s/serial"
+                certificate_out_dir "%(cadir)s/newcerts"
+                certificate_issuer_subca_certfile "%(cadir)s/cacert.pem"
+                max_cert_lifetime 168
+                cert_dir %(certdir)s
+                """ % {
+                    'cadir': cadir,
+                    'passphrase': self.conf.get_myproxy_ca_passphrase(),
+                    'certdir': \
+                        self.conf.get_security_trusted_certificate_directory()
+                }
+
+    def configure_myproxy_pam(self, force=False):
+        if not self.conf.get_myproxy_use_pam_login:
+            self.myproxy_pam_config = None
+            return
+
+        self.myproxy_pam_config = """
+                pam  "required"
+                pam_id "login"
+"""
+
+    def configure_myproxy_mapapp(self, force=False):
+        method = self.conf.get_security_authorization_method()
+        if method != "MyProxyGridmapCallout":
+            self.logger.debug("Not using MyProxy GridMap Callout, " +
+                "nothing to configure")
+            return
+        cadir = self.conf.get_myproxy_ca_directory() 
+        mapapp = os.path.join(cadir, 'mapapp')
+        dn = security.get_certificate_subject(
+                os.path.join(cadir, "cacert.pem"))
+
+        mapapp_template = pkgutil.get_data(
+                "globus.connect.server", "mapapp-template")
+
+        old_umask = os.umask(022)
+        mapapp_file = file(mapapp, "w")
+        try:
+            mapapp_file.write(mapapp_template % { 'dn': dn })
+        finally:
+            mapapp_file.close()
+        os.umask(old_umask)
+        os.chmod(mapapp, 0755)
+
+        self.myproxy_mapapp_config = "certificate_mapapp " + mapapp + "\n"
+
+    def configure_myproxy_port(self, force=False):
+        self.myproxy_port_config = ""
+        server = self.conf.get_myproxy_server()
+        if ":" in server:
+            port = int(server.split(":")[1])
+            self.myproxy_port_config = "-p %d " % port
+        
+    def configure_myproxy_cred_repo(self, force=False):
+        self.myproxy_cred_repo_config = """
+                authorized_retrievers      "*"
+                default_retrievers         "*"
+                authorized_renewers        "*"
+                default_renewers           "none"
+                default_key_retrievers     "none"
+                trusted_retrievers         "*"
+                default_trusted_retrievers "none"
+                """
+
+    def write_myproxy_conf(self):
+        old_mask = os.umask(077)
+        conffile = file(self.conf.get_myproxy_config_file(), "w")
+        try:
+            if self.myproxy_cred_repo_config is not None:
+                conffile.write(self.myproxy_cred_repo_config)
+            if self.myproxy_ca_config is not None:
+                conffile.write(self.myproxy_ca_config)
+            if self.myproxy_pam_config is not None:
+                conffile.write(self.myproxy_pam_config)
+            if self.myproxy_mapapp_config is not None:
+                conffile.write(self.myproxy_mapapp_config)
+        finally:
+            conffile.close()
+            os.umask(old_mask)
+
+    def write_myproxy_init_conf(self):
+        var_myproxy_d = self.conf.get_var_myproxy_d()
+        etc_myproxy_d = self.conf.get_etc_myproxy_d()
+
+        if not os.path.exists(var_myproxy_d):
+            os.makedirs(var_myproxy_d, 0755)
+        if not os.path.exists(etc_myproxy_d):
+            os.makedirs(etc_myproxy_d, 0755)
+
+        conf_file_name = os.path.join(var_myproxy_d, "globus-connect-server")
+        conf_link_name = os.path.join(etc_myproxy_d, "globus-connect-server")
+
+        if os.path.lexists(conf_link_name):
+            os.remove(conf_link_name)
+
+        conf_file = open(conf_file_name, 'w')
+        try:
+            store = os.path.join(
+                self.conf.get_myproxy_ca_directory(),
+                "store")
+
+            if not os.path.exists(store):
+                os.makedirs(store, 0700)
+            if self.conf.get_myproxy_use_pam_login():
+                conf_file.write("export MYPROXY_USER=root\n")
+            conf_file.write("export X509_CERT_DIR=\"%s\"\n" % \
+                    self.conf.get_security_trusted_certificate_directory())
+            conf_file.write("export X509_USER_CERT=\"%s\"\n" % \
+                    self.conf.get_security_certificate_file())
+            conf_file.write("export X509_USER_KEY=\"%s\"\n" % \
+                    self.conf.get_security_key_file())
+            conf_file.write("export X509_USER_PROXY=\"\"\n")
+            conf_file.write("export MYPROXY_OPTIONS=" + \
+                    "\"${MYPROXY_OPTIONS:+$MYPROXY_OPTIONS }%s-c %s -s %s\"" \
+                    % ( 
+                        self.myproxy_port_config,
+                        self.conf.get_myproxy_config_file(),
+                        store))
+            os.symlink(conf_file_name, conf_link_name)
+        finally:
+            conf_file.close()
+
+# vim: filetype=python:
---
+++ globusonline/gcmu/source/globus/connect/server/io/cleanup.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+
+# Copyright 2012-2013 University of Chicago
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Remove a GridFTP server configuration and remove it from a Globus Online endpoint
+
+globus-connect-server-io-cleanup [-h|--help]
+globus-connect-server-io-cleanup {-c FILENAME|--config-file=FILENAME}
+                                    {-v|--verbose}
+                                    {-r PATH|--root=PATH}
+                                    {-d|--delete-endpoint}
+
+The globus-connect-server-io-cleanup command deletes GridFTP service
+configuration previously created by running globus-connect-server-io-setup
+and removes it from a Globus Online endpoint. It deletes configuration files,
+stops, and disables the GridFTP server. It also removes the server from an
+endpoint, optionally deleting the endpoint.
+
+If the -d or --delete-endpoint command-line option is used,
+globus-connect-server-io-cleanup removes the endpoint named in the
+configuration file as well as cleaning up
+globus-connect-server-io-setup-generated configuration files.
+
+If the -r PATH or --root=PATH command-line option is used,
+globus-connect-server-io-cleanup will write its GridFTP configuration and
+certificates in a subdirectory rooted at PATH instead of /. This means, for
+example, that globus-connect-server-io-cleanup deletes GridFTP configuration
+files in PATH/etc/gridftp.d.
+
+The following options are available:
+
+-h, --help
+                                Display help information
+-c FILENAME, --config-file=FILENAME
+                                Use configuration file FILENAME instead of
+                                /etc/globus-connect-server.conf
+-v, --verbose                   Print more information about tasks
+-r PATH, --root=PATH            Add PATH as the directory prefix for the
+                                configuration files that
+                                globus-connect-server-io-cleanup writes
+-d, --delete-endpoint           Delete the Globus Online endpoint
+"""
+
+short_usage = """globus-connect-server-io-cleanup [-h|--help]
+globus-connect-server-io-cleanup {-c FILENAME|--config-file=FILENAME}
+                                    {-v|--verbose}
+                                    {-r PATH|--root=PATH}
+                                    {-d|--delete-endpoint}
+"""
+
+import getopt
+import getpass
+import os
+import socket
+import ssl
+import sys
+import time
+import traceback
+
+from globus.connect.server import get_api
+from globusonline.transfer.api_client import TransferAPIClient
+from globus.connect.server.io import IO
+from globus.connect.server.configfile import ConfigFile
+
+def usage(short=False, outstream=sys.stdout):
+    if short:
+        print >>outstream, short_usage
+    else:
+        print >>outstream, __doc__
+
+if __name__ == "__main__":
+    try:
+        conf_filename = None
+        api = None
+        force = False
+        debug = False
+        root = "/"
+        delete = False
+        try:
+            opts, arg = getopt.getopt(sys.argv[1:], "hc:vr:d",
+                    ["help", "config-file=", "verbose", "root=", "delete-endpoint"])
+        except getopt.GetoptError, e:
+            print >>sys.stderr, "Invalid option " + e.opt
+            usage(short=True, outstream=sys.stderr)
+            sys.exit(1)
+
+        if len(arg) > 0:
+            print >>sys.stderr, "Unexpected argument(s) " + " ".join(arg)
+            sys.exit(1)
+        
+        for (o, val) in opts:
+            if o in ['-h', '--help']:
+                usage()
+                sys.exit(0)
+            elif o in ['-c', '--config-file']:
+                conf_filename = val
+            elif o in ['-v',  '--verbose']:
+                debug = True
+            elif o in ['-r', '--root']:
+                root = val
+            elif o in ['-d', '--delete-endpoint']:
+                delete = True
+            else:
+                print >>sys.stderr, "Unknown option %s" %(o)
+                sys.exit(1)
+
+        os.umask(022)
+        conf = ConfigFile(config_file=conf_filename, root=root)
+        api = get_api(conf)
+        io = IO(config_obj=conf, api=api, debug=debug)
+        io.cleanup(delete=delete)
+    except KeyboardInterrupt, e:
+        print "Aborting..."
+        sys.exit(1)
+    except Exception, e:
+        if debug:
+            traceback.print_exc(file=sys.stderr)
+        else:
+            print str(e)
+        sys.exit(1)
+
+# vim: filetype=python:
