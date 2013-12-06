b96a4de096affc9bcfc466562b32fed38c0860f0
Joseph Bester <bester@mcs.anl.gov>
2013-12-06 16:18:40 -0500

    Add tests to globus_gsi_openssl_error package

--- gsi/openssl_error/source/test/globus-openssl-error-test.pl
+++ gsi/openssl_error/source/test/globus-openssl-error-test.pl
@@ -11,55 +11,29 @@ Tests to exercise the error handling functionality of the globus
 
 use strict;
 use Test::More;
+use File::Basename;
+use lib dirname($0);
 
+$ENV{PATH} = dirname($0).":.:".$ENV{PATH};
 my $test_prog = 'globus_openssl_error_test';
-
+my $stdoutfile = dirname($0)."/$test_prog.stdout";
 my @tests;
 my @todo;
 
 sub basic_func
 {
     my ($errors,$rc) = ("",0);
-   
-    $rc = system("./$test_prog 1>$test_prog.log.stdout 2>$test_prog.log.stderr");
+    ok($rc = system("$test_prog 1>$test_prog.log.stdout 2>$test_prog.log.stderr") == 0, "run $test_prog");
 
-    if($rc != 0)
-    {
-        $errors .= "Test exited with $rc. ";
-    }
-
-    if($rc & 128)
-    {
-        $errors .= "\n# Core file generated.";
-    }
-    open(EXPECTED, "<$test_prog.stdout") || die "Couldn't open $test_prog.stdout: $!\n";
-    open(LOGGED, "<$test_prog.log.stdout") || die "Couldn't open $test_prog.log.stdout: $!\n";
+    ok(open(EXPECTED, "<$stdoutfile"), "Open $stdoutfile");
+    ok(open(LOGGED, "<$test_prog.log.stdout"), "Open $test_prog.log.stdout");
     $rc = 0;
     while ( my $line = <EXPECTED> )
     {
         my $logged = <LOGGED>;
         $rc++ unless ( $logged =~ /$line/ );
     }
-					   
-    if($rc != 0)
-    {
-        $errors .= "Test produced unexpected output, see $test_prog.log.stdout";
-    }
-
-    ok($errors eq '', 'globus_error_wrap_openssl_error');
-
-    if($errors eq '')
-    {
-        if( -e "$test_prog.log.stdout" )
-        {
-	    unlink("$test_prog.log.stdout");
-        }
-    }
-    else
-    {
-        ok($errors, 'success');
-    }
-
+    ok($rc == 0, "Match reference output");
 }
 
 sub sig_handler
@@ -78,7 +52,7 @@ $SIG{'KILL'} = 'sig_handler';
 push(@tests, "basic_func();");
 
 # Now that the tests are defined, set up the Test to deal with them.
-plan tests => scalar(@tests), todo => \@todo;
+plan tests => 4*scalar(@tests), todo => \@todo;
 
 # And run them all.
 foreach (@tests)
