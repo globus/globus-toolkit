1357ec508e63a68fa7d98c32f0aad8649d5bc4bb
Joseph Bester <bester@mcs.anl.gov>
2014-01-27 08:32:22 -0500

    GT-515: Increase default proxy key size in gsi-proxy-core

--- gsi/proxy/proxy_core/source/library/globus_gsi_proxy_handle_attrs.c
+++ gsi/proxy/proxy_core/source/library/globus_gsi_proxy_handle_attrs.c
@@ -21,7 +21,7 @@
 #include "globus_i_gsi_proxy.h"
 #include <errno.h>
 
-#define DEFAULT_KEY_BITS                512
+#define DEFAULT_KEY_BITS                1024
 #define DEFAULT_PUB_EXPONENT            RSA_F4  /* 65537 */
 #define DEFAULT_SIGNING_ALGORITHM       NULL
 #define DEFAULT_CLOCK_SKEW              (5*60)    /* actually in seconds */
