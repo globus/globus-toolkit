1adecd59f3d7c2abe3262e90044ed3851df056cb
Joseph Bester <bester@mcs.anl.gov>
2014-02-12 14:25:22 -0500

    Test fixes

--- gridftp/control/source/test/get_lingering_close.sh
+++ gridftp/control/source/test/get_lingering_close.sh
@@ -2,14 +2,15 @@
 portfile="$(basename $0).port"
 rm -f "${portfile}"
 ./test_server > "${portfile}" &
+serverpid=$!
 i=0
 while [ ! -s "${portfile}" ]; do
     sleep 1;
     i=$(($i + 1))
     if [ $i -eq 10 ]; then
-        kill %1
+        [ ${serverpid:-0} -gt 0 ] && kill -0 $serverpid && kill $serverpid
         exit 1
     fi
 done
 $(basename $0 .sh) --host localhost $(cat ${portfile})
-kill %1
+([ ${serverpid:-0} -gt 0 ] && kill -0 $serverpid && kill $serverpid) || true
--- packaging/fedora/globus-ftp-control.spec
+++ packaging/fedora/globus-ftp-control.spec
@@ -1,6 +1,6 @@
 Name:		globus-ftp-control
 %global _name %(tr - _ <<< %{name})
-Version:	5.3
+Version:	5.4
 Release:	1%{?dist}
 Summary:	Globus Toolkit - GridFTP Control Library
 
@@ -134,6 +134,9 @@ rm -rf $RPM_BUILD_ROOT
 %{_mandir}/man3/*
 
 %changelog
+* Wed Feb 12 2014 Globus Toolkit <support@globus.org> - 5.4-1
+- Test fixes
+
 * Wed Feb 12 2014 Globus Toolkit <support@globus.org> - 5.3-1
 - Test fixes
 
