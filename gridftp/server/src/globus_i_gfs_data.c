2eb8c5cc05047788794753369b3b88d6eb78fee4
michaellink <mlink@mcs.anl.gov>
2014-04-04 14:25:43 -0500

    fix stor hang fix possible thread hang after consecutive ops

--- gridftp/server/src/globus_i_gfs_data.c
+++ gridftp/server/src/globus_i_gfs_data.c
@@ -11936,7 +11936,7 @@ globus_gridftp_server_get_optimal_concurrency(
     if(!op->writing)
     {    
         globus_mutex_lock(&op->session_handle->mutex);
-        if(op->data_handle->state = GLOBUS_L_GFS_DATA_HANDLE_INUSE && 
+        if(op->data_handle->state == GLOBUS_L_GFS_DATA_HANDLE_INUSE && 
             op->data_handle->is_mine)
         {
         /* query number of currently connected streams and update
