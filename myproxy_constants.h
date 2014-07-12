4c73cf886c48c6b821280a95588d770ae9208de1
jbasney <jbasney>
2014-07-12 15:05:23 +0000

    new MYPROXY_CREDS_MAX_NAMELEN constant for hashing filenames when long username or long credename is used

--- myproxy_constants.h
+++ myproxy_constants.h
@@ -22,6 +22,8 @@
 
 #define MYPROXY_DEFAULT_CLOCK_SKEW     300     /* 5 minutes */
 
+#define MYPROXY_CREDS_MAX_NAMELEN      80
+
 /* myproxy client protocol information */
 /* beware no string below may be a suffix of another */
 #define MYPROXY_VERSION_STRING      "VERSION="
