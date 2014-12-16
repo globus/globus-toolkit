760bf762f5ec207c214b76b15a850075d9080363
Joseph Bester <bester@mcs.anl.gov>
2014-12-16 14:52:25 -0500

    Add more context tests

--- gridftp/network_manager/test/context_test.c
+++ gridftp/network_manager/test/context_test.c
@@ -163,11 +163,15 @@ context_pre_listen_test(void)
             &attr_array_out);
     TEST_ASSERT_RESULT_SUCCESS(result);
 
+    TEST_ASSERT(attr_array_out != NULL);
+    TEST_ASSERT(attr_array_out[0].scope != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].scope,
             "return_function_called_module") == 0);
+    TEST_ASSERT(attr_array_out[0].name != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].name,
             "function") == 0);
-    TEST_ASSERT(strcmp(attr_array_out[0].name,
+    TEST_ASSERT(attr_array_out[0].value != NULL);
+    TEST_ASSERT(strcmp(attr_array_out[0].value,
             "pre_listen") == 0);
 
     globus_net_manager_attr_array_delete(attr_array_out);
@@ -302,11 +306,15 @@ context_post_listen_test(void)
             &attr_array_out);
     TEST_ASSERT_RESULT_SUCCESS(result);
 
+    TEST_ASSERT(attr_array_out != NULL);
+    TEST_ASSERT(attr_array_out[0].scope != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].scope,
             "return_function_called_module") == 0);
+    TEST_ASSERT(attr_array_out[0].name != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].name,
             "function") == 0);
-    TEST_ASSERT(strcmp(attr_array_out[0].name,
+    TEST_ASSERT(attr_array_out[0].value != NULL);
+    TEST_ASSERT(strcmp(attr_array_out[0].value,
             "post_listen") == 0);
 
     free(local_contact_out);
@@ -423,11 +431,15 @@ context_pre_accept_test(void)
             &attr_array_out);
     TEST_ASSERT_RESULT_SUCCESS(result);
 
+    TEST_ASSERT(attr_array_out != NULL);
+    TEST_ASSERT(attr_array_out[0].scope != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].scope,
             "return_function_called_module") == 0);
+    TEST_ASSERT(attr_array_out[0].name != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].name,
             "function") == 0);
-    TEST_ASSERT(strcmp(attr_array_out[0].name,
+    TEST_ASSERT(attr_array_out[0].value != NULL);
+    TEST_ASSERT(strcmp(attr_array_out[0].value,
             "pre_accept") == 0);
 
     globus_net_manager_attr_array_delete(attr_array_out);
@@ -562,11 +574,15 @@ context_post_accept_test(void)
             &attr_array_out);
     TEST_ASSERT_RESULT_SUCCESS(result);
 
+    TEST_ASSERT(attr_array_out != NULL);
+    TEST_ASSERT(attr_array_out[0].scope != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].scope,
             "return_function_called_module") == 0);
+    TEST_ASSERT(attr_array_out[0].name != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].name,
             "function") == 0);
-    TEST_ASSERT(strcmp(attr_array_out[0].name,
+    TEST_ASSERT(attr_array_out[0].value != NULL);
+    TEST_ASSERT(strcmp(attr_array_out[0].value,
             "post_accept") == 0);
 
     globus_net_manager_attr_array_delete(attr_array_out);
@@ -701,11 +717,15 @@ context_pre_connect_test(void)
             &attr_array_out);
     TEST_ASSERT_RESULT_SUCCESS(result);
 
+    TEST_ASSERT(attr_array_out != NULL);
+    TEST_ASSERT(attr_array_out[0].scope != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].scope,
             "return_function_called_module") == 0);
+    TEST_ASSERT(attr_array_out[0].name != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].name,
             "function") == 0);
-    TEST_ASSERT(strcmp(attr_array_out[0].name,
+    TEST_ASSERT(attr_array_out[0].value != NULL);
+    TEST_ASSERT(strcmp(attr_array_out[0].value,
             "pre_connect") == 0);
 
     globus_net_manager_attr_array_delete(attr_array_out);
@@ -830,11 +850,15 @@ context_post_connect_test(void)
             &attr_array_out);
     TEST_ASSERT_RESULT_SUCCESS(result);
 
+    TEST_ASSERT(attr_array_out != NULL);
+    TEST_ASSERT(attr_array_out[0].scope != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].scope,
             "return_function_called_module") == 0);
+    TEST_ASSERT(attr_array_out[0].name != NULL);
     TEST_ASSERT(strcmp(attr_array_out[0].name,
             "function") == 0);
-    TEST_ASSERT(strcmp(attr_array_out[0].name,
+    TEST_ASSERT(attr_array_out[0].value != NULL);
+    TEST_ASSERT(strcmp(attr_array_out[0].value,
             "post_connect") == 0);
 
     globus_net_manager_attr_array_delete(attr_array_out);
@@ -1061,6 +1085,45 @@ context_post_close_test(void)
 }
 /* context_post_close_test() */
 
+int
+context_chain_plus_minus_listen_test(void)
+{
+    globus_result_t                     result = GLOBUS_SUCCESS;
+    globus_net_manager_context_t        context = NULL;
+    const char                         *task_id = "42";
+    const char                         *transport = "tcp";
+    globus_net_manager_attr_t           attr[] = {
+        {"net_manager,", "manager", "python"},
+        {"python,", "pymod", "port_plus_one"},
+        {"net_manager,", "manager", "python"},
+        {"python,", "pymod", "port_minus_one"},
+        {"tcp", "port", "42"},
+        GLOBUS_NET_MANAGER_NULL_ATTR
+    };
+    globus_net_manager_attr_t          *attr_array_out;
+
+    result = globus_net_manager_context_init(&context, attr);
+    TEST_ASSERT_RESULT_SUCCESS(result);
+
+    result = globus_net_manager_context_pre_listen(
+            context,
+            task_id,
+            transport,
+            attr,
+            &attr_array_out);
+    TEST_ASSERT(result != GLOBUS_SUCCESS);
+    TEST_ASSERT(attr_array_out != NULL);
+    TEST_ASSERT(attr_array_out[0].scope != NULL);
+    TEST_ASSERT(strcmp(attr_array_out[0].scope, "tcp") == 0);
+    TEST_ASSERT(strcmp(attr_array_out[0].name, "port") == 0);
+    TEST_ASSERT(strcmp(attr_array_out[0].value, "42") == 0);
+
+    globus_net_manager_context_destroy(context);
+
+    return 0;
+}
+/* context_chain_plus_minus_listen_test() */
+
 struct tests
 {
     char * test_name;
@@ -1098,6 +1161,7 @@ main(int argc, char *argv[])
         TEST_INITIALIZER(context_pre_close_test),
         TEST_INITIALIZER(context_post_close_null_test),
         TEST_INITIALIZER(context_post_close_test),
+        TEST_INITIALIZER(context_chain_plus_minus_listen_test),
         {NULL, NULL}
     };
     int i;
